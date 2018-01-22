f6e89d76dec31fb5d0a292b19cfe21c8d9383dab
virajdave <virajdave@cmail.carleton.ca>
2018-01-22 00:21:59 -0500

    January 22 Edition

---
+++ models/newsletters/2018-01-22.js
@@ -0,0 +1,300 @@
+module.exports = {
+    toast: 'Keeping up with IEEE, a weekly newsletter',
+    articles: [
+        {
+            title: 'IEEE SPAC 2018 Networking Workshop',
+            img: 'NetworkingWorkshop-01.png',
+            date: ' Monday, January 22, 2018',
+            time: '5:30 PM - 7:00 PM',
+            location: 'ME 4359, Carleton University',
+            body: [
+                {
+                    tag: 'p',
+                    text: `The SPAC 2018 Networking workshop aims to prepare you for efficient communication between you and a professional. This workshop will give you tips and tricks on how to network, what to do and what not to do.`
+                },
+                {
+                    tag: 'p',
+                    text: `With SPAC 2018, the Student Professional Awareness Conference, coming up, this is the perfect opportunity to brush up on your networking skills before the formal dinner networking event!`
+                },
+                {
+                    tag: 'p',
+                    text: `If you have not already gotten your tickets to SPAC, get them now at: http://ieeespac.ca/`
+                },
+                {
+                    tag: 'a',
+                    attr: {
+                        href: 'https://docs.google.com/forms/d/e/1FAIpQLSfy8_JikSPIJx9KKEoN8MrzkrUMu5Fl8Vz6i_b3hOy080woDQ/viewform'
+                    },
+                    text: `Register here!`
+                },
+                {
+                    tag: `a`,
+                    attr: {
+                        href: 'https://www.facebook.com/events/1712512845483051/'
+                    },
+                    text: `Facebook Event Page`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `We hope to see all of you at the networking workshop!`
+                },
+            ]
+        },
+        {
+            title: 'IEEE SPAC 2018',
+            img: 'SPAC2018PagePhoto-01.png',
+            date: ' Thursday, January 25, 2018',
+            time: '5:30 PM - 10:00 PM',
+            location: 'The Hellenic Centre - 1315 Prince of Wales Drive, Ottawa, Ontario K2C 1N2',
+            body: [
+                {
+                    tag: 'p',
+                    text: `The IEEE Student Professional Awareness Conference (SPAC) is a formal dinner (buffet) event that aims to bridge the gap between students and their career goals. SPAC offers students and employers a unique opportunity to network with one another in a professional environment along with professionals and academics representing the fields of Engineering and Computer Science.`
+                },
+                {
+                    tag: 'p',
+                    text: 'Some of our patrons that are attending include: CSE, Accenture, Bell, You.i TV, Deloitte, DRDC, Lumenera and Real Decoy.'
+                },
+                {
+                    tag: 'p',
+                    text: `We have three types of tickets. The Early Bird Ticket is $25.00, which saves you $5 from the student ticket. The Student Ticket is $30.00 and the Professional Ticket is $50.00.`
+                },
+                {
+                    tag: 'p',
+                    text: `Both will be available at your IEEE student branch office and on the website at ieeespac.ca.`
+                },
+                {
+                    tag: 'p',
+                    text: `Before and after the dinner we will have professional guest speakers, each from a company you will able to network with during the event. For more information about the speakers and schedule, visit the website at ieeespac.ca`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `Hope to see you at SPAC 2018!`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'http://ieeespac.ca/'
+                    },
+                    text: 'Visit the SPAC Website'
+                },
+            ]
+        },
+        {
+            title: 'Diversity in Engineering & Technology Evening',
+            img: 'Diversity-in-Engineering-Graphic.jpg',
+            date: ' Tuesday, January 23, 2018',
+            time: '6:00 PM - 8:00 PM',
+            location: 'Richcraft Hall, Carleton University',
+            body: [
+                {
+                    tag: 'p',
+                    text: `As part of an exciting week of career-related programming, Co-op and Career Services and our presenting sponsor, Ericsson, are pleased to host a networking evening celebrating Diversity in Engineering and Technology!`
+                },
+                {
+                    tag: 'p',
+                    text: `This event will allow co-op students and new graduates from underrepresented groups to meet and mingle with employers from engineering and IT-related firms who are dedicated to creating a diverse workforce. Meet representatives from a range of engineering and IT firms and hear about their exciting plans for the future and their commitment to inclusion and diversity.`
+                },
+                {
+                    tag: 'p',
+                    text: 'Do not forget to bring your resumes and/or networking cards!'
+                },
+                {
+                    tag: 'p',
+                    text: 'Thank you to our Supporting Sponsor, Amdocs!'
+                },
+                {
+                    tag: 'p',
+                    text: 'This event is particularly relevant for students and new graduates under-represented in engineering and technological fields; however, any co-op students or students approaching graduation are welcome to attend.'
+                },
+                {
+                    tag: 'p',
+                    text: `Reminder: The following day (Jan. 24) is Co-op and Career Services Summer Job Fair! Make sure you stop by the UC Galleria from 10 a.m. to 3 p.m.and check it out!`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `We hope you can make it! Make sure you RSVP through Carleton's MySuccess.`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://carleton.ca/career/mysuccess/'
+                    },
+                    text: 'RSVP via MySuccess'
+                },
+            ]
+        },
+        {
+            title: 'PCB Design Workshop #1',
+            img: 'PCB-Design-Workshop-Banner-01.png',
+            date: 'Sunday, January 28, 2018',
+            time: '10:00 AM - 5:00 PM',
+            location: 'Richcraft Hall, Carleton University',
+            body: [
+                {
+                    tag: 'p',
+                    text: `IEEE Carleton is hosting a EAGLE PCB Design Workshop! Sign up at ME3359! Space is limited!`
+                },
+                {
+                    tag: 'p',
+                    text: `The heart of the board we will be interfacing is the ATMega32U4 chip: http://www.microchip.com/wwwproducts/en/ATmega32u4`
+                },
+                {
+                    tag: 'p',
+                    text: 'The workshop will be split into three sessions.'
+                },
+                {
+                    tag: 'p',
+                    text: 'Session 1 PCB Design'
+                },
+                {
+                    tag: 'p',
+                    text: 'Session 2 Soldering and Population'
+                },
+                {
+                    tag: 'p',
+                    text: `Session 3 Programming your ATmega.`
+                },
+                {
+                    tag: 'p',
+                    text: `By the end of this workshop series, you will be able to:
+                        - Use EAGLE PCB Design to add, connect, and verify parts together
+                        - Get your own board designed and manufactured
+                        - Solder both surface-mount and through-hole components on to your baord
+                        - Learn the process of loading programs on to your board. 
+                        - Test and run programs on your board`
+                },
+                {
+                    tag: 'p',
+                    text: `Payment will be collected on the first session. This workshop will cost $25 per person and $20 If you are an IEEE member (Your membership number will be checked during payment).`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `We hope you can make this workshop series - starting with this intro workshop!`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/394109351032972/'
+                    },
+                    text: 'Facebook Event Page'
+                },
+            ]
+        },
+        {
+            title: 'Stay Late and Make Workspace',
+            img: 'slam.jpg',
+            date: 'Every Thursday',
+            time: '6:30 PM - 9:30 PM',
+            location: 'ME 4463, Carleton University',
+            body: [
+                {
+                    tag: 'p',
+                    text: `Have the perfect big idea, but never knew where to get started?`
+
+                },
+                {
+                    tag: 'p',
+                    text: `Want to play with new hardware, learn technologies, and experiment with hacking?`
+                },
+                 {
+                    tag: 'p',
+                    text: `If you said yes to any of the above, this is the place for you! Stay Late and Make is an initiative by IEEE Carleton University to help you work on your personal projects. Our focus is on the tech to make your idea tick, and we're working on giving you the tools, funding, and the mentorship to make it happen.`
+                },
+                 {
+                    tag: 'p',
+                    text: `Come join us every Thursday starting January 18th from 5:30 PM - 9:30 PM in ME4463 at Carleton University!`
+                },
+            ]
+        },
+        {
+            title: 'Be a part of our organizing teams!',
+            img: 'branch_exec_team.jpg',
+            body: [
+                {
+                    tag: 'p',
+                    text: `We all volunteer our time and efforts to provide students and professionals multiple avenues to expose
+                    and drive innovation in the technical world.`
+                },
+                {
+                    tag: 'p',
+                    text: `Interested in being part of the team? This opportunity allows you to experiement and improve you
+                    technical, communication, management, and leadership skills, receive credits in Carleton's Co-Curricular
+                    Record (CCR), and enhance your resume when the accomplishments you make at IEEE.`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `Come by our office (ME 3359) or email us at info@ieeecarleton.ca`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `Our next Executive meetings are held on the following days in Mackenzie 4359`
+                },
+                {
+                    tag: 'p',
+                    text: `Wednesday, Jan 31st - 6:00 PM - 7:30 PM`
+                },
+                {
+                    tag: 'p',
+                    text: `Monday, Feb 12th - 6:00 PM - 7:30 PM`
+                },
+                {
+                    tag: 'p',
+                    text: `Wednesday, Feb 28th - 6:00 PM - 7:30 PM`
+                },
+                {
+                    tag: 'p',
+                    text: `Monday, Mar 12th - 6:00 PM - 7:30 PM`
+                },
+                 {
+                    tag: 'p',
+                    text: `Wednesday, Mar 28th - 6:00 PM - 7:30 PM`
+                },
+                {
+                    tag: 'p',
+                    text: `Monday, Apr 9th - 6:00 PM - 7:30 PM`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://docs.google.com/forms/d/e/1FAIpQLSe-JLKZBZn-krBqnAteq_C6ii852dHvZqEpFcXnE0ZYHcE52w/viewform/',
+                    },
+                    text: 'Mailing List Sign Up'
+                },
+
+
+            ]
+        },
+
+    ]
+}
