160453db0a9fc3906b271948f8a68001af900271
virajdave <virajdave@cmail.carleton.ca>
2018-01-28 12:30:28 -0500

    Jan 29 Edition

---
+++ models/newsletters/2018-01-29.js
@@ -0,0 +1,183 @@
+module.exports = {
+    toast: 'Keeping up with IEEE, a weekly newsletter',
+    articles: [
+        {
+            title: 'IEEE Carleton Executive Meeting',
+            date: 'Wednesday, January 31, 2018',
+            time: '6:00 PM - 7:30 PM',
+            location: 'ME 4359, Carleton University',
+            body: [
+                {
+                    tag: 'p',
+                    text: `Your IEEE Carleton Student branch has a general meeting today to provide updates on all events and activity within IEEE Carleton. Feel free to join us in ME 4359 for our meeting.`
+                },
+                {
+                    tag: 'p',
+                    text: `If you cannot make the meeting in person, but would like to contribute, feel free to join us via our Google Hangouts link.`
+                },
+                {
+                    tag: 'a',
+                    attr: {
+                        href: 'https://hangouts.google.com/hangouts/_/ieeecarleton.ca/ieee-general'
+                    },
+                    text: `IEEE Carleton Hangouts Meeting Link`
+                },
+            ]
+        },
+        {
+            title: 'Software Technical Interview Workshop',
+            img: 'tech_interview.png',
+            date: 'Wednesday, January 31, 2018',
+            time: '6:00 PM - 7:30 PM',
+            location: 'CB 2400',
+            body: [
+                {
+                    tag: 'p',
+                    text: `Hey everyone! We have a workshop coming up that will walk you through how to tackle technical interview questions. These are the same types of questions you will see in interviews with Google, Microsoft, Facebook, etc!`
+                },
+                {
+                    tag: 'p',
+                    text: `In the first half of the workshop, we will explain the general process you should follow in solving technical problems, some of the background knowledge you should have, and some of the common strategies for tackling these problems.`
+                },
+                {
+                    tag: 'p',
+                    text: 'In the second half of the workshop, a few easy, medium, and hard problems will be given to the attendees to work on.'
+                },
+                {
+                    tag: 'p',
+                    text: 'These problems are taken directly from interviews with big companies.'
+                },
+                {
+                    tag: 'p',
+                    text: 'Experienced people will also be around to help you out if you do not know how to solve a problem.'
+                },
+                {
+                    tag: 'p',
+                    text: `Basic prior programming knowledge is all you need.`
+                },
+                {
+                    tag: 'p',
+                    text: `This workshop is completely free and anyone who wants to get better at technical interview problems is welcome to attend.`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `We hope to see you there!`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://goo.gl/forms/yNShLS9YItnFP5962'
+                    },
+                    text: 'Please register here!'
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/213431922551851/'
+                    },
+                    text: 'Facebook Event Page'
+                },
+            ]
+        },
+        {
+            title: 'Stay Late and Make Workspace',
+            img: 'slam.jpg',
+            date: 'Every Thursday',
+            time: '6:30 PM - 9:30 PM',
+            location: 'ME 4463, Carleton University',
+            body: [
+                {
+                    tag: 'p',
+                    text: `Have the perfect big idea, but never knew where to get started?`
+
+                },
+                {
+                    tag: 'p',
+                    text: `Want to play with new hardware, learn technologies, and experiment with hacking?`
+                },
+                 {
+                    tag: 'p',
+                    text: `If you said yes to any of the above, this is the place for you! Stay Late and Make is an initiative by IEEE Carleton University to help you work on your personal projects. Our focus is on the tech to make your idea tick, and we're working on giving you the tools, funding, and the mentorship to make it happen.`
+                },
+                 {
+                    tag: 'p',
+                    text: `Come join us every Thursday starting January 18th from 5:30 PM - 9:30 PM in ME4463 at Carleton University!`
+                },
+            ]
+        },
+        {
+            title: 'Be a part of our organizing teams!',
+            img: 'branch_exec_team.jpg',
+            body: [
+                {
+                    tag: 'p',
+                    text: `We all volunteer our time and efforts to provide students and professionals multiple avenues to expose
+                    and drive innovation in the technical world.`
+                },
+                {
+                    tag: 'p',
+                    text: `Interested in being part of the team? This opportunity allows you to experiment and improve you
+                    technical, communication, management, and leadership skills, receive credits in Carleton's Co-Curricular
+                    Record (CCR), and enhance your resume when the accomplishments you make at IEEE.`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `Come by our office (ME 3359) or email us at info@ieeecarleton.ca`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `Our next Executive meetings are held on the following days in Mackenzie 4359`
+                },
+                {
+                    tag: 'p',
+                    text: `Wednesday, Jan 31st - 6:00 PM - 7:30 PM`
+                },
+                {
+                    tag: 'p',
+                    text: `Monday, Feb 12th - 6:00 PM - 7:30 PM`
+                },
+                {
+                    tag: 'p',
+                    text: `Wednesday, Feb 28th - 6:00 PM - 7:30 PM`
+                },
+                {
+                    tag: 'p',
+                    text: `Monday, Mar 12th - 6:00 PM - 7:30 PM`
+                },
+                 {
+                    tag: 'p',
+                    text: `Wednesday, Mar 28th - 6:00 PM - 7:30 PM`
+                },
+                {
+                    tag: 'p',
+                    text: `Monday, Apr 9th - 6:00 PM - 7:30 PM`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://docs.google.com/forms/d/e/1FAIpQLSe-JLKZBZn-krBqnAteq_C6ii852dHvZqEpFcXnE0ZYHcE52w/viewform/',
+                    },
+                    text: 'Mailing List Sign Up'
+                },
+
+
+            ]
+        },
+
+    ]
+}
