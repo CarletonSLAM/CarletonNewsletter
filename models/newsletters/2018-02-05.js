e3c07f051b1488ca20beab999447d02c77e00720
virajdave <virajdave@cmail.carleton.ca>
2018-02-04 13:38:00 -0500

    February 5 images

---
+++ models/newsletters/2018-02-05.js
@@ -0,0 +1,198 @@
+module.exports = {
+    toast: 'Keeping up with IEEE, a weekly newsletter',
+    articles: [
+         {
+            title: 'Seminar: Dr. John W. Chinneck',
+            img: 'talk_john.jpg',
+            date: 'Monday, February 5, 2018',
+            time: '6:00 PM - 7:30 PM',
+            location: 'ME 4359',
+            body: [
+                {
+                    tag: 'p',
+                    text: `Join us on February 5th in ME 4359 to hear Dr. Chinneck deliver a seminar about Optimization.`
+                },
+                {
+                    tag: 'p',
+                    text: `Title of the Seminar: What's the Best Way? Research on Optimization.`
+                },
+                {
+                    tag: 'p',
+                    text: 'Food and refreshments will be provided.'
+                },
+                {
+                    tag: 'p',
+                    text: 'Excerpt from his website: Professor Chinneck conducts research in the general area of applied optimization, including various applications in engineering. He has a special interest in the development of computer tools that assist in the formulation and analysis of very large and complex optimization models.'
+                },
+                {
+                    tag: 'p',
+                    text: 'His algorithms for the analysis of infeasible linear programs have been incorporated in many commercial linear programming solvers, including LINDO, CPLEX, XPRESS-MP, Frontline Systems Excel solvers, and IBMs OSL.'
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/332309483941025/'
+                    },
+                    text: 'Facebook Event Page'
+                },
+            ]
+        },
+        {
+            title: 'Machine Learning Workshop Part 1',
+            img: 'Machine-Learning-Workshop-Banner.png',
+            date: 'Tuesday, February 5, 2018',
+            time: '6:00 PM - 8:00 PM',
+            location: 'CB 5109',
+            body: [
+                {
+                    tag: 'p',
+                    text: `An informative workshop surrounding the topic of Machine Learning. An emerging major field of study and design in computing. This workshop will explore and introduce some of the theory and methods behind machine learning through an interactive learning environment. Definitely one you don't want to miss! This is a three part workshop.`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `We hope to see you there!`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/823725497814804'
+                    },
+                    text: 'Facebook Event Page'
+                },
+            ]
+        },
+        {
+            title: 'How to GIT Gud Workshop',
+            img: 'Git Gud-Workshop-01.png',
+            date: 'Wednesday, February 7, 2018',
+            time: '6:00 PM - 7:30 PM',
+            location: 'AP 340',
+            body: [
+                {
+                    tag: 'p',
+                    text: `Learning how to use GIT, basic skills and use.`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/561274117566183/'
+                    },
+                    text: 'Facebook Event Page'
+                },
+            ]
+        },
+        {
+            title: 'IEEE Carleton Survey',
+            date: 'Due on February 24, 2018',
+            body: [
+                {
+                    tag: 'p',
+                    text: `This survey is just to get a general view on what volunteers and execs think of IEEE. It is completely anonymous, so please be honest. It shouldn't take more than 10 minutes to complete. Survey closes on February 24, 2018.`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://goo.gl/forms/fl1DYaAMxBeXxSGA3/'
+                    },
+                    text: 'Survey Link'
+                },
+            ]
+        },
+        {
+            title: 'Stay Late and Make Workspace',
+            img: 'slam.jpg',
+            date: 'Every Thursday',
+            time: '6:30 PM - 9:30 PM',
+            location: 'ME 4463, Carleton University',
+            body: [
+                {
+                    tag: 'p',
+                    text: `Have the perfect big idea, but never knew where to get started?`
+
+                },
+                {
+                    tag: 'p',
+                    text: `Want to play with new hardware, learn technologies, and experiment with hacking?`
+                },
+                 {
+                    tag: 'p',
+                    text: `If you said yes to any of the above, this is the place for you! Stay Late and Make is an initiative by IEEE Carleton University to help you work on your personal projects. Our focus is on the tech to make your idea tick, and we're working on giving you the tools, funding, and the mentorship to make it happen.`
+                },
+                 {
+                    tag: 'p',
+                    text: `Come join us every Thursday starting January 18th from 5:30 PM - 9:30 PM in ME4463 at Carleton University!`
+                },
+            ]
+        },
+        {
+            title: 'Be a part of our organizing teams!',
+            img: 'branch_exec_team.jpg',
+            body: [
+                {
+                    tag: 'p',
+                    text: `We all volunteer our time and efforts to provide students and professionals multiple avenues to expose
+                    and drive innovation in the technical world.`
+                },
+                {
+                    tag: 'p',
+                    text: `Interested in being part of the team? This opportunity allows you to experiment and improve you
+                    technical, communication, management, and leadership skills, receive credits in Carleton's Co-Curricular
+                    Record (CCR), and enhance your resume when the accomplishments you make at IEEE.`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `Come by our office (ME 3359) or email us at info@ieeecarleton.ca`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `Our next Executive meetings are held on the following days in Mackenzie 4359`
+                },
+                {
+                    tag: 'p',
+                    text: `Monday, Feb 12th - 6:00 PM - 7:30 PM`
+                },
+                {
+                    tag: 'p',
+                    text: `Wednesday, Feb 28th - 6:00 PM - 7:30 PM`
+                },
+                {
+                    tag: 'p',
+                    text: `Monday, Mar 12th - 6:00 PM - 7:30 PM`
+                },
+                 {
+                    tag: 'p',
+                    text: `Wednesday, Mar 28th - 6:00 PM - 7:30 PM`
+                },
+                {
+                    tag: 'p',
+                    text: `Monday, Apr 9th - 6:00 PM - 7:30 PM`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://docs.google.com/forms/d/e/1FAIpQLSe-JLKZBZn-krBqnAteq_C6ii852dHvZqEpFcXnE0ZYHcE52w/viewform/',
+                    },
+                    text: 'Mailing List Sign Up'
+                },
+
+
+            ]
+        },
+
+    ]
+}
