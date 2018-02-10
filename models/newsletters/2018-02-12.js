59c4d006ff740a2786bf0878567b14b8b6665a58
virajdave <virajdave@cmail.carleton.ca>
2018-02-10 15:22:22 -0500

    February 12th Newsletter

---
+++ models/newsletters/2018-02-12.js
@@ -0,0 +1,240 @@
+module.exports = {
+    toast: 'Keeping up with IEEE, a weekly newsletter',
+    articles: [
+         {
+            title: 'IEEE Carleton Executive Meeting',
+            date: 'Monday, February 12, 2018',
+            time: '6:00 PM - 7:30 PM',
+            location: 'ME 4359, Carleton University',
+            body: [
+                {
+                    tag: 'p',
+                    text: `Your IEEE Carleton Student branch has a general meeting today to provide updates on all events and activity within IEEE Carleton. Feel free to join us in ME 4359 for our meeting.`
+                },
+                {
+                    tag: 'p',
+                    text: `If you cannot make the meeting in person, but would like to contribute, feel free to join us via our Google Hangouts link.`
+                },
+                {
+                    tag: 'a',
+                    attr: {
+                        href: 'https://hangouts.google.com/hangouts/_/ieeecarleton.ca/ieee-general'
+                    },
+                    text: `IEEE Carleton Hangouts Meeting Link`
+                },
+            ]
+        },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/332309483941025/'
+                    },
+                    text: 'Facebook Event Page'
+                },
+            ]
+        },
+        {
+            title: 'Machine Learning Workshop Part 2',
+            img: 'Machine-Learning-Workshop-Banner.png',
+            date: 'Tuesday, February 13, 2018',
+            time: '6:00 PM - 8:00 PM',
+            location: 'CB 5109',
+            body: [
+                {
+                    tag: 'p',
+                    text: `An informative workshop surrounding the topic of Machine Learning. An emerging major field of study and design in computing. This workshop will explore and introduce some of the theory and methods behind machine learning through an interactive learning environment. Definitely one you don't want to miss! This is a three part workshop.`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `We hope to see you there!`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/823725497814804'
+                    },
+                    text: 'Facebook Event Page'
+                },
+            ]
+        },
+        {
+            title: 'Latex Workshop',
+            date: 'Wednesday, February 14, 2018',
+            time: '6:00 PM - 7:30 PM',
+            location: 'TBD',
+            body: [
+                {
+                    tag: 'p',
+                    text: `Come learn LaTeX and how powerful this program is.`
+                },
+            ]
+        },
+    {
+            title: 'PCB Design Workshop #2',
+            img: 'pcb.png',
+            date: 'Sunday, February 18, 2018',
+            time: '10:00 AM - 5:00 PM',
+            location: 'ME 4463',
+            body: [
+                {
+                    tag: 'p',
+                    text: `The heart of the board we will be interfacing is the ATMega32U4 chip: http://www.microchip.com/wwwproducts/en/ATmega32u4`
+                },
+                {
+                    tag: 'p',
+                    text: 'The workshop will be split into three sessions.'
+                },
+                {
+                    tag: 'p',
+                    text: 'Session 2 Soldering and Population'
+                },
+                {
+                    tag: 'p',
+                    text: `Session 3 Programming your ATmega.`
+                },
+                {
+                    tag: 'p',
+                    text: `By the end of this workshop series, you will be able to:
+                        - Use EAGLE PCB Design to add, connect, and verify parts together
+                        - Get your own board designed and manufactured
+                        - Solder both surface-mount and through-hole components on to your board
+                        - Learn the process of loading programs on to your board. 
+                        - Test and run programs on your board`
+                },
+                {
+                    tag: 'p',
+                    text: `Payment will be collected on the first session. This workshop will cost $25 per person and $20 If you are an IEEE member (Your membership number will be checked during payment).`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `We hope you can make this workshop series!`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/394109351032972/'
+                    },
+                    text: 'Facebook Event Page'
+                },
+            ]
+        },
+        {
+            title: 'IEEE Carleton Survey',
+            date: 'Due on February 24, 2018',
+            body: [
+                {
+                    tag: 'p',
+                    text: `This survey is just to get a general view on what volunteers and execs think of IEEE. It is completely anonymous, so please be honest. It shouldn't take more than 10 minutes to complete. Survey closes on February 24, 2018.`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://goo.gl/forms/fl1DYaAMxBeXxSGA3'
+                    },
+                    text: 'Survey Link'
+                },
+            ]
+        },
+        {
+            title: 'Stay Late and Make Workspace',
+            img: 'slam.jpg',
+            date: 'Every Thursday',
+            time: '6:30 PM - 9:30 PM',
+            location: 'ME 4463, Carleton University',
+            body: [
+                {
+                    tag: 'p',
+                    text: `Have the perfect big idea, but never knew where to get started?`
+
+                },
+                {
+                    tag: 'p',
+                    text: `Want to play with new hardware, learn technologies, and experiment with hacking?`
+                },
+                 {
+                    tag: 'p',
+                    text: `If you said yes to any of the above, this is the place for you! Stay Late and Make is an initiative by IEEE Carleton University to help you work on your personal projects. Our focus is on the tech to make your idea tick, and we're working on giving you the tools, funding, and the mentorship to make it happen.`
+                },
+                 {
+                    tag: 'p',
+                    text: `Come join us every Thursday starting January 18th from 5:30 PM - 9:30 PM in ME4463 at Carleton University!`
+                },
+            ]
+        },
+        {
+            title: 'Be a part of our organizing teams!',
+            img: 'branch_exec_team.jpg',
+            body: [
+                {
+                    tag: 'p',
+                    text: `We all volunteer our time and efforts to provide students and professionals multiple avenues to expose
+                    and drive innovation in the technical world.`
+                },
+                {
+                    tag: 'p',
+                    text: `Interested in being part of the team? This opportunity allows you to experiment and improve you
+                    technical, communication, management, and leadership skills, receive credits in Carleton's Co-Curricular
+                    Record (CCR), and enhance your resume when the accomplishments you make at IEEE.`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `Come by our office (ME 3359) or email us at info@ieeecarleton.ca`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `Our next Executive meetings are held on the following days in Mackenzie 4359`
+                },
+                {
+                    tag: 'p',
+                    text: `Monday, Feb 12th - 6:00 PM - 7:30 PM`
+                },
+                {
+                    tag: 'p',
+                    text: `Wednesday, Feb 28th - 6:00 PM - 7:30 PM`
+                },
+                {
+                    tag: 'p',
+                    text: `Monday, Mar 12th - 6:00 PM - 7:30 PM`
+                },
+                 {
+                    tag: 'p',
+                    text: `Wednesday, Mar 28th - 6:00 PM - 7:30 PM`
+                },
+                {
+                    tag: 'p',
+                    text: `Monday, Apr 4th - 6:00 PM - 7:30 PM`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://docs.google.com/forms/d/e/1FAIpQLSe-JLKZBZn-krBqnAteq_C6ii852dHvZqEpFcXnE0ZYHcE52w/viewform/',
+                    },
+                    text: 'Mailing List Sign Up'
+                },
+
+
+            ]
+        },
+
+    ]
+}
