ec8678dbb0310b17048e592e96012b4014293d29
virajdave <virajdave@cmail.carleton.ca>
2018-02-20 19:36:39 -0500

    Git images for Feb 20 newsletter

---
+++ models/newsletters/2018-02-19.js
@@ -0,0 +1,193 @@
+module.exports = {
+    toast: 'Keeping up with IEEE, a weekly newsletter',
+    articles: [
+         {
+            title: '#HIP613: Healthcare Design Thinking Workshop Powered by IBM',
+            img: 'hackhealth.png',
+            date: 'Wednesday, February 21, 2018',
+            time: '6:00 PM - 9:30 PM',
+            location: 'IBM Design Studio, 10th Floor, IBM Ottawa Lab (3755 Riverside Drive)',
+            body: [
+                {
+                    tag: 'p',
+                    text: `Get yourself ready for the Hacking Health Ottawa's #HIP613 Hackathon! This workshop is the first step in solving your healthcare problem.`
+                },
+                {
+                    tag: 'p',
+                    text: `Bring your problem, and learn how to apply a human-centred design mindset to understand the present, and envision the future in a continuous cycle of observing, reflecting and making. Tickets are limited, get your tickets now!`
+                },
+                {
+                    tag: 'p',
+                    text: `Get your problem noticed when you pitch your problem at any of our workshops! If you are interested, contact us at ottawa@hackinghealth.ca`
+                },
+                {
+                    tag: 'p',
+                    text: `Don’t forget to create your Sparkboard account to post your healthcare project or join a team. Here's how to get started: 
+
+                            1. Sign up on Sparkboard
+                            2. Create your Sparkboard account with profile and picture
+                            3. Click on "New" to create your project. Describe the issue, your solution and the team you're looking for  - or join an existing project
+                            4. Don't forget to upload an image
+                            5. Share your project idea on social media to recruit teammates, using the hashtag #HIP613`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://hackinghealth.us3.list-manage.com/track/click?u=56d5182abb14eb9f4d1bb187c&id=8fa9bd05ae&e=7320090641'
+                    },
+                    text: 'Get Your Tickets'
+                },
+            ]
+        },
+        {
+            title: 'Shopify Industry Tour 2018',
+            img: 'Shopify Tour Poster.png',
+            date: 'Monday, March 5, 2018',
+            time: '3:00 PM - 4:00 PM',
+            location: '150 Elgin Street',
+            body: [
+                {
+                    tag: 'p',
+                    text: `IEEE Carleton is pleased to give you the opportunity to get an inside look at one of the most popular establishments in the business world, Shopify.`
+                },
+                {
+                    tag: 'p',
+                    text: `Shopify stakes its claim at creating a platform for business owners to market their products and organize their merchandise to better suit their needs. Shopify helps businesses to manage things such as taking care of customer orders, selling online and in retail stores.`
+                },
+                {
+                    tag: 'p',
+                    text: `Join the IEEE Carleton's tour at Shopify at 3:00pm on Monday March 5th. The tour is to feature a walk through of Shopify's office, which is unique even compared to the most modern of offices.`
+                },
+                {
+                    tag: 'p',
+                    text: `Register for the tour at the IEEE Carleton office, ME 3359. A $5 fee will be required and the first 15 participants will be allowed to go. Cutoff date for entry is Sunday March 4th.`
+                },
+                {
+                    tag: 'p',
+                    text: `Transportation: OC Transpo bus 4 to Rideau.`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `Don't miss this special opportunity to visit a growing company in Ottawa and in North America!`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/220354555194315/'
+                    },
+                    text: 'Facebook Event Page'
+                },
+                
+            ]
+        },
+        {
+            title: 'IEEE Carleton Survey',
+            date: 'Due on February 24, 2018',
+            body: [
+                {
+                    tag: 'p',
+                    text: `This survey is just to get a general view on what volunteers and execs think of IEEE. It is completely anonymous, so please be honest. It shouldn't take more than 10 minutes to complete. Survey closes on February 24, 2018.`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://goo.gl/forms/fl1DYaAMxBeXxSGA3'
+                    },
+                    text: 'Survey Link'
+                },
+            ]
+        },
+        {
+            title: 'Stay Late and Make Workspace',
+            img: 'slam.jpg',
+            date: 'Every Thursday',
+            time: '6:30 PM - 9:30 PM',
+            location: 'ME 4463, Carleton University',
+            body: [
+                {
+                    tag: 'p',
+                    text: `Have the perfect big idea, but never knew where to get started?`
+
+                },
+                {
+                    tag: 'p',
+                    text: `Want to play with new hardware, learn technologies, and experiment with hacking?`
+                },
+                 {
+                    tag: 'p',
+                    text: `If you said yes to any of the above, this is the place for you! Stay Late and Make is an initiative by IEEE Carleton University to help you work on your personal projects. Our focus is on the tech to make your idea tick, and we're working on giving you the tools, funding, and the mentorship to make it happen.`
+                },
+                 {
+                    tag: 'p',
+                    text: `Come join us every Thursday starting January 18th from 5:30 PM - 9:30 PM in ME4463 at Carleton University!`
+                },
+            ]
+        },
+        {
+            title: 'Be a part of our organizing teams!',
+            img: 'branch_exec_team.jpg',
+            body: [
+                {
+                    tag: 'p',
+                    text: `We all volunteer our time and efforts to provide students and professionals multiple avenues to expose
+                    and drive innovation in the technical world.`
+                },
+                {
+                    tag: 'p',
+                    text: `Interested in being part of the team? This opportunity allows you to experiment and improve you
+                    technical, communication, management, and leadership skills, receive credits in Carleton's Co-Curricular
+                    Record (CCR), and enhance your resume when the accomplishments you make at IEEE.`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `Come by our office (ME 3359) or email us at info@ieeecarleton.ca`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `Our next Executive meetings are held on the following days in Mackenzie 4359`
+                },
+                {
+                    tag: 'p',
+                    text: `Wednesday, Feb 28th - 6:00 PM - 7:30 PM`
+                },
+                {
+                    tag: 'p',
+                    text: `Monday, Mar 12th - 6:00 PM - 7:30 PM`
+                },
+                 {
+                    tag: 'p',
+                    text: `Wednesday, Mar 28th - 6:00 PM - 7:30 PM`
+                },
+                {
+                    tag: 'p',
+                    text: `Monday, Apr 4th - 6:00 PM - 7:30 PM`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://docs.google.com/forms/d/e/1FAIpQLSe-JLKZBZn-krBqnAteq_C6ii852dHvZqEpFcXnE0ZYHcE52w/viewform/',
+                    },
+                    text: 'Mailing List Sign Up'
+                },
+
+
+            ]
+        },
+
+    ]
+}
