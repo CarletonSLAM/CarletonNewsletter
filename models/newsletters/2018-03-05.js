01814316a1fbe7a4ebabacdf0288a194cf9d2cd2
virajdave <virajdave@cmail.carleton.ca>
2018-03-03 13:22:20 -0500

    March 3rd Newsletter

---
+++ models/newsletters/2018-03-05.js
@@ -0,0 +1,243 @@
+module.exports = {
+    toast: 'Keeping up with IEEE, a weekly newsletter',
+    articles: [
+        {
+            title: 'Shopify Industry Tour 2018',
+            img: 'Shopify.jpg',
+            date: 'Monday, March 5, 2018',
+            time: '3:00 PM - 4:00 PM',
+            location: '150 Elgin Street',
+            body: [
+                {
+                    tag: 'p',
+                    text: `IEEE Carleton is pleased to give you the opportunity to get an inside look at one of the most popular establishments in the business world, Shopify.`
+                },
+                {
+                    tag: 'p',
+                    text: `Shopify stakes its claim at creating a platform for business owners to market their products and organize their merchandise to better suit their needs. Shopify helps businesses to manage things such as taking care of customer orders, selling online and in retail stores.`
+                },
+                {
+                    tag: 'p',
+                    text: `Join the IEEE Carleton's tour at Shopify at 3:00pm on Monday March 5th. The tour is to feature a walk through of Shopify's office, which is unique even compared to the most modern of offices.`
+                },
+                {
+                    tag: 'p',
+                    text: `Register for the tour at the IEEE Carleton office, ME 3359. A $5 fee will be required and the first 15 participants will be allowed to go. Cutoff date for entry is Sunday March 4th.`
+                },
+                {
+                    tag: 'p',
+                    text: `Transportation: OC Transpo bus 4 to Rideau.`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `Don't miss this special opportunity to visit a growing company in Ottawa and in North America!`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/220354555194315/'
+                    },
+                    text: 'Facebook Event Page'
+                },
+                
+            ]
+        },
+        {
+            title: 'Machine Learning Workshop Part 3',
+            img: 'Machine-Learning-Workshop-Banner.png',
+            date: 'Tuesday, March 6, 2018',
+            time: '6:00 PM - 8:00 PM',
+            location: 'CB 5109',
+            body: [
+                {
+                    tag: 'p',
+                    text: `An informative workshop surrounding the topic of Machine Learning. An emerging major field of study and design in computing. This workshop will explore and introduce some of the theory and methods behind machine learning through an interactive learning environment. Definitely one you don't want to miss! This is a three part workshop.`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `We hope to see you there!`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/823725497814804'
+                    },
+                    text: 'Facebook Event Page'
+                },
+            ]
+         },
+        {
+            title: '9th Annual WIE Wine & Cheese',
+            img: 'WIE.jpg',
+            date: 'Wednesday, March 7, 2018',
+            time: '6:30 PM - 9:30 PM',
+            location: 'Lago Bar Grill View - 1001 Queen Elizabeth Driveway, Ottawa, Ontario K1S 5K7',
+            body: [
+                {
+                    tag: 'p',
+                    text: `The Annual IEEE WIE Wine and Cheese is hosted by the IEEE Women in Engineering uOttawa and CarletonU Student Branches. It is a networking event dedicated to connect engineering students from all fields with industry professionals and academia.`
+                },
+                {
+                    tag: 'p',
+                    text: `Get your tickets online or visit your IEEE student branch office to purchase with cash! Each ticket comes with a free drink ticket (remember your ID).
+                    ($7 for IEEE Students, $10 for Regular Students) 
+                    uOttawa – SITE 4026
+                    CarletonU – ME 3349.`
+                },
+                {
+                    tag: 'p',
+                    text: `Hey engineers! Are you looking to improve your networking skills and/or build your global network with industry professionals and professors? Well here's your chance!`
+                },
+                {
+                    tag: 'p',
+                    text: `The 9th Annual IEEE Women in Engineering Wine and Cheese provides students an opportunity to build meaningful connections with professors and industry professionals from all across Ottawa. With an evening filled of interesting speakers, ice-breakers, and of course wine and cheese, there's no better time and place to start networking!`
+                },
+                {
+                    tag: 'p',
+                    text: `This is a great opportunity for organizations to meaningfully connect and communicate with more than 100 engineering students and also academics and other industry professionals. Employers will have the chance to meet in person the next wave of innovators and problem solvers. Expect to see many students from co-op and recent/soon-to-be graduates looking to find out more about you and your work!`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `For more information, please visit our website at celebratewie.ca, or email us at celebratewie@gmail.com`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/339249453239712/'
+                    },
+                    text: 'Facebook Event Page'
+                },
+            ]
+        },
+        {
+            title: 'IEEE Carleton Elections',
+            date: 'Friday, March 9th, 2018',
+            time: 'End of Day',
+            location: 'ME 4463, Carleton University',
+            body: [
+                {
+                    tag: 'p',
+                    text: `This is a reminder to those who are interested in running for an IEEE Carleton exec position for next year, that you must fill out a nomination form and obtain at least 10 signatures by March 9. Copies of the form can be found in the IEEE office. The form has a field for an IEEE number for the candidate, but if you are currently a volunteer and don't have an IEEE membership, don't worry about filling that part out. However, keep in mind that if you are officially elected as an exec, you must purchase this membership.
+`
+
+                },
+                {
+                    tag: 'p',
+                    text: `At the bottom is a link to the position handbook if you want to know the responsibilities of each position`
+                },
+                {
+                    tag: 'p',
+                    text: `If you said yes to any of the above, this is the place for you! Stay Late and Make is an initiative by IEEE Carleton University to help you work on your personal projects. Our focus is on the tech to make your idea tick, and we're working on giving you the tools, funding, and the mentorship to make it happen.`
+                },
+                {
+                    tag: 'p',
+                    text: `Please email covicechair@ieeecarleton.ca for any questions!`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://drive.google.com/open?id=1jQ8LjpPgVj9EbbHLL6Z7kYBX7GYJJhfM'
+                    },
+                    text: 'IEEE Executive Position Desciptions'
+                },
+            ]
+        },
+        {
+            title: 'Stay Late and Make Workspace',
+            img: 'slam.jpg',
+            date: 'Every Thursday',
+            time: '6:30 PM - 9:30 PM',
+            location: 'ME 4463, Carleton University',
+            body: [
+                {
+                    tag: 'p',
+                    text: `Have the perfect big idea, but never knew where to get started?`
+
+                },
+                {
+                    tag: 'p',
+                    text: `Want to play with new hardware, learn technologies, and experiment with hacking?`
+                },
+                 {
+                    tag: 'p',
+                    text: `If you said yes to any of the above, this is the place for you! Stay Late and Make is an initiative by IEEE Carleton University to help you work on your personal projects. Our focus is on the tech to make your idea tick, and we're working on giving you the tools, funding, and the mentorship to make it happen.`
+                },
+                 {
+                    tag: 'p',
+                    text: `Come join us every Thursday starting January 18th from 5:30 PM - 9:30 PM in ME4463 at Carleton University!`
+                },
+            ]
+        },
+        {
+            title: 'Be a part of our organizing teams!',
+            img: 'branch_exec_team.jpg',
+            body: [
+                {
+                    tag: 'p',
+                    text: `We all volunteer our time and efforts to provide students and professionals multiple avenues to expose
+                    and drive innovation in the technical world.`
+                },
+                {
+                    tag: 'p',
+                    text: `Interested in being part of the team? This opportunity allows you to experiment and improve you
+                    technical, communication, management, and leadership skills, receive credits in Carleton's Co-Curricular
+                    Record (CCR), and enhance your resume when the accomplishments you make at IEEE.`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `Come by our office (ME 3359) or email us at info@ieeecarleton.ca`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `Our next Executive meetings are held on the following days in Mackenzie 4359`
+                },
+                {
+                    tag: 'p',
+                    text: `Monday, Mar 12th - 6:00 PM - 7:30 PM`
+                },
+                 {
+                    tag: 'p',
+                    text: `Wednesday, Mar 28th - 6:00 PM - 7:30 PM`
+                },
+                {
+                    tag: 'p',
+                    text: `Monday, Apr 4th - 6:00 PM - 7:30 PM`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://docs.google.com/forms/d/e/1FAIpQLSe-JLKZBZn-krBqnAteq_C6ii852dHvZqEpFcXnE0ZYHcE52w/viewform/',
+                    },
+                    text: 'Mailing List Sign Up'
+                },
+
+            ]
+        },
+        
+    ]
+}
