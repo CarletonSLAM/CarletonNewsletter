0e12a4ffce7a2ea90069cd3a1ad4db773e75f118
virajdave <virajdave@cmail.carleton.ca>
2018-03-24 13:46:30 -0400

    Images for March 26 newsletter

---
+++ models/newsletters/2018-03-26.js
@@ -0,0 +1,217 @@
+module.exports = {
+    toast: 'Keeping up with IEEE, a weekly newsletter',
+    articles: [
+         {
+            title: 'Technical Interview Workshop 2',
+            img: 'techinterview2.jpg',
+            date: 'Monday, March 26, 2018',
+            time: '6:00 PM - 8:00 PM',
+            location: 'ME 4463 (Maker Lab)',
+            body: [
+                {
+                    tag: 'p',
+                    text: `Hey everyone! We have a workshop coming up that will walk you through how to tackle technical interview questions. These are the same types of questions you will see in interviews with Google, Microsoft, Facebook, etc`
+                },
+                {
+                    tag: 'p',
+                    text: `In the first half of the workshop, we will explain some basic algorithms used in solving coding problems. We will explain each of the algorithms and walk through an example for each of them.`
+                },
+                {
+                    tag: 'p',
+                    text: `In the second half of the workshop, a few easy, medium, and hard problems will be given to the attendees to work on. These problems are taken directly from interviews with big companies. Experienced people will also be around to help you out if you don't know how to solve a problem.`
+                },
+                {
+                    tag: 'p',
+                    text: `Basic prior programming knowledge is all you need. This workshop is completely free and anyone who wants to get better at technical interview problems is welcome to attend.`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `We hope to see you there!`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/2065334820379926/'
+                    },
+                    text: 'Facebook Event Page'
+                },
+            ]
+        },
+        {
+            title: 'Beginner iOS Workshop',
+            img: 'iosworkshop.jpg',
+            date: 'Tuesday, March 27, 2018',
+            time: '6:00 PM - 8:00 PM',
+            location: 'ME 4463 (Maker Lab)',
+            body: [
+                {
+                    tag: 'p',
+                    text: `Ever wanted to build your own mobile app? Don't know how? Don't even know where to start? Come join us and learn how to build your first iOS app`
+                },
+                {
+                    tag: 'p',
+                    text: `In this workshop you'll learn how to build and run an iOS app using Xcode [Apple's own IDE] and then how to install it on your iPhone.`
+                },
+                {
+                    tag: 'p',
+                    text: `All you need is Xcode installed [link: https://itunes.apple.com/ca/app/xcode/id497799835?mt=12] and some basic programming knowledge.`
+                },
+                {
+                    tag: 'p',
+                    text: `NOTE: You do need a Mac to install Xcode.`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `We hope to see you there!`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/1872806372763371/'
+                    },
+                    text: 'Facebook Event Page'
+                },
+            ]
+        },
+        {
+            title: 'Machine Learning Workshop Part 3',
+            img: 'Machine-Learning-Workshop-Banner.png',
+            date: 'Tuesday, March 27, 2018',
+            time: '6:00 PM - 8:00 PM',
+            location: 'CB 5109',
+            body: [
+                {
+                    tag: 'p',
+                    text: `An informative workshop surrounding the topic of Machine Learning. An emerging major field of study and design in computing. This workshop will explore and introduce some of the theory and methods behind machine learning through an interactive learning environment. Definitely one you don't want to miss! This is a three part workshop.`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `We hope to see you there!`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/823725497814804'
+                    },
+                    text: 'Facebook Event Page'
+                },
+            ]
+        },
+        {
+            title: 'IEEE Carleton General Meeting - ELECTIONS',
+            date: 'Wednesday, March 28, 2018',
+            time: '6:00 PM - 7:30 PM',
+            location: 'ME 4359, Carleton University',
+            body: [
+                {
+                    tag: 'p',
+                    text: `Your IEEE Carleton Student branch has a general meeting today to provide updates on all events and activity within IEEE Carleton. Feel free to join us in ME 4359 for our meeting. This meeting will be about the elections pertaining for next year execs of the branch.`
+                },
+                {
+                    tag: 'p',
+                    text: `If you cannot make the meeting in person, but would like to contribute, feel free to join us via our Google Hangouts link.`
+                },
+                {
+                    tag: 'a',
+                    attr: {
+                        href: 'https://hangouts.google.com/hangouts/_/ieeecarleton.ca/ieee-general'
+                    },
+                    text: `IEEE Carleton Hangouts Meeting Link`
+                },
+            ]
+        },
+        {
+            title: 'Stay Late and Make Workspace',
+            img: 'slam.jpg',
+            date: 'Every Thursday',
+            time: '6:30 PM - 9:30 PM',
+            location: 'ME 4463, Carleton University',
+            body: [
+                {
+                    tag: 'p',
+                    text: `Have the perfect big idea, but never knew where to get started?`
+
+                },
+                {
+                    tag: 'p',
+                    text: `Want to play with new hardware, learn technologies, and experiment with hacking?`
+                },
+                 {
+                    tag: 'p',
+                    text: `If you said yes to any of the above, this is the place for you! Stay Late and Make is an initiative by IEEE Carleton University to help you work on your personal projects. Our focus is on the tech to make your idea tick, and we're working on giving you the tools, funding, and the mentorship to make it happen.`
+                },
+                 {
+                    tag: 'p',
+                    text: `Come join us every Thursday starting January 18th from 5:30 PM - 9:30 PM in ME4463 at Carleton University!`
+                },
+            ]
+        },
+        {
+            title: 'Be a part of our organizing teams!',
+            img: 'branch_exec_team.jpg',
+            body: [
+                {
+                    tag: 'p',
+                    text: `We all volunteer our time and efforts to provide students and professionals multiple avenues to expose
+                    and drive innovation in the technical world.`
+                },
+                {
+                    tag: 'p',
+                    text: `Interested in being part of the team? This opportunity allows you to experiment and improve you
+                    technical, communication, management, and leadership skills, receive credits in Carleton's Co-Curricular
+                    Record (CCR), and enhance your resume when the accomplishments you make at IEEE.`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `Come by our office (ME 3359) or email us at info@ieeecarleton.ca`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `Our next Executive meetings are held on the following days in Mackenzie 4359`
+                },
+                {
+                    tag: 'p',
+                    text: `Wednesday, Mar 28th - 6:00 PM - 7:30 PM`
+                },
+                {
+                    tag: 'p',
+                    text: `Monday, Apr 4th - 6:00 PM - 7:30 PM`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://docs.google.com/forms/d/e/1FAIpQLSe-JLKZBZn-krBqnAteq_C6ii852dHvZqEpFcXnE0ZYHcE52w/viewform/',
+                    },
+                    text: 'Mailing List Sign Up'
+                },
+
+            ]
+        },
+        
+    ]
+}
