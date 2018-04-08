3cefa15b89b9ad7bc5c6ae0c333fbd1c90baf0d1
virajdave <virajdave@cmail.carleton.ca>
2018-04-08 12:28:58 -0400

    Added Cookies & Cram Photo

---
+++ models/newsletters/2018-04-09.js
@@ -0,0 +1,177 @@
+module.exports = {
+    toast: 'Keeping up with IEEE, a weekly newsletter',
+    articles: [
+        {
+            title: 'IEEE Carleton Annual General Meeting',
+            date: 'Monday, April 9, 2018',
+            time: '6:00 PM - 7:30 PM',
+            location: 'ME 4359',
+            body: [
+                {
+                    tag: 'p',
+                    text: `Join us to see the events we have done during the year, the impact we have made, and the new exec team that will be running IEEE Carleton next year!`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `We hope to see you there!`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/2003381323259482/'
+                    },
+                    text: 'Facebook Event Page'
+                },
+            ]
+        },
+        {
+            title: 'Cookies& Cram',
+            date: 'Tbursday, April 12, 2018',
+            img: 'cookies.jpg',
+            time: '9:00 AM - 4:00 PM',
+            location: 'ME 3300 Block',
+            body: [
+                {
+                    tag: 'p',
+                    text: `IEEE along with CSCE are excited to announce that we will be co-hosting Cookies and Cram once again this semester with other Carleton Engineering societies!`
+                },
+                {
+                    tag: 'p',
+                    text: `The purpose of Cookies and Cram is to provide students with FREE food and study space as a way to de-stress right before the exam period.`
+                },
+                {
+                    tag: 'p',
+                    text: `All of the classrooms in the 3300 block of ME will be booked for your convenience. Be sure to stop by the society offices in the 3300 block for food, refreshments, and help from upper year students!`
+                },
+                {
+                    tag: 'p',
+                    text: `Below is a list of the Carleton Engineering Societies participating, as well as what they will be providing:
+                        •	CSCE - Baked goods
+                        •	CMAS - Popcorn and coffee/hot chocolate
+                        •	CSES - TBD
+                        •	IEEE - Veggies
+                        •	SCESoc - Fruit
+                        •	CUBEs - TBD`
+                },
+                {
+                    tag: 'p',
+                    text: `Society office times and study rooms may vary throughout the day.`
+                },
+                {
+                    tag: 'p',
+                    text: `De-stressing Activities: Therapy dogs - CSES`
+                },
+                {
+                    tag: 'p',
+                    text: `More information to come!`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `We hope to see you there!`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/229069564322198/'
+                    },
+                    text: 'Facebook Event Page'
+                },
+            ]
+        },
+        {
+            title: 'ORC - Call for Volunteers',
+            body: [
+                {
+                    tag: 'p',
+                    text: `Arduinos, 3D printing, networking with professionals, Lego Mindstorms, Lego displays, FREE LUNCH … what more can you ask for? The IEEE Ottawa Robotics Competition (ORC) is Ottawa’s largest non-profit robotics competition that attracts 500+ attendees annually.`
+                },
+                {
+                    tag: 'p',
+                    text: `To enrich your volunteering portfolio, to network with industry professionals, to apply for future leadership positions within our organization, and to inspire the next generation of STEM leaders, sign up NOW as a volunteer at http://orc.ieeeottawa.ca/volunteering. Mark your calendars for this year’s competition, Saturday, May 26th! All you need to be a great volunteer is enthusiasm and a contagious smile. Absolutely no programming/robotics knowledge required.`
+                },
+                {
+                    tag: 'p',
+                    text: `Check out the competition at http://bit.ly/ORC2016OTTCITIZEN. Students design and build robots to compete in a day filled with exciting Lego Mindstorms and Arduino challenges. One of our most popular challenges, Sumo wrestling involves a UFC-style fight between two robots! For more information on the ORC, please visit http://orc.ieeeottawa.ca or email us at orcinfo@ieeeottawa.ca.`
+                },
+            ]
+        },
+        {
+            title: 'SPAC 2019 Lead Candidate Form',
+            body: [
+                {
+                    tag: 'p',
+                    text: `we're looking for lead organizers for SPAC2019. If you're interested, please fill out the candidate form below`
+                },
+                {
+                    tag: 'p',
+                    text: `It's a great opportunity to improve your leadership and team player skills, and it looks good on a resume!`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://goo.gl/forms/QwxVW679DmVdxEil1'
+                    },
+                    text: 'SPAC 2019 Lead Candidate Form'
+                },
+            ]
+        },
+        {
+            title: 'Be a part of our organizing teams!',
+            img: 'branch_exec_team.jpg',
+            body: [
+                {
+                    tag: 'p',
+                    text: `We all volunteer our time and efforts to provide students and professionals multiple avenues to expose
+                    and drive innovation in the technical world.`
+                },
+                {
+                    tag: 'p',
+                    text: `Interested in being part of the team? This opportunity allows you to experiment and improve you
+                    technical, communication, management, and leadership skills, receive credits in Carleton's Co-Curricular
+                    Record (CCR), and enhance your resume when the accomplishments you make at IEEE.`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `Come by our office (ME 3359) or email us at info@ieeecarleton.ca`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `Our next Executive meetings are held on the following days in Mackenzie 4359`
+                },
+                {
+                    tag: 'p',
+                    text: `Monday, Apr 9th - 6:00 PM - 7:30 PM`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://docs.google.com/forms/d/e/1FAIpQLSe-JLKZBZn-krBqnAteq_C6ii852dHvZqEpFcXnE0ZYHcE52w/viewform/',
+                    },
+                    text: 'Mailing List Sign Up'
+                },
+
+            ]
+        },
+        
+    ]
+}
