37f629efcd4dad0f6ec10c59a49377e43a8b4944
riconaranjo <federico.naranjo@gmail.com>
2018-10-08 20:10:06 -0400

    Newsletter pushing resources for build

---
+++ models/newsletters/2018-10-8.js
@@ -0,0 +1,228 @@
+module.exports = {
+    toast: [
+        {
+            tag: 'h2',
+            text: 'Keeping up with IEEE Carleton.'
+        },
+        {
+            tag: 'p',
+            text: 'Feel free to forward this newsletter to anyone you think might be interested.'
+        },
+        {
+            tag: 'htmlString',
+            text: `If you are having trouble viewing this as an email, click <a href='http://staylateandmake.ca/CarletonNewsletter/output/2018-10-8.html'>here</a> to view in browser.`
+        } // !! do not forget to change above link with date of this file !!
+    ],
+
+    articles: [
+        {
+            title: 'omen in Engineering (WIE) Meet & Greet',
+            date: 'Thursday Oct. 18, 2018',
+            time: '17:30 - 19:00',
+            location: 'MC 2014',
+            img: 'wie-banner.png',
+            body: [
+                {
+                    tag: 'p',
+                    text: `🍕🎲👩🏽‍💼 Pizza, Games and Women In Engineering.`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `Come hang out with your WIE execs and meet other girls and guys in your programs.`
+                },
+                {
+                    tag: 'p',
+                    text: `This is a Meet & Greet hosted by WIE, where we will talk about our upcoming events, have a good time playing games and listening to your ideas!`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/CUIEEEWIE/'
+                    },
+                    text: 'Facebook Page'
+                },
+            ]
+        },
+        {
+            title: 'IEEE Ottawa Section Annual General Meeting (AGM) 2018',
+            date: 'Friday Oct. 19, 2018',
+            time: '17:30 - 22:15',
+            location: 'Convention Centre Trillium Ballroom (55 Colonel By Drive)',
+            body: [
+                {
+                    tag: 'p',
+                    text: `🤔 Ever wonder what IEEE does outside of the student branches? Come to the Ottawa Section's AGM to find out more!`
+                },
+                {
+                    tag: 'p',
+                    text: `All three Ottawa student branches report to the Ottawa Section (Algonquin, uOttawa and Carleton). The AGM is a great place to meet the section's exec team as well as industry professionals. Come see what they've been up to this year as well as what is happening in the tech industry in Ottawa.`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `There will be food (a 3 course meal), drinks, speakers, and awards presented including the outstanding student branch award! 🏆`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://events.vtools.ieee.org/m/177898'
+                    },
+                    text: 'Tickets'
+                },
+            ]
+        },
+        {
+            title: 'QlikHackOttawa 2018',
+            date: 'Friday Oct. 19, 2018 - Sunday Oct. 21, 2018',
+            time: '18:00 (Friday) - 9:00 (Sunday)',
+            location: '390 March Rd. Kanata',
+            body: [
+                {
+                    tag: 'p',
+                    text: `Qlik is hosting a hackathon in partnership with the United Nations for Developers and Designers, professionals and students, in Ottawa on the weekend of October 19! 📅`
+                },
+                {
+                    tag: 'htmlString',
+                    text: `Register <a href="https://lnkd.in/eXHuh5P">here</a> and come out to have fun, network, code, and design. It's free of charge and spaces are limited.`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://lnkd.in/eXHuh5P'
+                    },
+                    text: 'More Info'
+                },
+            ]
+        },
+        {
+            title: 'Resume Writing & Interview Skills for Engineering & IT Students',
+            date: 'Tuesday Oct. 16, 2018',
+            time: '17:30 - 19:00',
+            location: 'ME 3124',
+            body: [
+                {
+                    tag: 'p',
+                    text: `Want to learn how to write a killer resume and ace that interview? 📝👌🏼`
+                },
+                {
+                    tag: 'p',
+                    text: `The Faculty of Engineering and Design, along with Career Services is hosting a workshop on how to construct a resume that will stand out to technical recruiters and hiring managers, as well as help them prepare for interviews.`
+                },
+                {
+                    tag: 'htmlString',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `Registration is now open on mySuccess (via <a href='https://central.carleton.ca'>Carleton Central</a>) on the Career Services Event Calendar.`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://central.carleton.ca'
+                    },
+                    text: 'Carleton Central'
+                },
+            ]
+        },
+        {
+            title: 'Stay Late And Make (SLAM)',
+            date: 'Every Thursday',
+            time: '17:30 - 20:30',
+            location: 'ME 4463 (Makerspace)',
+            img: 'slam-2018.png',
+            body: [
+                {
+                    tag: 'p',
+                    text: `SLAM is a weekly club where students get together to work on their own or in a group project. 🔥`
+                },
+                {
+                    tag: 'p',
+                    text: `Bring your project to life! Get help from upper year students and network. Learn about hardware and software hacks and so much more.`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `Join us this Thursday in Mackenzie 4463.`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'http://staylateandmake.ca'
+                    },
+                    text: 'Website'
+                },
+            ]
+        },
+        {
+            title: 'IEEE Carleton General Meeting',
+            date: 'Thursday Oct. 11, 2018',
+            location: 'ME 3359',
+            img: 'ieee-carleton.png',
+            body: [
+                {
+                    tag: 'p',
+                    text: `We all volunteer our time and efforts to provide students and professionals multiple avenues to expose and drive innovation in the technical world.`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `Interested in being part of the team? 🙋🏼‍♀️🙋🏽‍♂️`
+                },
+                {
+                    tag: 'p',
+                    text: `This opportunity allows you to experiment and improve your technical, communication, management, and leadership skills, receive credits in Carleton's Co-Curricular Record (CCR), and enhance your resume when the accomplishments you make at IEEE, all the while making new friends!`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `Our office is in Mackenzie 3359, and our email us at info@ieeecarleton.ca.`
+                },
+                {
+                    tag: 'p',
+                    text: `Our executive meetings are held every other Monday in Mackenzie 4359 from 6 PM to 7:30 PM; stop by the office for more information.`
+                },
+                {
+                    tag: 'htmlString',
+                    text: `Follow us on <a href='http://facebook.com/ieeecarleton/'>facebook</a> and <a href='https://www.instagram.com/ieeecarleton/'>instagram</a> 🙏🏼`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://goo.gl/forms/LSmMK3SFyyLh04XJ2'
+                    },
+                    text: 'Mailing List Sign-up'
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://goo.gl/forms/xgM82qHVacNJ9WKH3'
+                    },
+                    text: 'Workshop Mailing List Sign-up'
+                },
+            ]
+        }
+    ]
+}
