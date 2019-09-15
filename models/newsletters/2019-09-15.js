867b2d0f55899f464b5659d6ddfceeeaccf1588d
Federico <federico.naranjo@gmail.com>
2019-09-15 14:18:10 -0400

    refinement

--- models/newsletters/2019-09-15.js
+++ models/newsletters/2019-09-15.js
@@ -14,6 +14,20 @@ module.exports = {
         }
     ],
     articles: [
+        // --- Volunteer Orientation --- //
+        {
+            title: `Volunteer Orientation`,            
+            date: `Tuesday September 17, 2019`,
+            time: `6:00 PM - 8:00 PM`,
+            location: `ME 4359`,
+            img: `orientation.png`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `Come on out to IEEE Volunteer Orientation! We will be going over the volunteering opportunities IEEE has to offer this year and eating pizza in ME 4359 at 7 PM on Tuesday September 17`
+                },
+            ]
+        },
         // --- Speaker Panel --- //
         {
             title: `IEEE WIE Carleton and CSES Speaker Panel`,
@@ -71,17 +85,55 @@ module.exports = {
                 },
             ]
         },
-        // --- Volunteer Orientation --- //
+        // --- 2019 Morgan Stanley WIT Round-Up --- //
         {
-            title: `Volunteer Orientation`,            
-            date: `Tuesday September 17, 2019`,
-            time: `6:00 PM - 8:00 PM`,
-            location: `ME 4359`,
+            title: `2019 Morgan Stanley WIT Round-Up`,            
+            date: `Friday, September 27, 2019`,
+            location: `Montreal`,
             img: `orientation.png`,
             body: [
                 {
                     tag: 'p',
-                    text: `Come on out to IEEE Volunteer Orientation! We will be going over the volunteering opportunities IEEE has to offer this year and eating pizza in ME 4359 at 7 PM on Tuesday September 17`
+                    text: `Morgan Stanley will be organizing a WIT Round-Up on Friday, September 27th. This is a great opportunity to learn more about technology, personal branding and career paths for students pursuing a technology degree.`
+                },
+                {
+                    tag: 'p',
+                    text: `This event would be most relevant to women in computer science, computer /software / electrical engineering and related disciplines.`
+                },
+                {
+                    tag: 'p',
+                    text: `*Travel reimbursement is available. Please email them for more information: valerie.kruk@morganstanley.com`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'mailto:wit-montreal@morganstanley.com'
+                    },
+                    text: `Register`
+                },
+            ]
+        },
+        // --- NASA Space Apps Hackathon --- //
+        {
+            title: `NASA Space Apps Hackathon`,            
+            date: `October 18 - 20, 2019`,
+            time: `6:00 PM - 8:00 PM`,
+            location: `ME 4359`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `This coming October, Space Apps Ottawa will be holding an intense 47 hour global weekend hackathon for developers, designers, engineers, entrepreneurs, astronomers, and enthusiasts. This event is being hosted at Carleton University!`
+                },
+                {
+                    tag: 'p',
+                    text: `Click on the button below for full info.`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://app.riipen.com/projects/6436/details'
+                    },
+                    text: `Signup / Info`
                 },
             ]
         },
