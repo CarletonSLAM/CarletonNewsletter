7b63f93ed7b09bc76d8bed7109e576b9cb30fec5
Federico <federico.naranjo@gmail.com>
2019-09-22 14:37:38 -0400

    asset push for weekly newsletter

---
+++ models/newsletters/2019-09-22.js
@@ -0,0 +1,135 @@
+module.exports = {
+    toast: [
+        {
+            tag: 'h2',
+            text: 'Keeping up with IEEE Carleton.'
+        },
+        {
+            tag: 'p',
+            text: 'Feel free to forward this newsletter to anyone you think might be interested.'
+        },
+        {
+            tag: 'htmlString',
+            text: `If you are having trouble viewing this as an email, click <a href='http://staylateandmake.ca/CarletonNewsletter/output/2019-09-22.html'>here</a> to view in browser.`
+        }
+    ],
+    articles: [
+        // --- New Event --- //
+        {
+            title: `cuHacking Fall General Meeting`,
+            date: `Monday September 23, 2019`,
+            time: `8:00 PM - 9:00 PM`,
+            location: `ME 4359`,
+            img: `cuhacking`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `cuHacking is holding its Fall General Meeting so that you can meet the entire team! If you want to get more involved with organizing Carleton's very own MLH hackathon feel free to attend the meeting today at 8 PM in ME 4359.`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/433565060837541/'
+                    },
+                    text: `Facebook Event`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://cuhacking.com/'
+                    },
+                    text: `Website`
+                },
+            ]
+        },
+        // --- New Event --- //
+        {
+            title: `IEEE and SCESoc GitHub Workshop`,
+            date: `Tuesday September 24, 2019`,
+            time: `6:00 PM - 8:00 PM`,
+            location: `CB 5109`,
+            img: `github.png`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `Have you ever wanted to program on a project with a team? Have you wanted to learn how industry and open-source collaborate in coding around the globe? Come learn how to use Git with SCESoc and IEEE! This workshop comprises of both learning simple Git theory and practical application!`
+                },
+                {
+                    tag: 'p',
+                    text: `The workshop starts at 6 PM in Canal Building 5109. This lab is booked to provide a preset environment for the participants to focus on learning and using the tool. However, if you are comfortable with the command line and have GIT set up on your computer, feel free to bring it.`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://forms.gle/ohrAc55pXJMtBaZ7A'
+                    },
+                    text: `Signup Form`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/500369127413723/'
+                    },
+                    text: `Facebook Event`
+                },
+            ]
+        },
+        // --- New Event --- //
+        {
+            title: `Intro to Vivado 2016`,
+            date: `Saturday September 28, 2019`,
+            time: `2:00 PM - 5:00 PM`,
+            location: `ME 4166`,
+            img: `vivado.png`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `Vivado is a powerful toolchain in the world of digital design and one of the most widely used in Education and Industry.`
+                },
+                {
+                    tag: 'p',
+                    text: `In this workshop, we will explore Vivado, what it is, how it works and try some of its features. We will do some basic Verilog HDL, block design and going over how to properly version control your projects for GitHub!`
+                },
+                {
+                    tag: 'p',
+                    text: `The aim of this workshop is to help introduce students taking ELEC3500 to Vivado 2016 or anyone just interested in getting started with FPGA design. We will be working with one of the best entry-level FPGAs, the Nexys A7 100T (The same board to be used in ELEC3500)`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://tinyurl.com/IEEE-Carleton-Intro-to-vivado'
+                    },
+                    text: `Signup Form`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/922824618066683/'
+                    },
+                    text: `Facebook Event`
+                },
+            ]
+        },
+        // --- New Event --- //
+        {
+            title: `IEEE Carleton General Meeting`,
+            date: `Wednesday September 25, 2019`,
+            time: `6:00 PM - 8:00 PM`,
+            location: `ME 4359`,
+            img: `ieee-carleton.png`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `The IEEE Carleton Student Branch hold a general meeting every two weeks. All students are welcome to attend especially if you are looking for opportunities to get involved.`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://ieeecarleton.ca'
+                    },
+                    text: `Website`
+                },
+            ]
+        },
+    ]
+}
