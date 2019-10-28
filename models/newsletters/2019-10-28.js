0cd4c4e6de12bc5ac0f7fcd6e5d501682c4d79de
Federico <federico.naranjo@gmail.com>
2019-10-28 13:58:59 -0400

    tentative build

---
+++ models/newsletters/2019-10-28.js
@@ -0,0 +1,208 @@
+module.exports = {
+    toast: [
+        {
+            tag: 'h2',
+            text: 'Keeping up with IEEE Carleton.'
+        },
+        {
+            tag: 'p',
+            text: 'Feel free to forward this newsletter to anyone you think might be interested.'
+        },
+        {
+            tag: 'htmlString',
+            text: `If you are having trouble viewing this as an email, click <a href='http://staylateandmake.ca/CarletonNewsletter/output/2019-10-28.html'>here</a> to view in browser.`
+        }
+    ],
+    articles: [
+        // --- New Event --- //
+        {
+            title: `Stay Late And Make SLAM`,
+            date: `Tuesday October 1, 2019`,
+            time: `6:00 PM - 9:00 PM`,
+            location: `ME 4463`,
+            img: `slam.png`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `Bring your own project or join one of our projects! We are running 3 main projects this year!`
+                },
+                {
+                    tag: 'p',
+                    text: `🎧 Reverse-engineering an Analog Amplifier 🎸`
+                },
+                {
+                    tag: 'p',
+                    text: `🛩 Quadcopter Project ⌘`
+                },
+                {
+                    tag: 'p',
+                    text: `🤖 AI Software Project 👾`
+                },
+                {
+                    tag: 'p',
+                    text: `We have many upper-year Electrical and Software Engineering students there to help!`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `SLAM sessions are happening twice a week now: Tuesdays and Thursdays, from 6 PM to 9 PM.`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.staylateandmake.ca'
+                    },
+                    text: `Website`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/442167939743249/'
+                    },
+                    text: `Events`
+                },
+            ]
+        },
+        // --- New Event --- //
+        {
+            title: `Software Security & Penetration Testing: An Interactive Introduction`,
+            date: `Tuesday October 29, 2019`,
+            time: `6:00 PM - 9:00 PM`,
+            location: `CB 5109`,
+            img: `security.png`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `SQL injections, XSS / cross-site scripting, replay attacks...`
+                },
+                {
+                    tag: 'p',
+                    text: `There’s a lot of security considerations in the modern web and application development world. Whether you want to become an ethical “white hat” hacker or make sure your app is secure, learning about security vulnerabilities is the first step to take!`
+                },
+                {
+                    tag: 'p',
+                    text: `In this workshop, we’ll be learning about the most common security vulnerabilities: what they are, how they work, how to execute them, and how to protect against them. Then there will be an interactive segment where everyone will be able to set up a vulnerable website on their own computer and try breaking into it.`
+                },
+                {
+                    tag: 'p',
+                    text: `Participation in both segments is not required. You are free to leave right after the presentation or stay a few hours for the interactive part. If you would like to set up for the interactive segment in advance, follow the setup instructions of your choice at https://github.com/bkimminich/juice-shop#setup.`
+                },
+                {
+                    tag: 'p',
+                    text: `For more details, check out the Facebook event below.`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://tinyurl.com/IEEE-Network-Security-Workshop'
+                    },
+                    text: `Sign-up`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/2204697802975129/'
+                    },
+                    text: `Facebook Event`
+                },
+            ]
+        },
+        // --- New Event --- //
+        {
+            title: `Advance Network Security Workshop`,
+            date: `Thursday November 7, 2019`,
+            date: `Thursday November 14, 2019`,
+            time: `6:00 PM`,
+            time: `7:30 PM`,
+            location: `ME 4359`,
+            img: `network.png`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `The sequence of workshops is presented by Professor Ashouri from the University of Isfahan who will review two important and widely-used network security protocols, namely Pretty Good Privacy (PGP) and Secure Socket Layer (SSL).`
+                },
+                {
+                    tag: 'p',
+                    text: `The workshops will explain the security requirements of an email system and discuss the PGP protocol that provides authentication, integrity and confidentiality services for email. Then, in the next two sessions, the SSL protocol, which is a security protocol of the transport layer of TCP/IP, is explained. Then there will be a discussion of the four sub-protocols of SSL namely SSL Handshake Protocol, Change Cipher Spec Protocol, Alert Protocol, and Record Protocol.`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://tinyurl.com/AdvanceNetworkSecurityWorkshop'
+                    },
+                    text: `Sign-up`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/547266379403375/'
+                    },
+                    text: `Facebook Event`
+                },
+            ]
+        },
+        // --- New Event --- //
+        {
+            title: `Trick or Treating in the 3300 Block`,
+            date: `Thursday October 31, 2019`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `All day on Halloween come on by the ME 3300 Block and trick or treat with the different societies!`
+                },
+            ]
+        },
+        // --- New Event --- //
+        {
+            title: `WISE National Conference 2020`,
+            date: `Saturday January 25, 2020`,
+            date: `Sunday January 26, 2020`,
+            time: `9:00 AM - 5:00 AM`,
+            location: `Hilton Toronto`,
+            img: `wise-conference.jpg`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `Women in Science and Engineering is proud to present their 8th Annual National Conference!`
+                },
+                {
+                    tag: 'p',
+                    text: `The WISE National Conference serves as a catalyst to inspire and empower individuals to pursue their passions, broaden their horizons, and form meaningful connections. The conference brings together delegates from all across Canada to share ideas and become inspired over the course of a two-day event dedicated to professional and personal growth, featuring inspirational leaders from a wide range of STEM fields, as well as workshops, case competitions, and career fairs.`
+                },
+                {
+                    tag: 'p',
+                    text: `Join them in Toronto on January 25-26th to be part of a movement dedicated to diversity and inclusion in STEM and learn how to Lead Without Limits!`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/cuwise/'
+                    },
+                    text: `CU-WISE`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://conference.wiseuoft.org'
+                    },
+                    text: `Website`
+                },
+            ]
+        },
+        // --- New Event --- //
+        {
+            title: `IEEE Carleton General Meeting`,
+            date: `Wednesday October 30, 2019`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `Our next meeting is on Wednesday October 30 from 6 to 7:30 PM in ME 4463.`
+                },
+            ]
+        },
+    ]
+}
