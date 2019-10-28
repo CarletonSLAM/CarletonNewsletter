ff00b56b28d565192b9de726a98d7e6b64f7d529
Federico <federico.naranjo@gmail.com>
2019-10-28 14:10:30 -0400

    tentative newsletter

--- models/newsletters/2019-10-28.js
+++ models/newsletters/2019-10-28.js
@@ -16,8 +16,98 @@ module.exports = {
     articles: [
         // --- New Event --- //
         {
+            title: `Software Security & Penetration Testing: An Interactive Introduction`,
+            date: `Tuesday October 29, 2019`,
+            time: `6:00 PM - 9:00 PM`,
+            location: `CB 5109`,
+            img: `security.png`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `SQL injections, XSS / cross-site scripting, replay attacks...`
+                },
+                {
+                    tag: 'p',
+                    text: `There’s a lot of security considerations in the modern web and application development world. Whether you want to become an ethical “white hat” hacker or make sure your app is secure, learning about security vulnerabilities is the first step to take!`
+                },
+                {
+                    tag: 'p',
+                    text: `In this workshop, we’ll be learning about the most common security vulnerabilities: what they are, how they work, how to execute them, and how to protect against them. Then there will be an interactive segment where everyone will be able to set up a vulnerable website on their own computer and try breaking into it.`
+                },
+                {
+                    tag: 'p',
+                    text: `Participation in both segments is not required. You are free to leave right after the presentation or stay a few hours for the interactive part. If you would like to set up for the interactive segment in advance, follow the setup instructions of your choice at https://github.com/bkimminich/juice-shop#setup.`
+                },
+                {
+                    tag: 'p',
+                    text: `For more details, check out the Facebook event below.`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://tinyurl.com/IEEE-Network-Security-Workshop'
+                    },
+                    text: `Sign-up`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/2204697802975129/'
+                    },
+                    text: `Facebook Event`
+                },
+            ]
+        },
+        // --- New Event --- //
+        {
+            title: `Trick or Treating in the ME 3300 Block`,
+            date: `Thursday October 31, 2019`,
+            img: `trick.png`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `All day on Halloween come on by the ME 3300 Block and trick or treat with the different societies!`
+                },
+            ]
+        },
+        // --- New Event --- //
+        {
+            title: `Advance Network Security Workshop`,
+            date: `Thursday November 7, 2019`,
+            date: `Monday November 11, 2019`,
+            date: `Thursday November 14, 2019`,
+            time: `6:00 PM - 7:30 PM`,
+            location: `ME 4359`,
+            img: `network.png`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `The sequence of workshops is presented by Professor Ashouri from the University of Isfahan who will review two important and widely-used network security protocols, namely Pretty Good Privacy (PGP) and Secure Socket Layer (SSL).`
+                },
+                {
+                    tag: 'p',
+                    text: `The workshops will explain the security requirements of an email system and discuss the PGP protocol that provides authentication, integrity and confidentiality services for email. Then, in the next two sessions, the SSL protocol, which is a security protocol of the transport layer of TCP/IP, is explained. Then there will be a discussion of the four sub-protocols of SSL namely SSL Handshake Protocol, Change Cipher Spec Protocol, Alert Protocol, and Record Protocol.`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://tinyurl.com/AdvanceNetworkSecurityWorkshop'
+                    },
+                    text: `Sign-up`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/547266379403375/'
+                    },
+                    text: `Facebook Event`
+                },
+            ]
+        },
+        // --- New Event --- //
+        {
             title: `Stay Late And Make SLAM`,
-            date: `Tuesday October 1, 2019`,
+            date: `Every Tuesday + Thursday`,
             time: `6:00 PM - 9:00 PM`,
             location: `ME 4463`,
             img: `slam.png`,
@@ -69,98 +159,8 @@ module.exports = {
         },
         // --- New Event --- //
         {
-            title: `Software Security & Penetration Testing: An Interactive Introduction`,
-            date: `Tuesday October 29, 2019`,
-            time: `6:00 PM - 9:00 PM`,
-            location: `CB 5109`,
-            img: `security.png`,
-            body: [
-                {
-                    tag: 'p',
-                    text: `SQL injections, XSS / cross-site scripting, replay attacks...`
-                },
-                {
-                    tag: 'p',
-                    text: `There’s a lot of security considerations in the modern web and application development world. Whether you want to become an ethical “white hat” hacker or make sure your app is secure, learning about security vulnerabilities is the first step to take!`
-                },
-                {
-                    tag: 'p',
-                    text: `In this workshop, we’ll be learning about the most common security vulnerabilities: what they are, how they work, how to execute them, and how to protect against them. Then there will be an interactive segment where everyone will be able to set up a vulnerable website on their own computer and try breaking into it.`
-                },
-                {
-                    tag: 'p',
-                    text: `Participation in both segments is not required. You are free to leave right after the presentation or stay a few hours for the interactive part. If you would like to set up for the interactive segment in advance, follow the setup instructions of your choice at https://github.com/bkimminich/juice-shop#setup.`
-                },
-                {
-                    tag: 'p',
-                    text: `For more details, check out the Facebook event below.`
-                },
-                {
-                    tag: 'button',
-                    attr: {
-                        href: 'https://tinyurl.com/IEEE-Network-Security-Workshop'
-                    },
-                    text: `Sign-up`
-                },
-                {
-                    tag: 'button',
-                    attr: {
-                        href: 'https://www.facebook.com/events/2204697802975129/'
-                    },
-                    text: `Facebook Event`
-                },
-            ]
-        },
-        // --- New Event --- //
-        {
-            title: `Advance Network Security Workshop`,
-            date: `Thursday November 7, 2019`,
-            date: `Thursday November 14, 2019`,
-            time: `6:00 PM`,
-            time: `7:30 PM`,
-            location: `ME 4359`,
-            img: `network.png`,
-            body: [
-                {
-                    tag: 'p',
-                    text: `The sequence of workshops is presented by Professor Ashouri from the University of Isfahan who will review two important and widely-used network security protocols, namely Pretty Good Privacy (PGP) and Secure Socket Layer (SSL).`
-                },
-                {
-                    tag: 'p',
-                    text: `The workshops will explain the security requirements of an email system and discuss the PGP protocol that provides authentication, integrity and confidentiality services for email. Then, in the next two sessions, the SSL protocol, which is a security protocol of the transport layer of TCP/IP, is explained. Then there will be a discussion of the four sub-protocols of SSL namely SSL Handshake Protocol, Change Cipher Spec Protocol, Alert Protocol, and Record Protocol.`
-                },
-                {
-                    tag: 'button',
-                    attr: {
-                        href: 'https://tinyurl.com/AdvanceNetworkSecurityWorkshop'
-                    },
-                    text: `Sign-up`
-                },
-                {
-                    tag: 'button',
-                    attr: {
-                        href: 'https://www.facebook.com/events/547266379403375/'
-                    },
-                    text: `Facebook Event`
-                },
-            ]
-        },
-        // --- New Event --- //
-        {
-            title: `Trick or Treating in the 3300 Block`,
-            date: `Thursday October 31, 2019`,
-            body: [
-                {
-                    tag: 'p',
-                    text: `All day on Halloween come on by the ME 3300 Block and trick or treat with the different societies!`
-                },
-            ]
-        },
-        // --- New Event --- //
-        {
             title: `WISE National Conference 2020`,
-            date: `Saturday January 25, 2020`,
-            date: `Sunday January 26, 2020`,
+            date: `January 25-26, 2020`,
             time: `9:00 AM - 5:00 AM`,
             location: `Hilton Toronto`,
             img: `wise-conference.jpg`,
@@ -197,10 +197,11 @@ module.exports = {
         {
             title: `IEEE Carleton General Meeting`,
             date: `Wednesday October 30, 2019`,
+            img: `ieee-carleton.png`,
             body: [
                 {
                     tag: 'p',
-                    text: `Our next meeting is on Wednesday October 30 from 6 to 7:30 PM in ME 4463.`
+                    text: `Our next meeting is on Wednesday October 30 from 6 to 7:30 PM in ME 4463. Come join us if you want to get involved with the student branch!`
                 },
             ]
         },
