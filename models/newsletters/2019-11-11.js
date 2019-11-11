a76fefbbe5523be977a6d1ac3a1335e1e4a11275
Federico <federico.naranjo@gmail.com>
2019-11-11 10:28:12 -0500

    tentative build

---
+++ models/newsletters/2019-11-11.js
@@ -0,0 +1,281 @@
+module.exports = {
+    toast: [
+        {
+            tag: 'h2',
+            text: 'Keeping up with IEEE Carleton.'
+        },
+        {
+            tag: 'p',
+            text: 'Feel free to forward this newsletter to anyone you think might be interested.'
+        },
+        {
+            tag: 'htmlString',
+            text: `If you are having trouble viewing this as an email, click <a href='http://staylateandmake.ca/CarletonNewsletter/output/2019-11-11.html'>here</a> to view in browser.`
+        }
+    ],
+    articles: [
+        // --- New Event --- //
+        {
+            title: `Advance Network Security Workshop`,
+            date: `November 11 + 14, 2019`,
+            time: `6:00 PM - 7:30 PM`,
+            location: `ME 4359`,
+            img: `network.png`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `The sequence of workshops is presented by Professor Ashouri from the University of Isfahan who will review two important and widely-used network security protocols, namely Pretty Good Privacy (PGP) and Secure Socket Layer (SSL).`
+                },
+                {
+                    tag: 'p',
+                    text: `The workshops will explain the security requirements of an email system and discuss the PGP protocol that provides authentication, integrity and confidentiality services for email. Then, in the next two sessions, the SSL protocol, which is a security protocol of the transport layer of TCP/IP, is explained. Then there will be a discussion of the four sub-protocols of SSL namely SSL Handshake Protocol, Change Cipher Spec Protocol, Alert Protocol, and Record Protocol.`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://tinyurl.com/AdvanceNetworkSecurityWorkshop'
+                    },
+                    text: `Sign-up`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/547266379403375/'
+                    },
+                    text: `Facebook Event`
+                },
+            ]
+        },
+        // --- New Event --- //
+        {
+            title: `Stay Late And Make SLAM`,
+            date: `Every Tuesday + Thursday`,
+            time: `6:00 PM - 9:00 PM`,
+            location: `ME 4463`,
+            img: `slam.png`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `Bring your own project or join one of our projects! We are running 3 main projects this year!`
+                },
+                {
+                    tag: 'p',
+                    text: `🎧 Reverse-engineering an Analog Amplifier 🎸`
+                },
+                {
+                    tag: 'p',
+                    text: `🛩 Quadcopter Project ⌘`
+                },
+                {
+                    tag: 'p',
+                    text: `🤖 AI Software Project 👾`
+                },
+                {
+                    tag: 'p',
+                    text: `We have many upper-year Electrical and Software Engineering students there to help!`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `SLAM sessions are happening twice a week now: Tuesdays and Thursdays, from 6 PM to 9 PM.`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.staylateandmake.ca'
+                    },
+                    text: `Website`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/442167939743249/'
+                    },
+                    text: `Facebook Event`
+                },
+            ]
+        },
+        // --- New Event --- //
+        {
+            title: `Extracting Medical Concepts Using Deep Learning`,
+            date: `Wednesday November 20, 2019`,
+            time: `6:00 PM - 7:30 PM`,
+            location: `ME 4463`,
+            img: `medical.png`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `Join us for a talk by Dr. Isar Nejadgholi on Deep Learning for Medical Records  Dr. Isar Nejadgholi is a research officer at National Research Council Canada. She received her PhD in biomedical engineering in 2012. In her MSc and PhD research, she designed neural network models and used them to solve speech-to-text and computer vision problems`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://medicaldl.eventbrite.ca'
+                    },
+                    text: `Sign-up`
+                },
+            ]
+        },
+        // --- New Event --- //
+        {
+            title: `Magnet Forensics and IEEE WIE Hackathon`,
+            date: `Tuesday November 19, 2019`,
+            time: `6:00 PM - 9:00 PM`,
+            location: `CB 3101`,
+            img: `wie-hacks.jpg`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `Coding skills are not required, but sleuthing skills are.  Solve a simulated digital crime, collaborate with our team members to acquire new skills, and learn a little more about how we help investigators uncover digital evidence at Magnet Forensics.  Food will be provided. Prizes awarded to the top 3 detectives.  The event is FREE, but make sure to reserve your spot by registering for the event!`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'http://bit.ly/MagnetForensicsAndWIEHack'
+                    },
+                    text: `Sign-up`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/1035565850110762/'
+                    },
+                    text: `Facebook Event`
+                },
+            ]
+        },
+        // --- New Event --- //
+        {
+            title: `Antennas & Metasurfaces for 5G`,
+            date: `Monday November 25, 2019`,
+            time: `6:00 PM - 8:00 PM`,
+            location: `ME 4463`,
+            img: `antennas.png`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `The future of wireless communication is expected to see an increase in demand for high data capacity to support massive connectivity between a plethora of electronic devices. This demand has led to the current development of fifth generation (5G) technologies in the millimeter-wave frequency band. With the increased prevalence of 5G technologies, there is a growing interest in developing antennas and metasurfaces for 5G applications. This talk will discuss some of the recent research conducted by the Antennas and Metamaterials Research Group, Department of Electronics, Carleton University.  Bio: Mohamed Emara (B. Eng, M.A.Sc.) received a B.Eng. degree in Aerospace Engineering in 2016, and an M.A.Sc. in Electrical and Computer Engineering in 2018, from Carleton University. He is currently working towards a Ph.D. in Electrical & Computer Engineering with a focus on metasurfaces for EM wave control.`
+                },
+                {
+                    tag: 'p',
+                    text: `Everyone will be able to understand this presentation. So there will be something everyone will learn, no matter what your background is and how much prior knowledge you have on the topic. All you need to do is to have an interest in the topic and attend the talk.`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `Food will be provided during this presentation!!!`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://tinyurl.com/talk-Emara'
+                    },
+                    text: `Sign-up`
+                },
+            ]
+        },
+        // --- New Event --- //
+        {
+            title: `Computerized Image Analysis`,
+            date: `Wednesday November 27, 2019`,
+            time: `6:00 PM - 7:30 PM`,
+            location: `MC 2014`,
+            img: `image-analytics.png`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `Join us for a talk by Dr. Adrian Chan on computerized image analysis  Adrian D.C. Chan is a Professor with the Department of Systems and Computer Engineering, Director of the NSERC CREATE Research and Education in Accessibility, Design, and Innovation (READi) training program, and co-founder of NeuroQore Inc.`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://iprocessing.eventbrite.ca'
+                    },
+                    text: `Sign-up`
+                },
+            ]
+        },
+        // --- New Event --- //
+        {
+            title: `WISE National Conference 2020`,
+            date: `January 25 + 26, 2020`,
+            location: `Hilton Toronto`,
+            img: `wise-conference.jpg`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `Women in Science and Engineering is proud to present their 8th Annual National Conference!`
+                },
+                {
+                    tag: 'p',
+                    text: `The WISE National Conference serves as a catalyst to inspire and empower individuals to pursue their passions, broaden their horizons, and form meaningful connections. The conference brings together delegates from all across Canada to share ideas and become inspired over the course of a two-day event dedicated to professional and personal growth, featuring inspirational leaders from a wide range of STEM fields, as well as workshops, case competitions, and career fairs.`
+                },
+                {
+                    tag: 'p',
+                    text: `Join them in Toronto on January 25-26th to be part of a movement dedicated to diversity and inclusion in STEM and learn how to Lead Without Limits!`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/cuwise/'
+                    },
+                    text: `CU-WISE`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://conference.wiseuoft.org'
+                    },
+                    text: `More Info`
+                },
+            ]
+        },
+        // --- New Event --- //
+        {
+            title: `IEEE SPAC 2020`,
+            date: `Thursday January 23, 2020`,
+            time: `5:30 PM - 10:30 PM`,
+            location: `Sala San Marco Conference & Events Centre`,
+            img: `spac.jpg`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `IEEE SPAC 2020 is back! We are currently selling tickets today! 🎫 We have a limited quantity of early bird tickets so be sure you’re the first to latch onto the discount! Want to know more or buy your tickets today?`
+                },
+                {
+                    tag: 'p',
+                    text: `Follow us on our website and make sure you put yourself as “interested” on our Facebook event to stay up to date! We also have Instagram now, so we will be posting a lot of awesome content!`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://ieeespac.ca/'
+                    },
+                    text: `Website (Tickets)`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/706341783179994/'
+                    },
+                    text: `Facebook Event`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.instagram.com/ieeespac/'
+                    },
+                    text: `Instagram`
+                },
+            ]
+        },
+    ]
+}
