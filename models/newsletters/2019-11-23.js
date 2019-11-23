f51e4d2b37c21eb58d802b88f643acd1619e62e3
Federico <federico.naranjo@gmail.com>
2019-11-23 15:49:14 -0500

    tentative newsletter

---
+++ models/newsletters/2019-11-23.js
@@ -0,0 +1,258 @@
+module.exports = {
+    toast: [
+        {
+            tag: 'h2',
+            text: 'Keeping up with IEEE Carleton.'
+        },
+        {
+            tag: 'p',
+            text: 'Feel free to forward this newsletter to anyone you think might be interested.'
+        },
+        {
+            tag: 'htmlString',
+            text: `If you are having trouble viewing this as an email, click <a href='http://staylateandmake.ca/CarletonNewsletter/output/2019-11-11.html'>here</a> to view in browser.`
+        }
+    ],
+    articles: [
+        // --- SLAM --- //
+        {
+            title: `Stay Late And Make SLAM`,
+            date: `Every Tuesday + Thursday`,
+            time: `6:00 PM - 9:00 PM`,
+            location: `ME 4463`,
+            img: `slam.png`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `Bring your own project or join one of our projects! We are running 3 main projects this year!`
+                },
+                {
+                    tag: 'p',
+                    text: `🎧 Reverse-engineering an Analog Amplifier 🎸`
+                },
+                {
+                    tag: 'p',
+                    text: `🛩 Quadcopter Project ⌘`
+                },
+                {
+                    tag: 'p',
+                    text: `🤖 AI Software Project 👾`
+                },
+                {
+                    tag: 'p',
+                    text: `We have many upper-year Electrical and Software Engineering students there to help!`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `SLAM sessions are happening twice a week now: Tuesdays and Thursdays, from 6 PM to 9 PM.`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.staylateandmake.ca'
+                    },
+                    text: `Website`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/442167939743249/'
+                    },
+                    text: `Facebook Event`
+                },
+            ]
+        },
+        // --- Antennas --- //
+        {
+            title: `Antennas & Metasurfaces for 5G`,
+            date: `Monday November 25, 2019`,
+            time: `6:00 PM - 8:00 PM`,
+            location: `ME 4463`,
+            img: `antennas.png`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `The future of wireless communication is expected to see an increase in demand for high data capacity to support massive connectivity between a plethora of electronic devices. This demand has led to the current development of fifth generation (5G) technologies in the millimeter-wave frequency band. With the increased prevalence of 5G technologies, there is a growing interest in developing antennas and metasurfaces for 5G applications.`
+                },
+                {
+                    tag: 'p',
+                    text: `This talk will discuss some of the recent research conducted by the Antennas and Metamaterials Research Group, Department of Electronics, Carleton University. Bio: Mohamed Emara (B. Eng, M.A.Sc.) received a B.Eng. degree in Aerospace Engineering in 2016, and an M.A.Sc. in Electrical and Computer Engineering in 2018, from Carleton University. He is currently working towards a Ph.D. in Electrical & Computer Engineering with a focus on metasurfaces for EM wave control.`
+                },
+                {
+                    tag: 'p',
+                    text: `Everyone will be able to understand this presentation. So there will be something everyone will learn, no matter what your background is and how much prior knowledge you have on the topic. All you need to do is to have an interest in the topic and attend the talk.`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `Food will be provided during this presentation!!!`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://tinyurl.com/talk-Emara'
+                    },
+                    text: `Sign-up`
+                },
+            ]
+        },
+        // --- Image Analysis --- //
+        {
+            title: `Computerized Image Analysis`,
+            date: `Wednesday November 27, 2019`,
+            time: `6:00 PM - 7:30 PM`,
+            location: `MC 2014`,
+            img: `image-analytics.png`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `Join us for a talk by Dr. Adrian Chan on computerized image analysis Adrian D.C. Chan is a Professor with the Department of Systems and Computer Engineering, Director of the NSERC CREATE Research and Education in Accessibility, Design, and Innovation (READi) training program, and co-founder of NeuroQore Inc.`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://iprocessing.eventbrite.ca'
+                    },
+                    text: `Sign-up`
+                },
+            ]
+        },
+        // --- LHD Build --- //
+        {
+            title: `MLH Local Hack Day: Build`,
+            date: `Saturday December 7, 2019`,
+            time: `6:00 PM - 9:00 PM`,
+            location: `Carleton (TBD)`,
+            img: `lhd-build.png`,
+            body: [
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `MLH Local Hack Day: Build is coming to Carleton on December 7th!`
+                },
+                {
+                    tag: 'p',
+                    text: `Join us and students all around the world in this global day-long hack day! It will be a day full of making a new projects, learning new skills, and meet other students interested in software, hardware, and technology.`
+                },
+                {
+                    tag: 'p',
+                    text: `Sign-in starts at 8:30am on Saturday December 7th. The event schedule and location will be sent out in the coming weeks. If you’d like to attend, please register with the link in our bio!`
+                },
+                {
+                    tag: 'htmlString',
+                    text: `Any questions can be sent to <a href="hackerexperience@cuhacking.com">hackerexperience@cuhacking.com</a> or directly to cuHacking's media pages.`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `If you’d like to attend, please register at the link below.`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://localhackday.mlh.io/build/locations/2531'
+                    },
+                    text: `Sign-up`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/3501446513213934/'
+                    },
+                    text: `Facebook Event`
+                },
+            ]
+        },
+        // --- WISE Conference --- //
+        {
+            title: `WISE National Conference 2020`,
+            date: `January 25 + 26, 2020`,
+            location: `Hilton Toronto`,
+            img: `wise-conference.jpg`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `Women in Science and Engineering is proud to present their 8th Annual National Conference!`
+                },
+                {
+                    tag: 'p',
+                    text: `The WISE National Conference serves as a catalyst to inspire and empower individuals to pursue their passions, broaden their horizons, and form meaningful connections. The conference brings together delegates from all across Canada to share ideas and become inspired over the course of a two-day event dedicated to professional and personal growth, featuring inspirational leaders from a wide range of STEM fields, as well as workshops, case competitions, and career fairs.`
+                },
+                {
+                    tag: 'p',
+                    text: `Join them in Toronto on January 25-26th to be part of a movement dedicated to diversity and inclusion in STEM and learn how to Lead Without Limits!`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/cuwise/'
+                    },
+                    text: `CU-WISE`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://conference.wiseuoft.org'
+                    },
+                    text: `More Info`
+                },
+            ]
+        },
+        // --- SPAC --- //
+        {
+            title: `IEEE SPAC 2020`,
+            date: `Thursday January 23, 2020`,
+            time: `5:30 PM - 10:30 PM`,
+            location: `Sala San Marco Conference & Events Centre`,
+            img: `spac.jpg`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `IEEE SPAC 2020 is back! We are currently selling tickets today! 🎫 We have a limited quantity of early bird tickets so be sure you’re the first to latch onto the discount! Want to know more or buy your tickets today?`
+                },
+                {
+                    tag: 'p',
+                    text: `Follow us on our website and make sure you put yourself as “interested” on our Facebook event to stay up to date! We also have Instagram now, so we will be posting a lot of awesome content!`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://ieeespac.ca/'
+                    },
+                    text: `Website (Tickets)`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/706341783179994/'
+                    },
+                    text: `Facebook Event`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.instagram.com/ieeespac/'
+                    },
+                    text: `Instagram`
+                },
+            ]
+        },
+    ]
+}
