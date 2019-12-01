0743ae9c13fab93e630d97116fea4054792d8a13
Federico <federico.naranjo@gmail.com>
2019-12-01 15:23:41 -0500

    tentative newsletter build

---
+++ models/newsletters/2019-12-1.js
@@ -0,0 +1,256 @@
+module.exports = {
+    toast: [
+        {
+            tag: 'h2',
+            text: 'Keeping up with IEEE Carleton.'
+        },
+        {
+            tag: 'p',
+            text: 'Feel free to forward this newsletter to anyone you think might be interested.'
+        },
+        {
+            tag: 'htmlString',
+            text: `If you are having trouble viewing this as an email, click <a href='http://staylateandmake.ca/CarletonNewsletter/output/2019-12-1.html'>here</a> to view in browser.`
+        }
+    ],
+    articles: [
+        // --- SLAM --- //
+        {
+            title: `Stay Late And Make SLAM`,
+            date: `Every Tuesday + Thursday`,
+            time: `6:00 PM - 9:00 PM`,
+            location: `ME 4463`,
+            img: `slam.png`,
+            body: [
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `Note: last SLAM session of the year on Tuesday Dec 3.`
+                },
+                {
+                    tag: 'p',
+                    text: `Bring your own project or join one of our projects! We are running 3 main projects this year!`
+                },
+                {
+                    tag: 'p',
+                    text: `🎧 Reverse-engineering an Analog Amplifier 🎸`
+                },
+                {
+                    tag: 'p',
+                    text: `🛩 Quadcopter Project ⌘`
+                },
+                {
+                    tag: 'p',
+                    text: `🤖 AI Software Project 👾`
+                },
+                {
+                    tag: 'p',
+                    text: `We have many upper-year Electrical and Software Engineering students there to help!`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `SLAM sessions are happening twice a week now: Tuesdays and Thursdays, from 6 PM to 9 PM.`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.staylateandmake.ca'
+                    },
+                    text: `Website`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/442167939743249/'
+                    },
+                    text: `Facebook Event`
+                },
+            ]
+        },
+        // --- LHD Build --- //
+        {
+            title: `MLH Local Hack Day: Build`,
+            date: `Saturday December 7, 2019`,
+            time: `6:00 PM - 9:00 PM`,
+            location: `Carleton (TBD)`,
+            img: `lhd-build.png`,
+            body: [
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `MLH Local Hack Day: Build is coming to Carleton on December 7th!`
+                },
+                {
+                    tag: 'p',
+                    text: `Join us and students all around the world in this global day-long hack day! It will be a day full of making a new projects, learning new skills, and meet other students interested in software, hardware, and technology.`
+                },
+                {
+                    tag: 'p',
+                    text: `Sign-in starts at 8:30am on Saturday December 7th. The event schedule and location will be sent out in the coming weeks. If you’d like to attend, please register with the link in our bio!`
+                },
+                {
+                    tag: 'htmlString',
+                    text: `Any questions can be sent to <a href="hackerexperience@cuhacking.com">hackerexperience@cuhacking.com</a> or directly to cuHacking's media pages.`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `If you’d like to attend, please register at the link below.`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://localhackday.mlh.io/build/locations/2531'
+                    },
+                    text: `Sign-up`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/3501446513213934/'
+                    },
+                    text: `Facebook Event`
+                },
+            ]
+        },
+        // --- cuHacking Applications --- //
+        {
+            title: `cuHacking 2020: Applications are Open!`,
+            img: `cuhacking-apps.png`,
+            body: [
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `cuHacking is back, bigger and better than ever!`
+                },
+                {
+                    tag: 'p',
+                    text: `Carleton's annual MLH hackathon is an amazing opportunity for you and your team to spend a day working on anything you'd like!`
+                },
+                {
+                    tag: 'p',
+                    text: `cuHacking 2020 once again will be hosted in River Building (Richcraft Hall) on January 11 + 12, 2020. Join cuHacking, IEEE Carleton, and other student clubs along with industry professionals and over 400 students to build something over the weekend. It's gonna be a lot of fun!`
+                },
+                {
+                    tag: 'p',
+                    text: `There will be awesome workshops, networking opportunities, and fun activities throughout the event.`
+                },
+                {
+                    tag: 'htmlString',
+                    text: `Applications are now open on <a href="https://cuhacking.com">cuhacking.com</a>.`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `Applications are open until Dec. 9 at 11:59 PM, don't procrastinate!`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://cuhacking.com'
+                    },
+                    text: `Website`
+                },
+            ]
+        },
+        // --- WISE Conference --- //
+        {
+            title: `WISE National Conference 2020`,
+            date: `January 25 + 26, 2020`,
+            location: `Hilton Toronto`,
+            img: `wise-conference.jpg`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `Women in Science and Engineering is proud to present their 8th Annual National Conference!`
+                },
+                {
+                    tag: 'p',
+                    text: `The WISE National Conference serves as a catalyst to inspire and empower individuals to pursue their passions, broaden their horizons, and form meaningful connections. The conference brings together delegates from all across Canada to share ideas and become inspired over the course of a two-day event dedicated to professional and personal growth, featuring inspirational leaders from a wide range of STEM fields, as well as workshops, case competitions, and career fairs.`
+                },
+                {
+                    tag: 'p',
+                    text: `Join them in Toronto on January 25-26th to be part of a movement dedicated to diversity and inclusion in STEM and learn how to Lead Without Limits!`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/cuwise/'
+                    },
+                    text: `CU-WISE`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://conference.wiseuoft.org'
+                    },
+                    text: `More Info`
+                },
+            ]
+        },
+        // --- SPAC --- //
+        {
+            title: `IEEE SPAC 2020`,
+            date: `Thursday January 23, 2020`,
+            time: `5:30 PM - 10:30 PM`,
+            location: `Sala San Marco Conference & Events Centre`,
+            img: `spac.jpg`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `IEEE SPAC 2020 is back! We are currently selling tickets today! 🎫 We have a limited quantity of early bird tickets so be sure you’re the first to latch onto the discount! Want to know more or buy your tickets today?`
+                },
+                {
+                    tag: 'p',
+                    text: `Follow us on our website and make sure you put yourself as “interested” on our Facebook event to stay up to date! We also have Instagram now, so we will be posting a lot of awesome content!`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://ieeespac.ca/'
+                    },
+                    text: `Website (Tickets)`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/706341783179994/'
+                    },
+                    text: `Facebook Event`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.instagram.com/ieeespac/'
+                    },
+                    text: `Instagram`
+                },
+            ]
+        },
+    ]
+}
