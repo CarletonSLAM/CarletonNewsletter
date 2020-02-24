738a6892b6d99a23a94192ed50b88da315b4f4a4
Federico <federico.naranjo@gmail.com>
2020-02-23 22:41:07 -0500

    tentative newsletter build

---
+++ models/newsletters/2020-02-23.js
@@ -0,0 +1,237 @@
+module.exports = {
+    toast: [
+        {
+            tag: 'h2',
+            text: 'Keeping up with IEEE Carleton.'
+        },
+        {
+            tag: 'p',
+            text: 'Feel free to forward this newsletter to anyone you think might be interested.'
+        },
+        {
+            tag: 'htmlString',
+            text: `If you are having trouble viewing this as an email, click <a href='http://staylateandmake.ca/CarletonNewsletter/output/2020-02-23.html'>here</a> to view in browser.`
+        }
+    ],
+    articles: [
+        // --- New Event --- //
+        {
+            title: `Stay Late And Make SLAM`,
+            date: `Every Thursday!`,
+            time: `6:00 PM - 9:00 PM`,
+            location: `ME 4463`,
+            img: `slam.png`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `Bring your own project or join one of our projects! We are running 3 main projects this year!`
+                },
+                {
+                    tag: 'p',
+                    text: `🎧 Reverse-engineering an Analog Amplifier 🎸`
+                },
+                {
+                    tag: 'p',
+                    text: `🛩 Quadcopter Project ⌘`
+                },
+                {
+                    tag: 'p',
+                    text: `🤖 AI Software Project 👾`
+                },
+                {
+                    tag: 'p',
+                    text: `We have many upper-year Electrical and Software Engineering students there to help!`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `**SLAM sessions are happening twice a week now: Tuesdays and Thursdays, from 6 PM to 9 PM.**`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.staylateandmake.ca'
+                    },
+                    text: `Website`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/442167939743249/'
+                    },
+                    text: `Facebook Event`
+                },
+            ]
+        },
+        // --- New Event --- //
+        {
+            title: `IEEE WIE 11th Annual Wine and Cheese`,
+            date: `Thursday March 5, 2020`,
+            time: `6:00 PM - 9:00 PM`,
+            location: `Sala San Marco`,
+            img: `w&c2020.jpg`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `Looking to make meaningful connections within  your community? Well, look no further.`
+                },
+                {
+                    tag: 'p',
+                    text: `Introducing IEEE Women in Engineering 11th annual Wine and Cheese. A fantastic opportunity for students to practice their networking skills and build a network with industry professionals and academia from the Ottawa community.`
+                },
+                {
+                    tag: 'p',
+                    text: `With an evening filled of interesting speakers, ice-breakers, and of course wine and cheese (and other foods!), there's no better time and place to start networking! 🍷🧀`
+                },
+                {
+                    tag: 'p',
+                    text: `Get your tickets online or visit your IEEE student branch office to purchase with cash! Each ticket comes with a FREE drink coupon (remember your ID).`
+                },
+                {
+                    tag: 'p',
+                    text: `($7 for IEEE Students, $10 for Regular Students)`
+                },
+                {
+                    tag: 'p',
+                    text: `uOttawa – SITE 4026`
+                },
+                {
+                    tag: 'p',
+                    text: `CarletonU – ME 3359`
+                },
+                {
+                    tag: 'p',
+                    text: `🙋‍♀️ OR BUY THEM ONLINE (both uOttawa and Carleton) AT https://www.celebratewie.ca/tickets/`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.celebratewie.ca/tickets/'
+                    },
+                    text: `Tickets`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/1526709784147976/'
+                    },
+                    text: `Facebook Event`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.celebratewie.ca'
+                    },
+                    text: `Website`
+                },
+            ]
+        },
+        // --- New Event --- //
+        {
+            title: `AI in Modern Power and Energy Systems!`,
+            date: `Wednesday March 4, 2020`,
+            time: `6:00 PM - 7:30 PM`,
+            location: `ME4359`,
+            img: `ai.png`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `Join us on AI in Modern Power and Energy Systems by Senior Director of BluWave.AI!`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://forms.gle/BR5hULmPG3MvKjaJ9'
+                    },
+                    text: `Registration`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://wie.ieeeottawa.ca/event/ai-in-modern-power-and-energy-systems/'
+                    },
+                    text: `More Info`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/479018892976861/'
+                    },
+                    text: `Facebook Event`
+                },
+            ]
+        },
+        // --- New Event --- //
+        {
+            title: `Introduction to Project Management`,
+            date: `Tuesday March 3, 2020`,
+            time: `6:00 PM - 7:30 PM`,
+            location: `ME 4463`,
+            img: `project-management.png`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `This workshop will be talking about the importance of project management, through the experience the presenter in leading some projects at different companies such as how he improved agile processes to get his team to deliver an 8-month project in 4 months. In general, students will be learning methods to make their team more productive.`
+                },
+                {
+                    tag: 'p',
+                    text: `In addition, he will also talk about his experience in becoming a Project Manager and will also give tips to help prospective students wanting to follow that managerial career path.`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://forms.gle/vQLuantDUPep6qnN7'
+                    },
+                    text: `Sign Up`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/489857578570748/'
+                    },
+                    text: `Facebook Event`
+                },
+            ]
+        },
+        // --- New Event --- //
+        {
+            title: `Problem Solving for Programming Interviews`,
+            date: `Friday March 6, 2020`,
+            time: `6:00 PM - 8:00 PM`,
+            location: `ME 4463`,
+            img: `problem-solving.png`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `🎖📝 Come learn how to solve problems — under pressure — in your next interview.`
+                },
+                {
+                    tag: 'p',
+                    text: `Join us to learn how to ace the problem portion of your interviews with the expertise from upper-year students. We'll be covering algorithmic and logic problems.`
+                },
+                {
+                    tag: 'p',
+                    text: `This workshop works well as preparation for your co-op interviews!`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://forms.gle/27hsRkK6wRbFCFgm9'
+                    },
+                    text: `Sign Up`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/1307178029479527/'
+                    },
+                    text: `Facebook Event`
+                }
+            ]
+        },
+    ]
+}
