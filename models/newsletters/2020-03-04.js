9432de4f21ab0f182331617c366e8bbe17277ac0
Federico <federico.naranjo@gmail.com>
2020-03-04 12:11:19 -0500

    tentative newsletter update build

---
+++ models/newsletters/2020-03-04.js
@@ -0,0 +1,358 @@
+module.exports = {
+    toast: [
+        {
+            tag: 'h2',
+            text: 'Keeping up with IEEE Carleton.'
+        },
+        {
+            tag: 'p',
+            text: 'Feel free to forward this newsletter to anyone you think might be interested.'
+        },
+        {
+            tag: 'htmlString',
+            text: `If you are having trouble viewing this as an email, click <a href='http://staylateandmake.ca/CarletonNewsletter/output/2020-03-04.html'>here</a> to view in browser.`
+        }
+    ],
+    articles: [
+        // --- New Event --- //
+        {
+            title: `Self-Management Interactive Workshop`,
+            date: `Thursday March 5, 2020`,
+            time: `11:30 AM - 1:30 PM`,
+            location: `uOttawa, STM Room 664`,
+            img: `interactive.png`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `Distractions have never been easier to fall into and the demands of your time, energy and mind have never been greater. Discover how to guard these assets to propel yourself onward and upward. This workshop will outline how to align your time, energy and mind to become more productive and accomplish more.`
+                },
+                {
+                    tag: 'p',
+                    text: `Scott Rust graduated with a BComm from Concordia University and continues his professional development towards his International Coaching Federation – ACC level accreditation. Scott specializes in productivity and leadership coaching to help high-achievers accomplish more, work at their best and erode stress.`
+                },
+                {
+                    tag: 'p',
+                    text: `Join us on AI in Modern Power and Energy Systems by Senior Director of BluWave.AI!`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'http://bit.ly/SMWYP'
+                    },
+                    text: `Registration`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'http://www.scottrust.com/sr-coaching-workshop/'
+                    },
+                    text: `More Info`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/861361234285309/'
+                    },
+                    text: `Facebook Event`
+                },
+            ]
+        },
+        // --- New Event --- //
+        {
+            title: `IEEE Battle Royale XIII`,
+            img: `b-r.png`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `🎮 Attention all gamers: IEEE Battle Royale XIII needs your help! We're currently looking for planning team members to help get IEEE's annual 24-hour LAN party off the ground!`
+                },
+                {
+                    tag: 'p',
+                    text: `If you're interested in leading teams in patronage, web design, volunteer coordination, and other fields, check out the Google form below! 👽`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://forms.gle/pAS64TSW1RnMHP4VA'
+                    },
+                    text: `Join Now!`
+                },
+            ]
+        },
+        // --- New Event --- //
+        {
+            title: `AI in Modern Power and Energy Systems!`,
+            date: `Wednesday March 4, 2020`,
+            time: `6:00 PM - 7:30 PM`,
+            location: `ME4359`,
+            img: `ai.png`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `Join us on AI in Modern Power and Energy Systems by Senior Director of BluWave.AI!`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://forms.gle/BR5hULmPG3MvKjaJ9'
+                    },
+                    text: `Registration`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://wie.ieeeottawa.ca/event/ai-in-modern-power-and-energy-systems/'
+                    },
+                    text: `More Info`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/479018892976861/'
+                    },
+                    text: `Facebook Event`
+                },
+            ]
+        },
+        // --- New Event --- //
+        {
+            title: `IEEE SPAC 2020 Lead Candidates`,
+            date: `Wednesday March 4, 2020`,
+            time: `23:59`,
+            img: `spac-leads.png`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `We are currently looking for SPAC 2021 Leads. There will be two leads for this event, the Carleton SPAC Lead and the uOttawa SPAC Lead. Applications have been extended to Wednesday, March 4 at 11:59 pm.`
+                },
+                {
+                    tag: 'p',
+                    text: `What is SPAC? SPAC is one of the largest hi-tech networking dinners situated in Ottawa. SPAC has served as one of the longest running networking night for over 9 years. Next year will mark the 10th anniversary for SPAC. For a reference to what SPAC is, click this here to watch a video.`
+                },
+                {
+                    tag: 'p',
+                    text: `Roles of SPAC Leads: The SPAC Leads are required to recruit, organize, and manage a team of 20+ organizers/volunteers. The SPAC Leads delegate tasks to team leads and ensures all preparation for the event falls according to plan. `
+                },
+                {
+                    tag: 'p',
+                    text: `Requirements for SPAC Leads: Event planning experience and motivation, with foresight for issues and ability to drive decisions through team members. Time management and organization skills are key assets. An additional asset is the ability to resolve conflicts internally within the team and externally with third parties.`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://forms.gle/vk7pqhrqa6eY3UjX7'
+                    },
+                    text: `Apply Here`
+                },
+            ]
+        },
+        // --- New Event --- //
+        {
+            title: `IEEE WIE 11th Annual Wine and Cheese`,
+            date: `Thursday March 5, 2020`,
+            time: `6:00 PM - 9:00 PM`,
+            location: `Sala San Marco`,
+            img: `w&c2020.jpg`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `Looking to make meaningful connections within  your community? Well, look no further.`
+                },
+                {
+                    tag: 'p',
+                    text: `Introducing IEEE Women in Engineering 11th annual Wine and Cheese. A fantastic opportunity for students to practice their networking skills and build a network with industry professionals and academia from the Ottawa community.`
+                },
+                {
+                    tag: 'p',
+                    text: `With an evening filled of interesting speakers, ice-breakers, and of course wine and cheese (and other foods!), there's no better time and place to start networking! 🍷🧀`
+                },
+                {
+                    tag: 'p',
+                    text: `Get your tickets online or visit your IEEE student branch office to purchase with cash! Each ticket comes with a FREE drink coupon (remember your ID).`
+                },
+                {
+                    tag: 'p',
+                    text: `($7 for IEEE Students, $10 for Regular Students)`
+                },
+                {
+                    tag: 'p',
+                    text: `uOttawa – SITE 4026`
+                },
+                {
+                    tag: 'p',
+                    text: `CarletonU – ME 3359`
+                },
+                {
+                    tag: 'p',
+                    text: `🙋‍♀️ OR BUY THEM ONLINE (both uOttawa and Carleton) AT https://www.celebratewie.ca/tickets/`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.celebratewie.ca/tickets/'
+                    },
+                    text: `Tickets`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/1526709784147976/'
+                    },
+                    text: `Facebook Event`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.celebratewie.ca'
+                    },
+                    text: `Website`
+                },
+            ]
+        },
+        // --- New Event --- //
+        {
+            title: `Stay Late And Make SLAM`,
+            date: `Every Thursday!`,
+            time: `6:00 PM - 9:00 PM`,
+            location: `ME 4463`,
+            img: `slam.png`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `Bring your own project or join one of our projects! We are running 3 main projects this year!`
+                },
+                {
+                    tag: 'p',
+                    text: `🎧 Reverse-engineering an Analog Amplifier 🎸`
+                },
+                {
+                    tag: 'p',
+                    text: `🛩 Quadcopter Project ⌘`
+                },
+                {
+                    tag: 'p',
+                    text: `🤖 AI Software Project 👾`
+                },
+                {
+                    tag: 'p',
+                    text: `We have many upper-year Electrical and Software Engineering students there to help!`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `SLAM sessions are happening twice a week now: Tuesdays and Thursdays, from 6 PM to 9 PM.`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.staylateandmake.ca'
+                    },
+                    text: `Website`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/442167939743249/'
+                    },
+                    text: `Facebook Event`
+                },
+            ]
+        },
+        // --- New Event --- //
+        {
+            title: `Problem Solving for Programming Interviews`,
+            date: `Friday March 6, 2020`,
+            time: `6:00 PM - 8:00 PM`,
+            location: `ME 4463`,
+            img: `problem-solving.png`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `🎖📝 Come learn how to solve problems — under pressure — in your next interview.`
+                },
+                {
+                    tag: 'p',
+                    text: `Join us to learn how to ace the problem portion of your interviews with the expertise from upper-year students. We'll be covering algorithmic and logic problems.`
+                },
+                {
+                    tag: 'p',
+                    text: `This seminar works well as preparation for your co-op interviews!`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://forms.gle/27hsRkK6wRbFCFgm9'
+                    },
+                    text: `Sign Up`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/1307178029479527/'
+                    },
+                    text: `Facebook Event`
+                }
+            ]
+        },
+        // --- New Event --- //
+        {
+            title: `AI, Machine Learning and their Applications`,
+            date: `Wednesday March 25, 2020`,
+            time: `6:00 PM - 8:30 PM`,
+            location: `CB 5109`,
+            img: `ml.png`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `This workshop provides an “in-depth” overview of Machine Learning, the principles of supervised learning as well as a survey of the algorithms for supervised and unsupervised learning and reinforcement learning and their applications in industry.`
+                },
+                {
+                    tag: 'p',
+                    text: `The goal of this talk is to provide an end-to-end understanding of the meaning of Machine Learning (ML) and where it lies in the bigger space of Artificial Intelligence (AI) with examples of how machine learning applications are deployed in industry. Our goal is to inspire more people to get involved in understanding and building ML/AI products.`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://tinyurl.com/IEEE-Carleton-ML-Workshop'
+                    },
+                    text: `Sign Up`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/613682229412095/'
+                    },
+                    text: `Facebook Event`
+                }
+            ]
+        },
+        // --- Exec of the Month --- //
+        {
+            title: `Exec of the Month`,
+            img: `exec-feb.png`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `Every month we wish to appreciate and thank one of our execs that went above and beyond in their duties. They showcase the values of the IEEE Carleton Student Branch and are a shining example to their peers. For the month of February the exec of the month was: Hemant Gupta`
+                },
+                {
+                    tag: 'p',
+                    attr: {
+                        style: {
+                            fontWeight: 'bold'
+                        }
+                    },
+                    text: `Here are a few words from our Exec of the Month:`
+                },
+                {
+                    tag: 'p',
+                    text: `Thanks everyone for choosing me. Its always been a great learning and fun opportunity to working with you all from past 3 years. And I hope I will continue fulfilling all my duties in upcoming future with best of my capabilities.`
+                },
+            ]
+        },
+    ]
+}
