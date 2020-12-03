faef42f6ee13b0e1ebb66502593eda79d2456584
Federico Naranjo <federico.naranjo@gmail.com>
2020-12-02 21:19:27 -0500

    showing brian how to do this

---
+++ models/newsletters/2020-12-02.js
@@ -0,0 +1,177 @@
+module.exports = {
+    toast: [
+        {
+            tag: 'h2',
+            text: 'Keeping up with IEEE Carleton.'
+        },
+        {
+            tag: 'p',
+            text: 'Feel free to forward this newsletter to anyone you think might be interested.'
+        },
+        {
+            tag: 'htmlString',
+            text: `If you are having trouble viewing this as an email, click <a href='http://staylateandmake.ca/CarletonNewsletter/output/2020-12-02.html'>here</a> to view in browser.`
+        }
+    ],
+    articles: [
+        // --- New Event --- //
+        {
+            title: `test event name`,
+            date: `Thursday May 30, 2019`,
+            date: `Friday May 31, 2019`,
+            time: `11:30 AM`,
+            time: `5:00 PM`,
+            location: `TEST 23`,
+            img: `https://drive.google.com/open?id=19MeStMbHEw1ARrqSHmHLY478SckBQ5e9`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `test description`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://button1.com'
+                    },
+                    text: `ADD BUTTON TEXT`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://button2.com'
+                    },
+                    text: `ADD BUTTON TEXT`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://button3.com'
+                    },
+                    text: `ADD BUTTON TEXT`
+                },
+            ]
+        },
+        // --- New Event --- //
+        {
+            title: `IEEE WIE 11th Annual Wine and Cheese`,
+            date: `Thursday March 5, 2020`,
+            time: `6:00 PM`,
+            time: `9:00 PM`,
+            location: `Sala San Marco`,
+            img: `https://drive.google.com/open?id=19_RnyKx5t77wJ6c9-TNgISU6pF0h1yvI`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `Looking to make meaningful connections within  your community? Well, look no further.  Introducing IEEE Women in Engineering 11th annual Wine and Cheese. A fantastic opportunity for students to practice their networking skills and build a network with industry professionals and academia from the Ottawa community.`
+                },
+                {
+                    tag: 'p',
+                    text: `With an evening filled of interesting speakers, ice-breakers, and of course wine and cheese (and other foods!), there's no better time and place to start networking! 🍷🧀  Get your tickets online or visit your IEEE student branch office to purchase with cash! Each ticket comes with a FREE drink coupon (remember your ID). ($7 for IEEE Students, $10 for Regular Students)  uOttawa – SITE 4026 CarletonU – ME 3359  🙋‍♀️ OR BUY THEM ONLINE (both uOttawa and Carleton) AT https://www.celebratewie.ca/tickets/`
+                },
+                {
+                    tag: 'p',
+                    text: `Side note: Make sure to use https NOT http website!`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.celebratewie.ca/tickets/'
+                    },
+                    text: `ADD BUTTON TEXT`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/1526709784147976/'
+                    },
+                    text: `ADD BUTTON TEXT`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.celebratewie.ca'
+                    },
+                    text: `ADD BUTTON TEXT`
+                },
+            ]
+        },
+        // --- New Event --- //
+        {
+            title: `AI in Modern Power and Energy Systems!`,
+            date: `Wednesday March 4, 2020`,
+            time: `6:00 PM`,
+            time: `7:30 PM`,
+            location: `ME4359`,
+            img: `https://drive.google.com/open?id=1L1mA_3InjNKbyRXnSMYljE9cvkeeRre2`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `Join us on AI in Modern Power and Energy Systems by Senior Director of BluWave.AI!`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://forms.gle/BR5hULmPG3MvKjaJ9'
+                    },
+                    text: `ADD BUTTON TEXT`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://wie.ieeeottawa.ca/event/ai-in-modern-power-and-energy-systems/'
+                    },
+                    text: `ADD BUTTON TEXT`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/479018892976861/'
+                    },
+                    text: `ADD BUTTON TEXT`
+                },
+            ]
+        },
+        // --- New Event --- //
+        {
+            title: `Introduction to Project Management`,
+            date: `Tuesday March 3, 2020`,
+            time: `6:00 PM`,
+            time: `7:30 PM`,
+            location: `ME 4463`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `This workshop will be talking about the importance of project management, through the experience the presenter in leading some projects at different companies such as how he improved agile processes to get his team to deliver an 8-month project in 4 months. In general, students will be learning methods to make their team more productive.   In addition, he will also talk about his experience in becoming a Project Manager and will also give tips to help prospective students wanting to follow that managerial career path.`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://tinyurl.com/project-management-seminar'
+                    },
+                    text: `ADD BUTTON TEXT`
+                },
+            ]
+        },
+        // --- New Event --- //
+        {
+            title: `Problem Solving for Programming Interviews`,
+            date: `Friday March 6, 2020`,
+            time: `6:00 PM`,
+            time: `8:00 PM`,
+            location: `ME 4463`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `🎖📝 Come learn how to solve problems — under pressure — in your next interview.  Join us to learn how to ace the problem portion of your interviews with the expertise from upper-year students. We'll be covering algorithmic and logic problems.  This workshop works well as preparation for your co-op interviews!`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://tinyurl.com/IEEE-Technical-Interview'
+                    },
+                    text: `ADD BUTTON TEXT`
+                },
+            ]
+        },
+    ]
+}
