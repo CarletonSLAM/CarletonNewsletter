9058db5300c812aa5c5c2e64ebd9cea4a844ad5c
brian_yang4444 <briany4444@gmail.com>
2020-12-30 01:32:41 -0400

    asd

---
+++ models/newsletters/2020-12-30.js
@@ -0,0 +1,295 @@
+module.exports = {
+    toast: [
+        {
+            tag: 'h2',
+            text: 'Keeping up with IEEE Carleton.'
+        },
+        {
+            tag: 'p',
+            text: 'Feel free to forward this newsletter to anyone you think might be interested.'
+        },
+        {
+            tag: 'htmlString',
+            text: `If you are having trouble viewing this as an email, click <a href='http://staylateandmake.ca/CarletonNewsletter/output/2020-12-30.html'>here</a> to view in browser.`
+        }
+    ],
+    articles: [
+        // --- New Event --- //
+        {
+            title: `test event name`,
+            date: `Thursday May 30, 2019`,
+            date: `Friday May 31, 2019`,
+            time: `11:30 AM`,
+            time: `5:00 PM`,
+            location: `TEST 23`,
+            img: `https://drive.google.com/open?id=19MeStMbHEw1ARrqSHmHLY478SckBQ5e9`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `test description`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://button1.com'
+                    },
+                    text: `ADD BUTTON TEXT`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://button2.com'
+                    },
+                    text: `ADD BUTTON TEXT`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://button3.com'
+                    },
+                    text: `ADD BUTTON TEXT`
+                },
+            ]
+        },
+        // --- New Event --- //
+        {
+            title: `IEEE Battle Royale XIII`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `🎮 Attention all gamers: IEEE Battle Royale XIII needs your help! We're currently looking for planning team members to help get IEEE's annual 24-hour LAN party off the ground! If you're interested in leading teams in patronage, web design, volunteer coordination, and other fields, check out the Google form below! 👽`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://forms.gle/pAS64TSW1RnMHP4VA'
+                    },
+                    text: `ADD BUTTON TEXT`
+                },
+            ]
+        },
+        // --- New Event --- //
+        {
+            title: `Humanitarian Technology Seminar`,
+            date: `Thursday March 19, 2020`,
+            time: `6:00 PM`,
+            time: `7:30 PM`,
+            location: `ME 4463`,
+            img: `pic1 - Academics IEEE Carleton.png https://drive.google.com/open?id=1d2oM5-LjtFH5akoDJMtUz9RNdWSZqzRx, https://drive.google.com/open?id=1V8_Pq3O09eTMKXgNKX4q3bxszl8Xv704, https://drive.google.com/open?id=1YiBbWFdmm4LEr5ks5X1vmyxUn4kELxiQ, https://drive.google.com/open?id=1-PTXNqHQtV31Pn31Q_B6aESdtDliGGFf`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `Humanitarian Activities has been a mission of the IEEE, advancing technology for humanity. As engineers used their skills for the greater good, IEEE SIGHT volunteers work all over the world on different humanitarian projects. Particularly In Canada, students and young professionals have demonstrated commitment to solving problems in their communities. Anis Ben Arfi, currently leading IEEE Canada Humanitarian Initiatives Committee, will be presenting exemplary IEEE SIGHT projects and events, aiming to inspire you to join the IEEE humanitarian efforts!`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://tinyurl.com/Humanitariantechnologyseminar'
+                    },
+                    text: `ADD BUTTON TEXT`
+                },
+            ]
+        },
+        // --- New Event --- //
+        {
+            title: `IEEE Carleton AGM`,
+            date: `Monday March 30, 2020`,
+            time: `5:30 PM`,
+            time: `7:30 PM`,
+            location: `https://www.twitch.tv/ieeecarleton`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `Join us to see the events we have done during the year, the impact we have made, and the new exec team that will be running IEEE Carleton next year!`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.twitch.tv/ieeecarleton'
+                    },
+                    text: `ADD BUTTON TEXT`
+                },
+            ]
+        },
+        // --- New Event --- //
+        {
+            title: `Clarifying the Path to becoming a Professional Engineer`,
+            date: `Tuesday May 26, 2020`,
+            time: `12:00 PM`,
+            time: `2:00 AM`,
+            location: `Online Event`,
+            img: `PEO-01 - João Guilherme Nizer Rahmeier.jpg`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `IEEE Young Professionals and Woman In Engineering Ottawa Sections in partnership with Professional Engineers Ontario present an online talk in "Clarifying the Path to Becoming a Professional Engineer." Pre-register now: https://bit.ly/YPPEOEB ! We look forward to speaking with you!`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://bit.ly/YPPEOEB'
+                    },
+                    text: `ADD BUTTON TEXT`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/1088987881457326/'
+                    },
+                    text: `ADD BUTTON TEXT`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://site.ieee.org/ottawa-yp/'
+                    },
+                    text: `ADD BUTTON TEXT`
+                },
+            ]
+        },
+        // --- New Event --- //
+        {
+            title: `[Online Talk]: AI in Modern Power and Energy Systems (Part II)!`,
+            date: `Wednesday May 27, 2020`,
+            time: `2:00 PM`,
+            time: `3:00 PM`,
+            location: `Online: the link will be sent to those registered closer to the date`,
+            img: `Screen Shot 2020-05-05 at 9.14.31 PM - IEEE WIE.png`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `Join us for AI in Modern Power and Energy Systems (Part II) by Dr. Mostafa FarrokhAbadi, the senior director of technology in BluWave.ai.`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://bit.ly/35qz040'
+                    },
+                    text: `ADD BUTTON TEXT`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://wie.ieeeottawa.ca/event/online-talk-ai-in-modern-power-and-energy-systems-part-ii/'
+                    },
+                    text: `ADD BUTTON TEXT`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/263844228074701/'
+                    },
+                    text: `ADD BUTTON TEXT`
+                },
+            ]
+        },
+        // --- New Event --- //
+        {
+            title: `Accelerating 5G Design Innovation Through Simulation`,
+            date: `Wednesday June 10, 2020`,
+            time: `2:30 AM`,
+            time: `4:30 AM`,
+            location: `Online Event`,
+            img: `flyer - João Guilherme Nizer Rahmeier.jpeg`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `Accelerating 5G Design Innovation Through Simulation -End User Equipment / Base Station-`
+                },
+                {
+                    tag: 'p',
+                    text: `5G connectivity is the next technological revolution. This pervasive, ultrafast compute network will connect billions of devices with data on-demand. It will drive economic expansion in many sectors, spawn new products and services, and transform our lives as we know it. Yet, before 5G can deliver on its promises and quality of service (QoS) metrics, wireless systems designers and engineers must overcome sizable challenges. Ansys 5G simulation solutions empower these individuals to solve the complexities impeding device, network and data center design. Ansys 5G simulation solutions provide electromagnetics, semiconductor, electronics cooling and mechanical analysis tools to accurately simulate 5G radio and related technologies. The multi-solution platform leverages high-performance computing that can be deployed across the enterprise, allowing designers and engineering experts to collaborate more effectively.`
+                },
+                {
+                    tag: 'p',
+                    text: `This seminar will highlight the following 5G engineering challenges:`
+                },
+                {
+                    tag: 'p',
+                    text: `End User Equipment`
+                },
+                {
+                    tag: 'p',
+                    text: `multi-frequency band antenna integration`
+                },
+                {
+                    tag: 'p',
+                    text: `modeling of mm-wave array antennas`
+                },
+                {
+                    tag: 'p',
+                    text: `RFI, EMI & Desense Mitigation`
+                },
+                {
+                    tag: 'p',
+                    text: `Base-Station Antenna Modeling`
+                },
+                {
+                    tag: 'p',
+                    text: `Full Communication Analysis in Electrically Large & Complex Environment`
+                },
+                {
+                    tag: 'p',
+                    text: `RFI, Data Coverage & ElectroThermal Reliability`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/239229434044499/'
+                    },
+                    text: `ADD BUTTON TEXT`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://bit.ly/Ansys5G'
+                    },
+                    text: `ADD BUTTON TEXT`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://events.vtools.ieee.org/m/231928'
+                    },
+                    text: `ADD BUTTON TEXT`
+                },
+            ]
+        },
+        // --- New Event --- //
+        {
+            title: `ADAS Simulation: Exceeding the Customer Requirements`,
+            date: `Friday June 12, 2020`,
+            time: `10:00 AM`,
+            time: `11:00 AM`,
+            location: `Online`,
+            img: `Flyer_ADAS Simulation - João Guilherme Nizer Rahmeier.jpg`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `We are happy to invite you for the webinar organized by IEEE YPAG, Windsor Section in partnership with Kevin Martin (Supervisor, ADAS Simulation Assisted Driving,) Ford Motor Company, on "ADAS (Advanced Driver Assistance Systems) Simulation: Exceeding the Customer Requirements". Register now: https://bit.ly/ADASYP`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://bit.ly/ADASYP'
+                    },
+                    text: `ADD BUTTON TEXT`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/events/258583595210219/'
+                    },
+                    text: `ADD BUTTON TEXT`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.linkedin.com/events/6675069037887680512/'
+                    },
+                    text: `ADD BUTTON TEXT`
+                },
+            ]
+        },
+    ]
+}
