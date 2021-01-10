41d1d92f5ea9b94a3c6135cc9a5bf01aa43dab18
brian_yang4444 <briany4444@gmail.com>
2021-01-09 23:25:25 -0500

    Jan 9th

--- models/newsletters/2021-01-09.js
+++ models/newsletters/2021-01-09.js
@@ -22,6 +22,23 @@ module.exports = {
             location: `Online Event`,
             img: `PEO2021-01 - João Guilherme Nizer Rahmeier.png`,
             body: [
+              {
+                  tag: 'p',
+                  text: `If you are an engineering undergraduate or post-graduate student, or a recent graduate starting out your engineering career, then this seminar might be for you.`
+              },
+              {
+                  tag: 'p',
+                  text: `In it you will learn: What is PEO? What engineering experience is PEO looking for once I graduate? I have international engineering education and experience; how is that evaluated by PEO? How is my engineering experience evaluated by PEO? How do I prepare my Experience Record? What is the PPE? What is the EIT Program and the Student Membership Program?`
+              },
+              {
+                  tag: 'p',
+                  text: `Emphasis of this webinar will be on the P. Eng. licensure process - what PEO is looking for. The webinar will focus on how engineering experience is evaluated and how you can facilitate the process. Learn the dos and don’ts of preparing your licensing application for evaluation. To participate, you will require: internet connection to view the webinar, and/or phone access to dial into the conference for audio.`
+              },
+
+              {
+                  tag: 'p',
+                  text: `We look forward to speaking with you!`
+              },
                 {
                     tag: 'button',
                     attr: {
@@ -59,12 +76,17 @@ module.exports = {
                 },
                 {
                     tag: 'p',
-                    text: `We are kicking off the new year with an informal time of chatting and Q&A. In this event you will meet the new team of executives as well as participate in an informal chat about the volunteering activities and events we organize for young engineers and engineering students! Learn more about IEEE Young Professionals! See the importance we have in contributing to this wonderfull community!`
+                    text: `We are kicking off the new year with an informal time of chatting and Q&A. In this event you will meet the new team of executives as well as participate in an informal chat about the volunteering activities and events we organize for young engineers and engineering students! Learn more about IEEE Young Professionals! See the importance we have in contributing to this wonderful community!`
                 },
                 {
                     tag: 'p',
-                    text: `Feel free to submit any possible question you might have in the registration form! We are looking forward to see you there! Register now!`
+                    text: `Feel free to submit any possible question you might have in the registration form!`
                 },
+                {
+                    tag: 'p',
+                    text: `We are looking forward to see you there! Register now!`
+                },
+
                 {
                     tag: 'button',
                     attr: {
