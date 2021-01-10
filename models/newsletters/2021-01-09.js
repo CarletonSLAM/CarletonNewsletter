103e798d4693ee888db840cf151ff3ef7a63b218
brian_yang4444 <briany4444@gmail.com>
2021-01-09 23:14:31 -0500

    newsletter Jan 9th 2021

---
+++ models/newsletters/2021-01-09.js
@@ -0,0 +1,92 @@
+module.exports = {
+    toast: [
+        {
+            tag: 'h2',
+            text: 'Keeping up with IEEE Carleton.'
+        },
+        {
+            tag: 'p',
+            text: 'Feel free to forward this newsletter to anyone you think might be interested.'
+        },
+        {
+            tag: 'htmlString',
+            text: `If you are having trouble viewing this as an email, click <a href='http://staylateandmake.ca/CarletonNewsletter/output/2021-01-09.html'>here</a> to view in browser.`
+        }
+    ],
+    articles: [
+        // --- New Event --- //
+        {
+            title: `Clarifying the path to becoming a Professional Engineer`,
+            date: `Friday March 5, 2021`,
+            time: `4:00 PM - 5:00 PM`,
+            location: `Online Event`,
+            img: `PEO2021-01 - João Guilherme Nizer Rahmeier.png`,
+            body: [
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://bit.ly/YPPEO21'
+                    },
+                    text: `REGISTER`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.linkedin.com/company/65487492'
+                    },
+                    text: `LINKEDIN`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.peo.on.ca'
+                    },
+                    text: `Professional Engineers Ontario Website`
+                },
+            ]
+        },
+        // --- New Event --- //
+        {
+            title: `IEEE Ottawa YPAG: Meet the Team`,
+            date: `Thursday January 28, 2021`,
+            time: `6:00 PM - 7:00 PM`,
+            location: `Online Event`,
+            img: `meet the team_poster - João Guilherme Nizer Rahmeier.jpg`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `Are you interested to learn more on what IEEE Young Professionals Ottawa is all about?`
+                },
+                {
+                    tag: 'p',
+                    text: `We are kicking off the new year with an informal time of chatting and Q&A. In this event you will meet the new team of executives as well as participate in an informal chat about the volunteering activities and events we organize for young engineers and engineering students! Learn more about IEEE Young Professionals! See the importance we have in contributing to this wonderfull community!`
+                },
+                {
+                    tag: 'p',
+                    text: `Feel free to submit any possible question you might have in the registration form! We are looking forward to see you there! Register now!`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.eventbrite.ca/e/ieee-ottawa-ypag-meet-the-team-tickets-136095189375'
+                    },
+                    text: `REGISTER`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.linkedin.com/company/65487492'
+                    },
+                    text: `LINKEDIN`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/IEEEYPOttawa'
+                    },
+                    text: `FACEBOOK`
+                },
+            ]
+        },
+    ]
+}
