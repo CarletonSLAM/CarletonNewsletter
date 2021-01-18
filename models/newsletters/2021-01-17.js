cd3fc59cb5e41004503854342ff582be346f5351
brian_yang4444 <briany4444@gmail.com>
2021-01-17 23:01:55 -0500

    Jan 17

---
+++ models/newsletters/2021-01-17.js
@@ -0,0 +1,140 @@
+module.exports = {
+    toast: [
+        {
+            tag: 'h2',
+            text: 'Keeping up with IEEE Carleton.'
+        },
+        {
+            tag: 'p',
+            text: 'Feel free to forward this newsletter to anyone you think might be interested.'
+        },
+        {
+            tag: 'htmlString',
+            text: `If you are having trouble viewing this as an email, click <a href='http://staylateandmake.ca/CarletonNewsletter/output/2021-01-17.html'>here</a> to view in browser.`
+        }
+    ],
+    articles: [
+        // --- New Event --- //
+        {
+            title: `Clarifying the path to becoming a Professional Engineer`,
+            date: `Friday March 5, 2021`,
+            time: `4:00 PM - 5:00 PM`,
+            location: `Online Event`,
+            img: `PEO2021-01 - João Guilherme Nizer Rahmeier.png`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `If you are an engineering undergraduate or post-graduate student, or a recent graduate starting out your engineering career, then this seminar might be for you.`
+                },
+                {
+                    tag: 'p',
+                    text: `In it you will learn: What is PEO? What engineering experience is PEO looking for once I graduate? I have international engineering education and experience; how is that evaluated by PEO? How is my engineering experience evaluated by PEO? How do I prepare my Experience Record? What is the PPE? What is the EIT Program and the Student Membership Program?`
+                },
+                {
+                    tag: 'p',
+                    text: `Emphasis of this webinar will be on the P. Eng. licensure process - what PEO is looking for. The webinar will focus on how engineering experience is evaluated and how you can facilitate the process. Learn the dos and don’ts of preparing your licensing application for evaluation. To participate, you will require: internet connection to view the webinar, and/or phone access to dial into the conference for audio. We look forward to speaking with you!`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://bit.ly/YPPEO21'
+                    },
+                    text: `REGISTER`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.linkedin.com/company/65487492'
+                    },
+                    text: `LINKEDIN`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.peo.on.ca'
+                    },
+                    text: `OUR WEBSITE`
+                },
+            ]
+        },
+        // --- New Event --- //
+        {
+            title: `IEEE Ottawa YPAG: Meet the Team`,
+            date: `Thursday January 28, 2021`,
+            time: `6:00 PM`,
+            time: `7:00 PM`,
+            location: `Online Event`,
+            img: `meet the team_poster - João Guilherme Nizer Rahmeier.jpg`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `Are you interested to learn more on what IEEE Young Professionals Ottawa is all about?`
+                },
+                {
+                    tag: 'p',
+                    text: `We are kicking off the new year with an informal time of chatting and Q&A. In this event you will meet the new team of executives as well as participate in an informal chat about the volunteering activities and events we organize for young engineers and engineering students! Learn more about IEEE Young Professionals! See the importance we have in contributing to this wonderfull community!`
+                },
+                {
+                    tag: 'p',
+                    text: `Feel free to submit any possible question you might have in the registration form! We are looking forward to see you there! Register now!`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.eventbrite.ca/e/ieee-ottawa-ypag-meet-the-team-tickets-136095189375'
+                    },
+                    text: `REGISTER`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.linkedin.com/company/65487492'
+                    },
+                    text: `LINKEDIN`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/IEEEYPOttawa'
+                    },
+                    text: `FACEBOOK`
+                },
+            ]
+        },
+        // --- New Event --- //
+        {
+            title: `Welcome to Ottawa's LARGEST 24-Hour LAN competition for Charity!`,
+            date: `Tuesday February 16, 2021`,
+            time: `6:00 PM - 6:00 PM`,
+            location: `Online Event`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `From intense gaming to extraordinary prizes and unbeatable bragging rights, Battle Royale has it all.`
+                },
+                {
+                    tag: 'p',
+                    text: `Every year, over 100 competitors bring their best to Ottawa's largest 24-hour gaming event, for a chance to win incredible prizes - and you could be one of them!`
+                },
+                {
+                    tag: 'p',
+                    text: `If you think you have what it takes to rise to the top or simply want to have a good time then Battle Royale is for you.`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://discordapp.com/invite/fFqpypN'
+                    },
+                    text: `DISCORD LINK`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://br13.netlify.app/'
+                    },
+                    text: `WEBSITE`
+                },
+            ]
+        },
+    ]
+}
