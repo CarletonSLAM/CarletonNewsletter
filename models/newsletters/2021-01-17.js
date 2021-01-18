b514015ec40e7d014c8e72e3880aea037b6c2d6b
brian_yang4444 <briany4444@gmail.com>
2021-01-17 23:26:51 -0500

    images corrupt

--- models/newsletters/2021-01-17.js
+++ models/newsletters/2021-01-17.js
@@ -10,7 +10,7 @@ module.exports = {
         },
         {
             tag: 'htmlString',
-            text: `If you are having trouble viewing this as an email, click <a href='http://staylateandmake.ca/CarletonNewsletter/output/2021-01-17.html'>here</a> to view in browser.`
+            text: `If you are having trouble viewing this as an email or it's images, click <a href='http://staylateandmake.ca/CarletonNewsletter/output/2021-01-17.html'>here</a> to view in browser.`
         }
     ],
     articles: [
