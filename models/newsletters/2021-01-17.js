37036e55c2f85a3984cccf1249f0677a1e738ecb
brian_yang4444 <briany4444@gmail.com>
2021-01-17 23:41:07 -0500

    png

--- models/newsletters/2021-01-17.js
+++ models/newsletters/2021-01-17.js
@@ -20,7 +20,7 @@ module.exports = {
             date: `Friday March 5, 2021`,
             time: `4:00 PM - 5:00 PM`,
             location: `Online Event`,
-            img: `PEO2021-01 - João Guilherme Nizer Rahmeier.png`,
+            img: `PEO2021-01 - João Guilherme Nizer Rahmeier.jpg`,
             body: [
                 {
                     tag: 'p',
