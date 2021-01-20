d045e9e101d0b0a1f5f10de34bae53db497cc715
brian_yang4444 <briany4444@gmail.com>
2021-01-20 00:23:48 -0500

    new this week

---
+++ models/newsletters/2021-01-20.js
@@ -0,0 +1,174 @@
+module.exports = {
+    toast: [
+        {
+            tag: 'h2',
+            text: 'Keeping up with IEEE Carleton.'
+        },
+        {
+            tag: 'p',
+            text: 'Feel free to forward this newsletter to anyone you think might be interested.'
+        },
+        {
+            tag: 'htmlString',
+            text: `If you are having trouble viewing this as an email, click <a href='http://staylateandmake.ca/CarletonNewsletter/output/2021-01-20.html'>here</a> to view in browser.`
+        }
+    ],
+    articles: [
+        // --- New Event --- //
+        {
+            title: `Clarifying the path to becoming a Professional Engineer`,
+            date: `Friday March 5, 2021`,
+            time: `4:00 PM - 5:00 PM`,
+            location: `Online Event`,
+            img: `clarify.jpg`,
+            body: [
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://bit.ly/YPPEO21'
+                    },
+                    text: `REGISTER`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.linkedin.com/company/65487492'
+                    },
+                    text: `LINKEDIN`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.peo.on.ca'
+                    },
+                    text: `PEO Website`
+                },
+            ]
+        },
+        // --- New Event --- //
+        {
+            title: `IEEE Ottawa YPAG: Meet the Team`,
+            date: `Thursday January 28, 2021`,
+            time: `6:00 PM - 7:00 PM`,
+            location: `Online Event`,
+            img: `meet.png`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `Are you interested to learn more on what IEEE Young Professionals Ottawa is all about?`
+                },
+                {
+                    tag: 'p',
+                    text: `We are kicking off the new year with an informal time of chatting and Q&A.`
+                },
+                {
+                    tag: 'p',
+                    text: `In this event you will meet the new team of executives as well as participate in an informal chat about the volunteering activities and events we organize for young engineers and engineering students! Learn more about IEEE Young Professionals! See the importance we have in contributing to this wonderful community! Feel free to submit any possible question you might have in the registration form!`
+                },
+                {
+                    tag: 'p',
+                    text: `We are looking forward to see you there! Register now!`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.eventbrite.ca/e/ieee-ottawa-ypag-meet-the-team-tickets-136095189375'
+                    },
+                    text: `REGISTER`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.linkedin.com/company/65487492'
+                    },
+                    text: `LINKEDIN`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/IEEEYPOttawa'
+                    },
+                    text: `FACEBOOK`
+                },
+            ]
+        },
+        // --- New Event --- //
+        {
+            title: `Webinar: Honing Your Research Skills`,
+            date: `Monday February 15, 2021`,
+            time: `4:00 PM - 5:00 PM`,
+            location: `Online Event`,
+            img: `researchSkills.png`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `Interested in improving your research skills? This webinar is for you!!`
+                },
+                {
+                    tag: 'p',
+                    text: `This webinar will cover general research skills necessary for success in engineering research, and is intended to undergraduate students, new graduate students, or anyone starting or getting back into engineering research. Topics covered will include journal paper reading and writing, academic metrics, research communication, problem solving, and more!`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'http://bit.ly/YPOSKILLS'
+                    },
+                    text: `REGISTER`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.linkedin.com/events/6757363321290207232/about/'
+                    },
+                    text: `LINKEDIN`
+                },
+            ]
+        },
+        // --- New Event --- //
+        {
+            title: `IEEE Student Professional Awareness Conference (SPAC)`,
+            date: `Thursday January 21, 2021`,
+            time: `5:30 PM`,
+            location: `Online Event`,
+            body: [
+                {
+                    tag: 'p',
+                    text: `Hey guys! IEEE’s annual student professional awareness VIRTUAL conference is coming up this Thursday, January 21, 2021 at 5:30 pm EST and we invite all streams of engineering students to come network and build connections!`
+                },
+                {
+                    tag: 'p',
+                    text: `The IEEE Student Professional Awareness Conference (SPAC) is an annual formal dinner event that serves esteemed professionals and allows them to engage with engineering and computer science students within their fields. This year the conference will be held online to maintain health and safety regulations as due to the current circumstances regarding the COVID-19 pandemic. SPAC provides students, academics, and industry professionals a chance to network and bridge the gap between classrooms and boardrooms, thereby empowering its attendees to build professional connections and form a bond.`
+                },
+                {
+                    tag: 'p',
+                    text: `It’s completely free so make sure to register because it’s an opportunity you don’t want to miss out on!`
+                },
+                {
+                    tag: 'p',
+                    text: `Check out our Instagram and Facebook for more information and register on ieeespac.ca.`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://ieeespac.ca/'
+                    },
+                    text: `IEEE SPAC Website`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.facebook.com/ieeespacottawa/'
+                    },
+                    text: `FACEBOOK`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://www.linkedin.com/company/spacottawa'
+                    },
+                    text: `LINKEDIN`
+                },
+            ]
+        },
+    ]
+}
