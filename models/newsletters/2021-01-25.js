b2ee085004e0ef211a118d1322e26b66cc4aead8
brian_yang4444 <briany4444@gmail.com>
2021-01-25 00:15:08 -0500

    25 jan

---
+++ models/newsletters/2021-01-25.js
@@ -0,0 +1,166 @@
+module.exports = {
+    toast: [
+        {
+            tag: 'h2',
+            text: 'Keeping up with IEEE Carleton.'
+        },
+        {
+            tag: 'p',
+            text: 'Feel free to forward this newsletter to anyone you think might be interested.'
+        },
+        {
+            tag: 'htmlString',
+            text: `If you are having trouble viewing this as an email, click <a href='http://staylateandmake.ca/CarletonNewsletter/output/2021-01-25.html'>here</a> to view in browser.`
+        }
+    ],
+    articles: [
+        // --- New Event --- //
+
+          {
+              title: `Clarifying the path to becoming a Professional Engineer`,
+              date: `Friday March 5, 2021`,
+              time: `4:00 PM - 5:00 PM`,
+              location: `Online Event`,
+              img: `clarify.jpg`,
+              body: [
+                  {
+                      tag: 'button',
+                      attr: {
+                          href: 'https://bit.ly/YPPEO21'
+                      },
+                      text: `REGISTER`
+                  },
+                  {
+                      tag: 'button',
+                      attr: {
+                          href: 'https://www.linkedin.com/company/65487492'
+                      },
+                      text: `LINKEDIN`
+                  },
+                  {
+                      tag: 'button',
+                      attr: {
+                          href: 'https://www.peo.on.ca'
+                      },
+                      text: `PEO Website`
+                  },
+              ]
+          },
+          // --- New Event --- //
+          {
+              title: `IEEE Ottawa YPAG: Meet the Team`,
+              date: `Thursday January 28, 2021`,
+              time: `6:00 PM - 7:00 PM`,
+              location: `Online Event`,
+              img: `meet.png`,
+              body: [
+                  {
+                      tag: 'p',
+                      text: `Are you interested to learn more on what IEEE Young Professionals Ottawa is all about?`
+                  },
+                  {
+                      tag: 'p',
+                      text: `We are kicking off the new year with an informal time of chatting and Q&A.`
+                  },
+                  {
+                      tag: 'p',
+                      text: `In this event you will meet the new team of executives as well as participate in an informal chat about the volunteering activities and events we organize for young engineers and engineering students! Learn more about IEEE Young Professionals! See the importance we have in contributing to this wonderful community! Feel free to submit any possible question you might have in the registration form!`
+                  },
+                  {
+                      tag: 'p',
+                      text: `We are looking forward to see you there! Register now!`
+                  },
+                  {
+                      tag: 'button',
+                      attr: {
+                          href: 'https://www.eventbrite.ca/e/ieee-ottawa-ypag-meet-the-team-tickets-136095189375'
+                      },
+                      text: `REGISTER`
+                  },
+                  {
+                      tag: 'button',
+                      attr: {
+                          href: 'https://www.linkedin.com/company/65487492'
+                      },
+                      text: `LINKEDIN`
+                  },
+                  {
+                      tag: 'button',
+                      attr: {
+                          href: 'https://www.facebook.com/IEEEYPOttawa'
+                      },
+                      text: `FACEBOOK`
+                  },
+              ]
+          },
+          // --- New Event --- //
+          {
+              title: `Webinar: Honing Your Research Skills`,
+              date: `Monday February 15, 2021`,
+              time: `4:00 PM - 5:00 PM`,
+              location: `Online Event`,
+              img: `researchSkills.png`,
+              body: [
+                  {
+                      tag: 'p',
+                      text: `Interested in improving your research skills? This webinar is for you!!`
+                  },
+                  {
+                      tag: 'p',
+                      text: `This webinar will cover general research skills necessary for success in engineering research, and is intended to undergraduate students, new graduate students, or anyone starting or getting back into engineering research. Topics covered will include journal paper reading and writing, academic metrics, research communication, problem solving, and more!`
+                  },
+                  {
+                      tag: 'button',
+                      attr: {
+                          href: 'http://bit.ly/YPOSKILLS'
+                      },
+                      text: `REGISTER`
+                  },
+                  {
+                      tag: 'button',
+                      attr: {
+                          href: 'https://www.linkedin.com/events/6757363321290207232/about/'
+                      },
+                      text: `LINKEDIN`
+                  },
+              ]
+          },
+
+          // --- New Event --- //
+          {
+              title: `Welcome to Ottawa's LARGEST 24-Hour LAN competition for Charity!`,
+              date: `Tuesday February 16, 2021`,
+              time: `6:00 PM - 6:00 PM`,
+              location: `Online Event`,
+              img: 'posterBr.png',
+              body: [
+                  {
+                      tag: 'p',
+                      text: `From intense gaming to extraordinary prizes and unbeatable bragging rights, Battle Royale has it all.`
+                  },
+                  {
+                      tag: 'p',
+                      text: `Every year, over 100 competitors bring their best to Ottawa's largest 24-hour gaming event, for a chance to win incredible prizes - and you could be one of them!`
+                  },
+                  {
+                      tag: 'p',
+                      text: `If you think you have what it takes to rise to the top or simply want to have a good time then Battle Royale is for you.`
+                  },
+                  {
+                      tag: 'button',
+                      attr: {
+                          href: 'https://discordapp.com/invite/fFqpypN'
+                      },
+                      text: `DISCORD LINK`
+                  },
+                  {
+                      tag: 'button',
+                      attr: {
+                          href: 'https://br13.netlify.app/'
+                      },
+                      text: `WEBSITE`
+                  },
+              ]
+          },
+    ]
+}
