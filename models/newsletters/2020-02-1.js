08222e1e658f79e6e776e10d6940bc5c38d77088
brian_yang4444 <briany4444@gmail.com>
2021-02-01 01:22:45 -0500

    new stuff

--- models/newsletters/2020-02-1.js
+++ models/newsletters/2020-02-1.js
@@ -15,6 +15,42 @@ module.exports = {
     ],
     articles: [
         // --- New Event --- //
+        // --- New Event --- //
+        {
+            title: `Welcome to Ottawa's LARGEST 24-Hour LAN competition for Charity!`,
+            date: `Tuesday February 16, 2021`,
+            time: `6:00 PM - 6:00 PM`,
+            location: `Online Event`,
+            img: 'br13.jpg',
+            body: [
+                {
+                    tag: 'p',
+                    text: `From intense gaming to extraordinary prizes and unbeatable bragging rights, Battle Royale has it all.`
+                },
+                {
+                    tag: 'p',
+                    text: `Every year, over 100 competitors bring their best to Ottawa's largest 24-hour gaming event, for a chance to win incredible prizes - and you could be one of them!`
+                },
+                {
+                    tag: 'p',
+                    text: `If you think you have what it takes to rise to the top or simply want to have a good time then Battle Royale is for you.`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://discordapp.com/invite/fFqpypN'
+                    },
+                    text: `DISCORD LINK`
+                },
+                {
+                    tag: 'button',
+                    attr: {
+                        href: 'https://br13.netlify.app/'
+                    },
+                    text: `WEBSITE`
+                },
+            ]
+        },
 
           {
               title: `Clarifying the path to becoming a Professional Engineer`,
@@ -58,53 +94,7 @@ module.exports = {
                   },
               ]
           },
-          // --- New Event --- //
-          {
-              title: `IEEE Ottawa YPAG: Meet the Team`,
-              date: `Thursday January 28, 2021`,
-              time: `6:00 PM - 7:00 PM`,
-              location: `Online Event`,
-              img: `meet.png`,
-              body: [
-                  {
-                      tag: 'p',
-                      text: `Are you interested to learn more on what IEEE Young Professionals Ottawa is all about?`
-                  },
-                  {
-                      tag: 'p',
-                      text: `We are kicking off the new year with an informal time of chatting and Q&A.`
-                  },
-                  {
-                      tag: 'p',
-                      text: `In this event you will meet the new team of executives as well as participate in an informal chat about the volunteering activities and events we organize for young engineers and engineering students! Learn more about IEEE Young Professionals! See the importance we have in contributing to this wonderful community! Feel free to submit any possible question you might have in the registration form!`
-                  },
-                  {
-                      tag: 'p',
-                      text: `We are looking forward to see you there! Register now!`
-                  },
-                  {
-                      tag: 'button',
-                      attr: {
-                          href: 'https://www.eventbrite.ca/e/ieee-ottawa-ypag-meet-the-team-tickets-136095189375'
-                      },
-                      text: `REGISTER`
-                  },
-                  {
-                      tag: 'button',
-                      attr: {
-                          href: 'https://www.linkedin.com/company/65487492'
-                      },
-                      text: `LINKEDIN`
-                  },
-                  {
-                      tag: 'button',
-                      attr: {
-                          href: 'https://www.facebook.com/IEEEYPOttawa'
-                      },
-                      text: `FACEBOOK`
-                  },
-              ]
-          },
+
           // --- New Event --- //
           {
               title: `Webinar: Honing Your Research Skills`,
@@ -134,43 +124,6 @@ module.exports = {
                           href: 'https://www.linkedin.com/events/6757363321290207232/about/'
                       },
                       text: `LINKEDIN`
-                  },
-              ]
-          },
-
-          // --- New Event --- //
-          {
-              title: `Welcome to Ottawa's LARGEST 24-Hour LAN competition for Charity!`,
-              date: `Tuesday February 16, 2021`,
-              time: `6:00 PM - 6:00 PM`,
-              location: `Online Event`,
-              img: 'br13.jpg',
-              body: [
-                  {
-                      tag: 'p',
-                      text: `From intense gaming to extraordinary prizes and unbeatable bragging rights, Battle Royale has it all.`
-                  },
-                  {
-                      tag: 'p',
-                      text: `Every year, over 100 competitors bring their best to Ottawa's largest 24-hour gaming event, for a chance to win incredible prizes - and you could be one of them!`
-                  },
-                  {
-                      tag: 'p',
-                      text: `If you think you have what it takes to rise to the top or simply want to have a good time then Battle Royale is for you.`
-                  },
-                  {
-                      tag: 'button',
-                      attr: {
-                          href: 'https://discordapp.com/invite/fFqpypN'
-                      },
-                      text: `DISCORD LINK`
-                  },
-                  {
-                      tag: 'button',
-                      attr: {
-                          href: 'https://br13.netlify.app/'
-                      },
-                      text: `WEBSITE`
                   },
               ]
           },
