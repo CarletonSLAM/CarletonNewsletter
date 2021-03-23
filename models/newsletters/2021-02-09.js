e8c92d4d99f9ba0b2879705b1585e10fee5365d7
brian_yang4444 <briany4444@gmail.com>
2021-03-23 01:24:34 -0400

    asd

--- models/newsletters/2021-02-09.js
+++ models/newsletters/2021-02-09.js
@@ -14,92 +14,44 @@ module.exports = {
         }
     ],
     articles: [
-      {
-          title: `IEEE Annual Election Season`,
-          img: `elections.png`,
-          body: [
-            {
-                tag: 'p',
-                text: `Want to be an exec ? Gain some leadership experience and have fun on a team? Be a leader in Carleton and Global engineering community?`
-            },
-            {
-                tag: 'p',
-                text: ` Unlike in the past, this year we will be holding two elections, the Officers Elections and the Director Election, and each will have their own separate application form respectively.`
-            },
-            {
-              tag:'p',
-              text:`If you're interested in becoming part of the next IEEE Carleton executive team, please submit the application form seen below.`
-            },
-              {
-                  tag: 'button',
-                  attr: {
-                      href: 'https://accounts.google.com/ServiceLogin?service=wise&passive=1209600&continue=https://docs.google.com/document/u/1/d/1je1tLaQi_KDuUjpjZO3oXb4T_D5LMV7ZTPAMHmp9Obg/edit&followup=https://docs.google.com/document/u/1/d/1je1tLaQi_KDuUjpjZO3oXb4T_D5LMV7ZTPAMHmp9Obg/edit&ltmpl=docs&authuser=1'
-                  },
-                  text: `HANDBOOK`
-              },
-              {
-                  tag: 'button',
-                  attr: {
-                      href: 'https://docs.google.com/forms/d/e/1FAIpQLScFH9Mes0ftM1Kpstc4d1NB-vZqixpc_2tffi_ROSrHtJ95JQ/viewform'
-                  },
-                  text: `DIRECTORSHIP FORM`
-              },
-          ]
-      },
-
-      {
-          title: `Professional Development Series: Landing your Dream Job`,
-          date: 'March 14 @ 12:00 pm - 1:00 pm',
-          img: `pds4.jpg`,
-          body: [
-            {
-                tag: 'p',
-                text: `With the onset of the pandemic from early March 2020, nearly everything from shopping groceries to job recruitment is being virtualized. It completely changed the landscape for graduating students to get a job. Not just students even organizations are finding it difficult to recruit the perfect candidate for the position. Are you a recent graduate student looking for a job? Are you in your final term of school and wondering how to get your dream job?
-IEEE WIE Ottawa along with IEEE YP Ottawa brings to you Professional Development Series! Join us for our 4th event of the Professional Development Series – Landing your dream job. The event will discuss how one should approach job search in a way they could easily get their dream job during this pandemic. It is going to be delivered by Tegbir Singh – data analyst professional. He will be sharing his experience of successfully landed on his job during the pandemic.`
-            },
-              {
-                  tag: 'button',
-                  attr: {
-                      href: 'https://wie.ieeeottawa.ca/event/professional-development-series-landing-your-dream-job/'
-                  },
-                  text: `EVENT SIGNUP`
-              },
-          ]
-      },
-
 
       // --- New Event --- //
       {
-          title: `Seminar on Leaky-Wave Antennas for 5G Applications`,
-          date: `Friday March 19, 2021`,
-          time: `4:00 PM - 5:00 OM`,
+          title: `IEEE and SREE Trivia Night`,
+          date: `Wednesday March 24, 2021`,
+          time: `7:00 PM`,
           location: `Online`,
-          img: `localSquare.jpg`,
+          img: `triv.png`,
           body: [
               {
                   tag: 'p',
-                  text: `Join us for a workshop with Nima Javanbakht, who is currently pursuing a Ph.D. degree in electrical engineering at Carleton University. His research interest includes analysis and design of leaky-wave antennas, sidelobe suppression, reconfigurable antennas, compact microwave sensors, and 5G mm-wave applications.`
+                  text: `Join IEEE on Wednesday @ 7pm for IEEE and SREE Trivia Night! From science to pop-culture, you and your three teammates could win amazing prizes! Sign up with IEEE using the docs page!`
+              },
+              {
+                  tag: 'p',
+                  text: `Where: SREESociety and IEEE discords`
+              },
+              {
+                  tag: 'p',
+                  text: `When: Wednesday 24, 2021 @7PM`
+              },
+              {
+                  tag: 'p',
+                  text: `Wanna join? Please fill out the form below:`
               },
               {
                   tag: 'button',
                   attr: {
-                      href: 'https://www.eventbrite.ca/e/leaky-wave-antennas-for-5g-applications-tickets-142521139565'
+                      href: 'https://forms.gle/xKw9jxMSiotihqcg9'
                   },
-                  text: `REGISTER`
+                  text: `SIGN UP`
               },
               {
                   tag: 'button',
                   attr: {
-                      href: 'https://www.facebook.com/events/332308274858513'
+                      href: 'https://zoom.us/j/91904620400?pwd=cDRlcW45ZWxpUXlLejVEaTdzdlQ2QT09'
                   },
-                  text: `FACEBOOK`
-              },
-              {
-                  tag: 'button',
-                  attr: {
-                      href: 'https://www.linkedin.com/events/6769633183890771969/'
-                  },
-                  text: `LINKEDIN`
+                  text: `ZOOM LINK`
               },
           ]
       },
