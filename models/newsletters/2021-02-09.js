a22fc5679bc37b479015cdf0d0ffc1c5e5180c11
brian_yang4444 <briany4444@gmail.com>
2021-04-20 20:24:24 -0400

    asd

--- models/newsletters/2021-02-09.js
+++ models/newsletters/2021-02-09.js
@@ -17,33 +17,38 @@ module.exports = {
 
       // --- New Event --- //
       {
-          title: `IEEE Year End Debate Night`,
-          date: `April 14, 2021`,
-          time: `7:00 PM`,
+          title: `Share your Experience - Call for Speakers`,
+          time: `TBA`,
           location: `Online`,
-          img: `debate.png`,
+          img: `summer.jpg`,
           body: [
               {
                   tag: 'p',
-                  text: `Join IEEE on the last day of the year for a night of debates on various random topics! Sign up as a team of 2-4 and compete against other societies and see who will be crowned C-Eng’s best debate team! May the most absurd arguments win!! Also there's a chill and study zone where we play games and stuff. WINNERS GET PRIZES`
+                  text: `Have you recently (or not-so-recently) transitioned from university or academia to a STEM career? We are planning a series of talks this summer to help the next generation of grads make this transition; they would love to hear your experiences and insights as they make the jump. Potential topics include:`
               },
               {
-                  tag: 'p',
-                  text: `Wanna join? Please fill out the form below:`
+                tag: 'p',
+                text:`Industry vs. grad school`
               },
               {
-                  tag: 'button',
-                  attr: {
-                      href: 'https://docs.google.com/forms/d/e/1FAIpQLSc_sWXuJyi49Wqc-3TP8wt7qiHfzI9ZCXJSr0uwXMIbruaOzA/viewform'
-                  },
-                  text: `SIGN UP`
+                tag: 'p',
+                text:`Looking for a job`
               },
               {
-                  tag: 'button',
-                  attr: {
-                      href: 'https://discord.gg/vbDKznwXdp'
-                  },
-                  text: `IEEE DISCORD`
+                tag: 'p',
+                text:`Personal testimonies`
+              },
+              {
+                tag: 'p',
+                text:`Personal life as a professional`
+              },
+              {
+                tag: 'p',
+                text:`Keeping up to date`
+              },
+              {
+                tag: 'p',
+                text: `Please reach out if you are interested in speaking at one of the events.`
               },
           ]
       },
