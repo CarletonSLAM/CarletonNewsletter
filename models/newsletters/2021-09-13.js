f62a3df09e2fdb69240be1fd3047db1ef665fd3a
brian_yang4444 <briany4444@gmail.com>
2021-09-19 18:08:27 -0400

    asd

--- models/newsletters/2021-09-13.js
+++ models/newsletters/2021-09-13.js
@@ -16,66 +16,75 @@ module.exports = {
     articles: [
       // --- New Event --- //
       {
-          title: `CAIS Game Night`,
-          time: `Wednesday September 15th, 7:00pm - 9:00pm EST`,
-          location: `CAIS Discord`,
-          img: `CopyofGame_Night_Poster.png`,
+          title: `Learn about the Internet of THings`,
+          time: `Wednesday September 20th, 21st, 10 AM`,
+          location: `ZOOM`,
           body: [
               {
                   tag: 'p',
-                  text: `Have you recently (or not-so-recently) transitioned from university or academia to a STEM career? We are planning a series of talks this summer to help the next generation of grads make this transition; they would love to hear your experiences and insights as they make the jump. Potential topics include:`
+                  text: `Come learn all about the Internet of Things!
+Professor Maede Ashouri will do an introduction to IoT security requirements and some of the well-known IoT security architectures for the first day, before delving into the security requirements of some of the applications of IoT, including smart grid, smart life, smart health, and many more on the second day.
+The workshop is open to both graduate and undergraduate students.`
               },
               {
                 tag: 'p',
-                text:`Registration End Date: Thursday, September 16 2021`
+                text:`Day 1: Introduction to IoT security requirements and some of the well-known IoT security architectures`
               },
               {
                 tag: 'p',
-                text:`Join the Carleton Artificial Intelligence Society for our first Game Night of the year on Wednesday, Sept. 15 at 7 pm EST!`
+                text:`Day 2: Security requirements of the applications of IoT, including smart grid, smart life, smart health, etc`
               },
               {
                 tag: 'p',
-                text:`You will get to know more about CAIS and other attendees. See you there!!`
+                text:`The first day of the workshop will be hosted on zoom at the following link on September 20th at 10 am:`
               },
               {
                 tag: 'button',
                 attr: {
-                    href: 'https://discord.gg/BPqSRnaC'
+                    href: 'https://us04web.zoom.us/j/77041188966#success'
                 },
-                text:`Discord`
+                text:`DAY 1 LINK`
+              },
+              {
+                tag: 'p',
+                text:`The second day of the workshop will be hosted on zoom at the following link on September 21st at 10 am:`
+              },
+              {
+                tag: 'button',
+                attr: {
+                    href: 'https://us02web.zoom.us/j/87940656720#success'
+                },
+                text:`DAY 2 LINK`
+              },
+              {
+                tag: 'button',
+                attr: {
+                    href: 'https://www.facebook.com/ieeecarleton/photos/a.201988126536758/4364443543624508/?type=3'
+                },
+                text:`FACEBOOK`
               },
           ]
       },
 
       {
-          title: `Computer Science Clubs and Societies Info Session`,
-          time: `Saturday Sept. 18, 9:00 AM EST`,
+          title: `CCSS Tech Talk Jason Bernard`,
+          time: `Saturday Sept.20, 6PM EDT`,
           location: `Online`,
-          img: `CCSS_CSStuClubExpo.jpg`,
           body: [
             {
                 tag: 'p',
-                text: `Are you an incoming or returning CS student looking to get involved this year, but not sure how? `
+                text: `On September 20th at 6PM EDT, the Carleton Computer Science Science will be hosting Dr. Jason Bernard, a postdoctoral affiliate at the Vector Institute for Artificial Intelligence, and a research assistant at Athabasca University. `
             },
             {
                 tag: 'p',
-                text: `Well, look no further! To kick off (and close off) Carleton Computer Science Society’s Week of Awesome, we are collaborating with clubs and societies affiliated with the School of Computer Science (SCS) to bring you an exciting, virtual CS Clubs & Societies Information Session!`
+                text: ` This presentation will discuss Dr. Bernard’s work on inferring L-systems and RGGs using artificial intelligence techniques. Additionally, some practical applications will be presented such as plant modelling and neural activity to demonstrate the widespread potential utility of this research.`
             },
             {
-                tag: 'p',
-                text: `This info session will have two identical sessions on Sunday, September 12 @ 1:00 pm EDT and Saturday, September 18 @ 9:00 am EDT.`
-            },
-            {
-                tag: 'p',
-                text: `Every club and society will speak for approximately 5 minutes each, and there will be a 30 minutes Q&A session at the end.`
-            },
-            {
-                tag: 'p',
-                text: `An email will be going out to all CS students soon with the link to the event. So be sure to check your Carleton email account within the next 2 weeks!`
-            },
-            {
-                tag: 'p',
-                text: `We hope to see you all there!!`
+                tag: 'button',
+                attr: {
+                    href: 'https://ccss.carleton.ca/events/inferring-the-algorithms-used-by-different-processes/'
+                },
+                text: `LINK`
             },
 
           ]
