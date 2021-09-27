6e1e9c58bf07823d5755629537df0d3eed8d2b3d
brian_yang4444 <briany4444@gmail.com>
2021-09-26 22:40:23 -0400

    1

--- models/newsletters/2021-09-19.js
+++ models/newsletters/2021-09-19.js
@@ -16,101 +16,46 @@ module.exports = {
     articles: [
       // --- New Event --- //
       {
-          title: `Learn about the Internet of Things`,
-          time: `Wednesday September 20th, 21st, 10 AM`,
-          location: `ZOOM`,
+          title: `AI coffee Hour 2: Reinformcement Learning`,
+          time: `Thursday October 7th, 21st, 6:00 - 7:30 PM`,
+          location: `MS Teams`,
+          img: `Carleton AI Society.png`,
           body: [
               {
                   tag: 'p',
-                  text: `Come learn all about the Internet of Things!
-Professor Maede Ashouri will do an introduction to IoT security requirements and some of the well-known IoT security architectures for the first day, before delving into the security requirements of some of the applications of IoT, including smart grid, smart life, smart health, and many more on the second day.
-The workshop is open to both graduate and undergraduate students.`
+                  text: `Presenting CAIS’s AI Coffee Hour! Our AI Coffee hour is a social event where everyone is invited to bring a cup of coffee and chat with the CAIS members about anything relating to Artificial Intelligence and Machine Learning. Want to exchange where you get your AI news and research? Feeling good or need help with your personal AI project? This is the perfect place to meet fellow AI enthusiasts. The discussions will be lightly guided by new themes every week, but nothing AI-related is off-limits. No prior experience or knowledge is required.`
               },
               {
                 tag: 'p',
-                text:`Day 1: Introduction to IoT security requirements and some of the well-known IoT security architectures`
-              },
-              {
-                tag: 'p',
-                text:`Day 2: Security requirements of the applications of IoT, including smart grid, smart life, smart health, etc`
-              },
-              {
-                tag: 'p',
-                text:`The first day of the workshop will be hosted on zoom at the following link on September 20th at 10 am:`
+                text:`This week we’ll be discussing reinforcement learning and other related AI/ML topics.`
               },
               {
                 tag: 'button',
                 attr: {
-                    href: 'https://us04web.zoom.us/j/77041188966#success'
+                    href: 'https://teams.microsoft.com/dl/launcher/launcher.html?url=%2F_%23%2Fl%2Fmeetup-join%2F19%3Ameeting_OTA4MmFkZDktM2E1My00NjQxLTk1MzUtOTdjZWM2MjRkODk3%40thread.v2%2F0%3Fcontext%3D%257b%2522Tid%2522%253a%25226ad91895-de06-485e-bc51-fce126cc8530%2522%252c%2522Oid%2522%253a%2522ab72a30b-581e-45d2-abc3-37cc554dfefb%2522%257d%26anon%3Dtrue&type=meetup-join&deeplinkId=8a32e66e-4779-4a9b-b8ed-5c8d7e3ffeed&directDl=true&msLaunch=true&enableMobilePage=true&suppressPrompt=true'
                 },
-                text:`DAY 1 LINK`
-              },
-              {
-                tag: 'p',
-                text:`The second day of the workshop will be hosted on zoom at the following link on September 21st at 10 am:`
-              },
-              {
-                tag: 'button',
-                attr: {
-                    href: 'https://us02web.zoom.us/j/87940656720#success'
-                },
-                text:`DAY 2 LINK`
-              },
-              {
-                tag: 'button',
-                attr: {
-                    href: 'https://www.facebook.com/ieeecarleton/photos/a.201988126536758/4364443543624508/?type=3'
-                },
-                text:`FACEBOOK`
+                text:`JOIN LINK`
               },
           ]
       },
-
       {
-          title: `CCSS Tech Talk Jason Bernard`,
-          time: `Saturday Sept.20, 6PM EDT`,
+          title: `Linear Regression Workshop`,
+          time: `Wednesday October 13th, 7:30 pM`,
           location: `Online`,
+          img: `CAIS WS2 lin reg.png`,
           body: [
             {
                 tag: 'p',
-                text: `On September 20th at 6PM EDT, the Carleton Computer Science Science will be hosting Dr. Jason Bernard, a postdoctoral affiliate at the Vector Institute for Artificial Intelligence, and a research assistant at Athabasca University. `
-            },
-            {
-                tag: 'p',
-                text: ` This presentation will discuss Dr. Bernard’s work on inferring L-systems and RGGs using artificial intelligence techniques. Additionally, some practical applications will be presented such as plant modelling and neural activity to demonstrate the widespread potential utility of this research.`
-            },
-            {
-                tag: 'button',
-                attr: {
-                    href: 'https://ccss.carleton.ca/events/inferring-the-algorithms-used-by-different-processes/'
-                },
-                text: `LINK`
-            },
-
-          ]
-      },
-      {
-          title: `Introduction to Scientific Python`,
-          time: `Monday, September 20th 2021, 5:30 PM - 7:30 PM `,
-          location: `Online`,
-          img: `IntroductiontoScientificPython.png`,
-          body: [
-            {
-                tag: 'p',
-                text: `In the introduction to the scientific python workshop, we will be delving into some of the most common data science tools that are used such as Numpy, Pandas, Sci-Kit Learn and matplotlib. The goal would be to showcase some of the common operations we use in standard data science/ machine learning workflows and to share some of the intuition behind the inner working of these libraries. This workshop is for those with a beginner understanding of Python. `
+                text: `In this workshop, we will be introducing one of the foundational algorithms in machine learning: linear regression. Linear regression is a learning algorithm that finds a linear relationship between the inputted features and the desired output. This workshop will cover the definition of linear regression, suggest situations where it might be useful, and explain how the algorithm works. Although some knowledge of linear algebra and calculus is needed to understand some of the technical details involved in linear regression, this workshop will be suitable for those who just want to get an intuition on how it works.`
             },
             {
                 tag: 'p',
                 text: `The following week, we will be offering Office Hours on Discord! The presenter and other CAIS executives will be available to answer questions about the workshop content and other information! Check it out:`
             },
             {
-                tag: 'p',
-                text: `Please make sure to register with Microsoft Teams to attend! Registration will be open till the end of the event.`
-            },
-            {
                 tag: 'button',
                 attr: {
-                    href: 'https://teams.microsoft.com/registration/lRjZagbeXki8UfzhJsyFMA,eqaDjyVuv0K2RTJhoBkv7Q,S2Faye49wEOo53wBS-3GVw,KrB6PGZ5FUuGkaKGaVP6Qw,GjeLFJyyckWCL3_scG7o4Q,ANg8--Lz9026q2jecLtofA?mode=read&tenantId=6ad91895-de06-485e-bc51-fce126cc8530&skipauthstrap=1'
+                    href: 'https://teams.microsoft.com/registration/lRjZagbeXki8UfzhJsyFMA,eqaDjyVuv0K2RTJhoBkv7Q,S2Faye49wEOo53wBS-3GVw,PDPVXpyRokOVQmJJD-A2bQ,dSP3F-53DUqzVKge5eckbw,GVjaRR3WGkKtzIQ0hAYr3w?mode=read&tenantId=6ad91895-de06-485e-bc51-fce126cc8530&skipauthstrap=1'
                 },
                 text: `REGISTRATION`
             },
