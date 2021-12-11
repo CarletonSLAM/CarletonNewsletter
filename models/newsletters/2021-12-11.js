d5db8d1e3ca9bee9c1f8dcee9f2cb42a8c1b97a9
brian_yang4444 <briany4444@gmail.com>
2021-12-11 02:24:24 -0500

    sdsad

--- models/newsletters/2021-12-11.js
+++ models/newsletters/2021-12-11.js
@@ -21,11 +21,11 @@ module.exports = {
         body: [
           {
             tag: 'p',
-            text:'Curious about the current state of AI? So are we! On the Carleton AI Show, we’re inviting guests from a plethora of different backgrounds to come and discuss how AI have affected their lives, how they perceive the future of AI, and much, much more! Join us, the Carleton AI Society, as we talk about all things AI with numerous industry professionals. Special thanks to Quddus Gordon for our intro and outro jingle, and the CAIS team for making this all possible.'
+            text:`Curious about the current state of AI? So are we! On the Carleton AI Show, we’re inviting guests from a plethora of different backgrounds to come and discuss how AI have affected their lives, how they perceive the future of AI, and much, much more! Join us, the Carleton AI Society, as we talk about all things AI with numerous industry professionals. Special thanks to Quddus Gordon for our intro and outro jingle, and the CAIS team for making this all possible.`
           },
           {
               tag: 'p',
-              text: 'Our fourth official podcast episode features new host Serdar Tuncer and his interview with Carleton University’s own Dr. Tracey P. Lauriault from the School of Journalism and Communication! Dr. Lauriault is an Associate Professor of Critical Media and Big Data. Her research covers open data, big data, open smart cities, open government, data sovereignty, data preservation and data governance. Join us as Dr. Lauriault discusses digital twins, trust in artificial intelligence and recommendations for future AI researchers from a critical data perspective. Host’s Twitter: @serdartun `
+              text: `Our fourth official podcast episode features new host Serdar Tuncer and his interview with Carleton University’s own Dr. Tracey P. Lauriault from the School of Journalism and Communication! Dr. Lauriault is an Associate Professor of Critical Media and Big Data. Her research covers open data, big data, open smart cities, open government, data sovereignty, data preservation and data governance. Join us as Dr. Lauriault discusses digital twins, trust in artificial intelligence and recommendations for future AI researchers from a critical data perspective. Host’s Twitter: @serdartun `
           },
           {
               tag: 'button',
