31d919c35e1779241b80bf2370fcdd21250c4bbf
brian_yang4444 <briany4444@gmail.com>
2022-01-17 01:19:31 -0400

    mn

--- models/newsletters/2021-12-18.js
+++ models/newsletters/2021-12-18.js
@@ -16,23 +16,65 @@ module.exports = {
     articles: [
       // --- New Event --- //
       {
-        title: `Carleton AI Show: Episode 4!`,
-        img: `tracy_updated.png`,
+        title: `Nasa Systems Engineering: Introduction To Project Life Cycle And Future Developments In Space Exploration`,
+        img: `nasa.jpeg`,
+        date: `January 28, 2022`,
+        time: `11:00 PM - 12:00 PM EST`,
+        location: `Online Event`,
         body: [
           {
             tag: 'p',
-            text:`Curious about the current state of AI? So are we! On the Carleton AI Show, we’re inviting guests from a plethora of different backgrounds to come and discuss how AI have affected their lives, how they perceive the future of AI, and much, much more! Join us, the Carleton AI Society, as we talk about all things AI with numerous industry professionals. Special thanks to Quddus Gordon for our intro and outro jingle, and the CAIS team for making this all possible.`
-          },
-          {
-              tag: 'p',
-              text: `Our fourth official podcast episode features new host Serdar Tuncer and his interview with Carleton University’s own Dr. Tracey P. Lauriault from the School of Journalism and Communication! Dr. Lauriault is an Associate Professor of Critical Media and Big Data. Her research covers open data, big data, open smart cities, open government, data sovereignty, data preservation and data governance. Join us as Dr. Lauriault discusses digital twins, trust in artificial intelligence and recommendations for future AI researchers from a critical data perspective. Host’s Twitter: @serdartun `
+            text: `
+Many engineers seeking to enter the space industry lack insight on project life cycle phases that may impede the project process and aptness to meet both requirements and deadlines. In this talk, we will go over the different phases that compose Project Life Cycle. This presentation will provide an overview of each phase, including: Concept Studies, Concept and Technology Development, Preliminary Design and Technology Completion, Final Design and Fabrication, System Assembly, Integration and Test, Launch, Operations and Sustainment, and Closeout. The presentation will conclude with an outlook of future technologies for space exploration and settlement.
+
+#engineering #ieee #youngprofessionals #projectlifecycle #technology #industry #NASA"`
           },
           {
               tag: 'button',
               attr: {
-                  href: 'https://carletonai.com/podcast/'
+                  href: 'https://events.vtools.ieee.org/event/register/299073'
               },
-              text: `LINK`
+              text: `REGISTER`
+          },
+        ]
+      },
+      {
+        title: `Intro to Neural Networks`,
+        img: `neural.png`,
+        date: `Wednesday January 19, 2022`,
+        time: `6:00 PM - 8:00 PM EST`,
+        location: `Online Event`,
+        body: [
+          {
+            tag: 'p',
+            text: `Deep Learning is the technology behind countless innovations in the past few years with applications in autonomous driving, biomedical informatics, image recognition and natural language processing. But what is deep learning, and how does it work? In this workshop, we will go over the foundational concepts of deep learning, particularly neural networks and how simple parts can build to form a complex system capable of modelling complex relationships.`
+          },
+          {
+              tag: 'button',
+              attr: {
+                  href: 'https://teams.microsoft.com/registration/lRjZagbeXki8UfzhJsyFMA,eqaDjyVuv0K2RTJhoBkv7Q,S2Faye49wEOo53wBS-3GVw,38J1avJfiUCOkKxft2PngQ,LZSEC9ALLE6s8FjwC5GagA,SwecGrH_aEazpXmR9ANriQ?mode=read&tenantId=6ad91895-de06-485e-bc51-fce126cc8530'
+              },
+              text: `REGISTER`
+          },
+        ]
+      },
+      {
+        title: `Neural Networks Project 1: Building Your First Neural Network`,
+        img: `Neural_Network_Project_1.png`,
+        date: `Thursday January 26, 2022`,
+        time: `6:00 PM - 8:00 PM EST`,
+        location: `Online Event`,
+        body: [
+          {
+            tag: 'p',
+            text: `Neural networks have taken over the world of artificial intelligence and machine learning, but how do you build one? Together in this hands-on workshop, we build a simple neural network that will make predictions based on tabular data. We will utilize state-of-the-art python libraries to build, train and test our neural networks.`
+          },
+          {
+              tag: 'button',
+              attr: {
+                  href: 'https://teams.microsoft.com/registration/lRjZagbeXki8UfzhJsyFMA,eqaDjyVuv0K2RTJhoBkv7Q,S2Faye49wEOo53wBS-3GVw,3sUWMLTfMEOzKbRALRySQw,RvxwSLQhw0Ssi4TYwdH-ew,ruzpkqpJT0S-Uks4LGgVSg?mode=read&tenantId=6ad91895-de06-485e-bc51-fce126cc8530'
+              },
+              text: `REGISTER`
           },
         ]
       }
