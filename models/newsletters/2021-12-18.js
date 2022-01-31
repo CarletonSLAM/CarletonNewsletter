22bc327cb429a244c669764cad19de7686eea0ea
brian_yang4444 <briany4444@gmail.com>
2022-01-30 21:43:38 -0500

    asdad

--- models/newsletters/2021-12-18.js
+++ models/newsletters/2021-12-18.js
@@ -16,66 +16,21 @@ module.exports = {
     articles: [
       // --- New Event --- //
       {
-        title: `Nasa Systems Engineering: Introduction To Project Life Cycle And Future Developments In Space Exploration`,
-        img: `nasa.jpeg`,
-        date: `January 28, 2022`,
-        time: `11:00 PM - 12:00 PM EST`,
+        title: `Integrated Sillicon Photonics`,
+        img: `SeminarFlyer_DanielBenedikovic-1.jpg`,
+        date: `Thursday February 10, 2022`,
+        time: `11:00 AM`,
         location: `Online Event`,
         body: [
-          {
-            tag: 'p',
-            text: `
-Many engineers seeking to enter the space industry lack insight on project life cycle phases that may impede the project process and aptness to meet both requirements and deadlines. In this talk, we will go over the different phases that compose Project Life Cycle. This presentation will provide an overview of each phase, including: Concept Studies, Concept and Technology Development, Preliminary Design and Technology Completion, Final Design and Fabrication, System Assembly, Integration and Test, Launch, Operations and Sustainment, and Closeout. The presentation will conclude with an outlook of future technologies for space exploration and settlement.
+        ]
+      },
+      {
+        title: `Neural Networks Project 2: Build your first computer vision project!`,
+        date: `Wednesday February 9, 2022`,
+        time: `6:00 PM - 8:00 PM EST`,
+        location: `Online Event`,
+        body: [
 
-#engineering #ieee #youngprofessionals #projectlifecycle #technology #industry #NASA"`
-          },
-          {
-              tag: 'button',
-              attr: {
-                  href: 'https://events.vtools.ieee.org/event/register/299073'
-              },
-              text: `REGISTER`
-          },
-        ]
-      },
-      {
-        title: `Intro to Neural Networks`,
-        img: `neural.png`,
-        date: `Wednesday January 19, 2022`,
-        time: `6:00 PM - 8:00 PM EST`,
-        location: `Online Event`,
-        body: [
-          {
-            tag: 'p',
-            text: `Deep Learning is the technology behind countless innovations in the past few years with applications in autonomous driving, biomedical informatics, image recognition and natural language processing. But what is deep learning, and how does it work? In this workshop, we will go over the foundational concepts of deep learning, particularly neural networks and how simple parts can build to form a complex system capable of modelling complex relationships.`
-          },
-          {
-              tag: 'button',
-              attr: {
-                  href: 'https://teams.microsoft.com/registration/lRjZagbeXki8UfzhJsyFMA,eqaDjyVuv0K2RTJhoBkv7Q,S2Faye49wEOo53wBS-3GVw,38J1avJfiUCOkKxft2PngQ,LZSEC9ALLE6s8FjwC5GagA,SwecGrH_aEazpXmR9ANriQ?mode=read&tenantId=6ad91895-de06-485e-bc51-fce126cc8530'
-              },
-              text: `REGISTER`
-          },
-        ]
-      },
-      {
-        title: `Neural Networks Project 1: Building Your First Neural Network`,
-        img: `Neural_Network_Project_1.png`,
-        date: `Thursday January 26, 2022`,
-        time: `6:00 PM - 8:00 PM EST`,
-        location: `Online Event`,
-        body: [
-          {
-            tag: 'p',
-            text: `Neural networks have taken over the world of artificial intelligence and machine learning, but how do you build one? Together in this hands-on workshop, we build a simple neural network that will make predictions based on tabular data. We will utilize state-of-the-art python libraries to build, train and test our neural networks.`
-          },
-          {
-              tag: 'button',
-              attr: {
-                  href: 'https://teams.microsoft.com/registration/lRjZagbeXki8UfzhJsyFMA,eqaDjyVuv0K2RTJhoBkv7Q,S2Faye49wEOo53wBS-3GVw,3sUWMLTfMEOzKbRALRySQw,RvxwSLQhw0Ssi4TYwdH-ew,ruzpkqpJT0S-Uks4LGgVSg?mode=read&tenantId=6ad91895-de06-485e-bc51-fce126cc8530'
-              },
-              text: `REGISTER`
-          },
         ]
       }
     ]
