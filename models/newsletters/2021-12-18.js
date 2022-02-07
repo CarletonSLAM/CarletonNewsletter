9497fab576bb3141a136faedde74a9080952abe9
brian_yang4444 <briany4444@gmail.com>
2022-02-07 18:26:14 -0500

    asd

--- models/newsletters/2021-12-18.js
+++ models/newsletters/2021-12-18.js
@@ -16,6 +16,61 @@ module.exports = {
     articles: [
       // --- New Event --- //
       {
+        title: `Battle Royale 14`,
+        img: `BR14_Thumbnail.png`,
+        date: `Friday March 4th to 6th, 2022`,
+        time: `12pm - 12pm`,
+        location: `Online Event`,
+        body: [
+          {
+              tag: 'p',
+              text: `On behalf of the Battle Royale Organizing Committee, in association with the IEEE Student Branches of the University of Ottawa and Carleton University, we invite YOU to participate in the 14th annual Battle Royale gaming event! `
+          },
+          {
+            tag: 'p',
+            text: `What is Battle Royale?`
+          },
+          {
+            tag: 'p',
+            text: `Battle Royale 14 is a 72-hour gaming event taking place from March 4-6, 2022, and featuring $1000 in prizes! This year, we are looking at raising money for our charity of choice, the Ottawa Food Bank.`
+          },
+          {
+            tag: 'p',
+            text: `Gaming Tournaments`
+          },
+          {
+            tag: 'p',
+            text: `Battle Royale 14 will feature League of Legends, VALORANT, and Rocket League, with each game featuring its own tournament and prize pool! The total prize pool is $590! `
+          },
+          {
+            tag: 'p',
+            text: `Game Jam `
+          },
+          {
+            tag: 'p',
+            text: `In teams of 1 to 4, you can choose to either come up with a creative game design or program a minigame; no prior game development experience is required. We're also giving away $410 as rewards for the best submissions and for simply participating! `
+          },
+          {
+              tag: 'p',
+              text: `In addition, industry professionals will be offering technical workshops related to game development and industry-standard tools and technologies. On top of that, we’re also hosting a networking event on Hopin featuring game studios such as Propel VR!`
+          },
+          {
+            tag: 'button',
+            attr: {
+                href: 'https://br14.battleroyale.ca/'
+            },
+            text: `REGISTER`
+          },
+          {
+            tag: 'button',
+            attr: {
+              href: 'https://discord.gg/Gc2pUmxcqK'
+            },
+            text: `DISCORD`
+          }
+        ]
+      },
+      {
         title: `Integrated Sillicon Photonics`,
         img: `SeminarFlyer_DanielBenedikovic-1.jpg`,
         date: `Thursday February 10, 2022`,
@@ -25,14 +80,29 @@ module.exports = {
         ]
       },
       {
-        title: `Neural Networks Project 2: Build your first computer vision project!`,
-        date: `Wednesday February 9, 2022`,
+        title: `AI Cofee Hour 2: Getting Into, Surviving and Thriving in Graduate School Feat. Dr. Jason Bernard`,
+        date: `Wednesday February 16, 2022`,
         time: `6:00 PM - 8:00 PM EST`,
         location: `Online Event`,
         body: [
           {
               tag: 'p',
-              text: `More info coming soon!`
+              text: `Presenting CAIS’s AI Coffee Hour! Our AI Coffee hour is a social event where everyone is invited to bring a cup of coffee and chat with the CAIS members about anything relating to Artificial Intelligence and Machine Learning. Want to exchange where you get your AI news and research? Feeling good or need help with your personal AI project? This is the perfect place to meet fellow AI enthusiasts. The discussions will be lightly guided by new themes every week, but nothing AI-related is off-limits. No prior experience or knowledge is required.`
+          },
+          {
+            tag: 'p',
+            text: `This week will feature Dr. Jason Bernard discussing Getting Into, Surviving and Thriving in Graduate School, learn more about him below!`
+          },
+          {
+            tag: 'p',
+            text: `Dr. Jason Bernard is a postdoctoral researcher at McMaster University studying applications of machine learning in medical education and neuroscience on an NSERC postdoctoral fellowship grant. He obtained his B.Sc. with honours from Bishop’s University in 1995. After over a decade in the industry and several years in the military, he did an M.Sc. from Athabasca University and Ph.D. from the University of Saskatchewan. He was a successful graduate student publishing 17 papers and receiving several prestigious awards including the NSERC CGS/D, Alberta Innovates Technology Futures graduate student award, and the Excellence in Research award. He enjoys sharing his experiences in graduate school online on Discord and Reddit, and, of course, live talks.`
+          },
+          {
+            tag: 'button',
+            attr: {
+                href: 'https://teams.microsoft.com/dl/launcher/launcher.html?url=%2F_%23%2Fl%2Fmeetup-join%2F19%3Ameeting_YjJjNWM5YzYtNGY2OS00MmNhLTliNjQtZmU4Y2ZjMmU1NTk5%40thread.v2%2F0%3Fcontext%3D%257B%2522Tid%2522%3A%25226ad91895-de06-485e-bc51-fce126cc8530%2522%2C%2522Oid%2522%3A%2522ab72a30b-581e-45d2-abc3-37cc554dfefb%2522%257D%26anon%3Dtrue&type=meetup-join&deeplinkId=45bed26f-20e4-4e4b-abdd-bf5261df8ba8&directDl=true&msLaunch=true&enableMobilePage=true&suppressPrompt=true'
+            },
+            text: `REGISTER`
           }
         ]
       }
