6ddf214978eaa121406f742afd80f053df1d2ef7
brian_yang4444 <briany4444@gmail.com>
2022-03-08 17:38:43 -0500

    adadad

--- models/newsletters/2022-02-20.js
+++ models/newsletters/2022-02-20.js
@@ -14,141 +14,16 @@ module.exports = {
         }
     ],
     articles: [
-      // --- New Event --- //
       {
-        title: `Battle Royale 14`,
-        img: `BR14_Thumbnail.png`,
-        date: `Friday March 4th to 6th, 2022`,
-        time: `12pm - 12pm all weekend`,
-        location: `Online Event`,
-        body: [
-          {
-              tag: 'p',
-              text: `On behalf of the Battle Royale Organizing Committee, in association with the IEEE Student Branches of the University of Ottawa and Carleton University, we invite YOU to participate in the 14th annual Battle Royale gaming event! `
-          },
-          {
-            tag: 'p',
-            text: `What is Battle Royale?`
-          },
-          {
-            tag: 'p',
-            text: `Battle Royale 14 is a 72-hour gaming event taking place from March 4-6, 2022, and featuring $1000 in prizes! This year, we are looking at raising money for our charity of choice, the Ottawa Food Bank.`
-          },
-          {
-            tag: 'p',
-            text: `Gaming Tournaments`
-          },
-          {
-            tag: 'p',
-            text: `Battle Royale 14 will feature League of Legends, VALORANT, and Rocket League, with each game featuring its own tournament and prize pool! The total prize pool is $590! `
-          },
-          {
-            tag: 'p',
-            text: `Game Jam `
-          },
-          {
-            tag: 'p',
-            text: `In teams of 1 to 4, you can choose to either come up with a creative game design or program a minigame; no prior game development experience is required. We're also giving away $410 as rewards for the best submissions and for simply participating! `
-          },
-          {
-              tag: 'p',
-              text: `In addition, industry professionals will be offering technical workshops related to game development and industry-standard tools and technologies. On top of that, we’re also hosting a networking event on Hopin featuring game studios such as Propel VR!`
-          },
-          {
-            tag: 'button',
-            attr: {
-                href: 'https://br14.battleroyale.ca/'
-            },
-            text: `WEBSITE`
-          },
-          {
-            tag: 'button',
-            attr: {
-              href: 'https://discord.gg/Gc2pUmxcqK'
-            },
-            text: `DISCORD`
-          },
-          {
-            tag: 'button',
-            attr: {
-              href: 'https://smash.gg/tournament/battle-royale-14/details'
-            },
-            text: `REGISTER`
-          }
-        ]
-      },
-      {
-        title: `Clarifying the Path to Becoming a Professional Engineer`,
-        date: `Tuesday February 22, 2022`,
-        img: `PEO2022 - João Guilherme Nizer Rahmeier.jpg`,
-        time: `12:00 PM - 1:30 PM EST`,
-        location: `Online Event`,
-        body: [
-          {
-            tag: 'p',
-            text: `"We are hosting our annual talk on ""Clarifying the Path to Becoming a Professional Engineer"", a partnership with Professional Engineers Ontario and co-hosting by IEEE Women In Engineering Ottawa, Young Professionals IEEE Montreal Section, IEEE Carleton Student Branch and IEEE uOttawa Student Branch.`
-          },
-          {
-              tag: 'p',
-              text: `If you are an engineering undergraduate or post-graduate student, or a recent graduate starting out your engineering career, then this seminar might be for you. In it you will learn:`
-          },
-          {
-            tag: 'p',
-            text: `- What is PEO?`
-          },
-          {
-            tag: 'p',
-            text: `- What engineering experience is PEO looking for once I graduate?`
-          },
-          {
-            tag: 'p',
-            text: `- I have international engineering education and experience; how is that evaluated by PEO?`
-          },
-          {
-            tag: 'p',
-            text: `- How is my engineering experience evaluated by PEO?`
-          },
-          {
-            tag: 'p',
-            text: `- How do I prepare my Experience Record?`
-          },
-          {
-            tag: 'p',
-            text: `- What is the PPE?`
-          },
-          {
-            tag: 'p',
-            text: `- What is the EIT Program and the Student Membership Program?`
-          },
-          {
-            tag: 'p',
-            text: `Emphasis of this webinar will be on the P. Eng. licensure process - what PEO is looking for. The webinar will focus on how engineering experience is evaluated and how you can facilitate the process. Learn the dos and don’ts of preparing your licensing application for evaluation.
-            To participate, you will require: internet connection to view the webinar, and/or phone access to dial into the conference for audio.
-            We look forward to speaking with you!`
-          },
-          {
-            tag: 'p',
-            text: `#ieee #peo #professional #engineer #ontario #canada #experience #undergraduate #graduate`
-          },
-          {
-            tag: 'button',
-            attr: {
-                href: 'http://bit.ly/YPPEO22'
-            },
-            text: `REGISTER`
-          }
-        ]
-      },
-      {
-        title: `Intro to Convolutional Neural Networks`,
-        date: `Wednesday March 2, 2022`,
-        img: `Intro_to_Convolutional_Neural_Network.png`,
+        title: `Build your first Computer Vision Project`,
+        date: `Wednesday March 9, 2022`,
+        img: `Copy-of-cais-ws3-feat-seltr.png`,
         time: `6:00 PM - 8:00 PM EST`,
         location: `Online Event`,
         body: [
           {
               tag: 'p',
-              text: `The introduction of Convolutional Neural Networks (CNNs) led to some of the largest advancements in Deep Learning, particularly in computer vision tasks such as image classification, object detection, and semantic segmentation. Over the years, many renowned CNN-based architectures have reported state-of-the-art performance in these computer vision tasks, each one improving upon the last. In this workshop, we will introduce the core concepts behind CNNs that make them work so well and review some of the most successful and famous CNN-based architectures.`
+              text: `One of the earliest successes of Convolutional Neural Networks (CNNs) was in handwritten digit recognition using the LeNet architecture. LeNet inspired a myriad of advancements in Deep Learning and led to many more renowned CNN-based architectures. In this project-based workshop, we invite you to join us in recreating the LeNet architecture using the MNIST database of handwritten digits and explore the many design changes that could improve classification accuracy.`
           }
         ]
       }
