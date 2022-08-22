a5d7638f1c0b63780c9ac86539b8a7543320b15a
brian_yang4444 <briany4444@gmail.com>
2022-08-21 22:52:14 -0300

    asd

--- models/newsletters/2022-03-29.js
+++ models/newsletters/2022-03-29.js
@@ -15,80 +15,36 @@ module.exports = {
     ],
     articles: [
       {
-        title: `IEEE CARLETON 2022 AGM`,
-        date: `Friday April 8, 2022`,
-        time: `6:00 PM`,
-        location: `Online Event`,
-        img: `2022 AGM.jpg`,
+        title: `AMATEUR RADIO - Get Started!`,
+        date: `Thursday, August 25 2022`,
+        time: `12:00 PM - 1:00 PM`,
+        location: `Room ME 4124`,
+        img: `Registration UCradio.eventbrite.ca (2).png`,
         body: [
           {
               tag: 'p',
-              text: `IEEE Carleton will be holding its virtual Annual General Meeting (AGM) for 2022 and General Elections at 6:00 PM on Friday, April 8! During the meeting, we'll be reviewing all the great events be held throughout the year, listening to platforms from officer and executive position candidates, and playing some fun games.`
+              text: `Communicate with people around the world!`
           },
           {
               tag: 'p',
-              text: `Every position is open, and each one gives students unique opportunities to apply their leadership skills while networking with professionals from the world's largest international technology institute, so I encourage you all to apply!`
+              text: `Experiment with Electronics`
           },
           {
               tag: 'p',
-              text: `To run for a position, fill out a platform similar to the example below and email it to chair@ieeecarleton.ca before the AGM.`
+              text: `Help your Community!`
           },
           {
               tag: 'p',
-              text: `We are excited to share IEEE Carleton's activities with you all, and we can't wait to meet you all!`
-          },
-          {
-              tag: 'p',
-              text: `See you soon!`
+              text: `Learn about the Amateur Radio Operator Certificate`
           },
           {
               tag: 'button',
               attr: {
-                  href: 'https://bit.ly/39IsLLX'
+                  href: 'bit.ly/YPORAC'
               },
-              text: `MORE DETAILS`
-          },
-          {
-              tag: 'button',
-              attr: {
-                  href: 'https://bit.ly/3ohHUfQ'
-              },
-              text: `EX. POSITION`
-          },
-          {
-              tag: 'button',
-              attr: {
-                  href: 'https://meet.google.com/wfq-voto-zft'
-              },
-              text: `MEETS LINK`
+              text: `REGISTER`
           }
         ]
       },
-      {
-        title: `13th Annual Wine & Cheese`,
-        date: `Friday April 1, 2022`,
-        location: `Canadian Museum of Nature (240 McLeod St, Ottawa, ON K2P 2R1)`,
-        time: `7:00 - 10:00 PM`,
-        body: [
-          {
-              tag: 'p',
-              text: `Tickets for the 2022 Wine and Cheese are available! Come out April 1st to the Canadian Museum of Nature for a sweet networking event with music and food! Would love to see lots of people there`
-          },
-          {
-              tag: 'button',
-              attr: {
-                  href: 'https://www.celebratewie.ca/'
-              },
-              text: `OFFICIAL WEBSITE`
-          },
-          {
-              tag: 'button',
-              attr: {
-                  href: 'https://ieee-uottawa.square.site/'
-              },
-              text: `IEEE UOTTAWA SITE`
-          }
-        ]
-      }
     ]
 }
