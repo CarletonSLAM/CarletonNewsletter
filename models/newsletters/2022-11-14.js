a2a6357d49a84427186e5db13c24bd28ece4c7f9
brian_yang4444 <briany4444@gmail.com>
2023-01-17 15:33:52 -0500

    asda

--- models/newsletters/2022-11-14.js
+++ models/newsletters/2022-11-14.js
@@ -15,130 +15,46 @@ module.exports = {
     ],
     articles: [
       {
-        title: `L3Harris Talk`,
-        date: `Wednesday Nov. 15 2022`,
-        time: `6:00 - 7:30 PM`,
-        location: `NI4010`,
-        img: `Lets_get_aero-dynamic.png`,
+        title: `LaTeX Software Workshops`,
+        img: `w1 - Ghassan Arnouk.png`,
+        location: `Minto CASE 6030`,
         body: [
           {
               tag: 'p',
-              text: `Come learn about L3Harris and Canada’s Aerospace and Defense sector at IEEE Carleton’s Tech Talk with L3Harris’ David Bourret this Tuesday evening at 6pm in NI4010. `
+              text: `Week 1: What is LaTeX?  @Jan. 25th 7 - 9pm`
           },
           {
-            tag: 'p',
-            text: `Pizza will be provided!`
+              tag: 'p',
+              text: `Week 2: More Advanced LaTeX packages @Feb. 1 7-8 pm`
+          },
+          {
+              tag: 'p',
+              text: `Week 3: LaTeX in different environments   @Feb. 7   7-8 pm`
+          },
+          {
+              tag: 'p',
+              text: `Week 4: Fully Reproducible Documents, Literate Programming, and Experiment LaTeX   @Feb. 15   7-8pm`
+          },
+          {
+              tag: 'p',
+              text: `Note that the workshops will run a hybrid model. It will be in person while being streamed online via zoom`
           },
           {
             tag: 'button',
             attr: {
-              href: 'https://docs.google.com/forms/d/e/1FAIpQLSc_FYVl2jy-D3GmtlarhDS-udUYthJsMplhZH4UIcSM7uHsmA/viewform'
+              href: ''
             },
-            text: `RSVP`
+            text: `WEEK 1 REGISTRATION`
           }
         ]
       },
       {
-        title: `Trends in Energy Transition`,
-        date: `Friday Nov. 18 2022`,
-        time: `12:00 PM EST`,
-        location: `NI4010`,
-        img: `TrendsEnergyTransition_Final.jpeg`,
-        body: [
-          
-        ]
-      },
-      {
-        title: `Canadian Space Conference 2023`,
-        date: `January 28th and 29th 2023`,
-        location: `Montreal`,
-        img: `sedseeds-banner-red-black.png`,
+        title: `IEEE Sweater Sale`,
+        img: `sweater.png`,
         body: [
           {
               tag: 'p',
-              text: `on behalf of SEDS-Canada, Students for the Exploration & Development of Space, a non-profit organisation that promotes and encourages students to pursue opportunities in the space industry. We are excited to announce our annual space conference, known as the Canadian Space Conference, will take place on January 28th and 29th of 2023 in Montreal. The conference is broadcasted as well if anyone would like to join us virtually. `
-          },
-          {
-            tag: 'p',
-            text: `The theme of this year’s conference is Space Through A Looking Glass with sessions and workshops on JWST and Canada’s contributions, Robotics, Space Health, Astrophotography, Space Law and more! The conference gives students the opportunity to network with professionals and space enthusiasts, showcase their research and to learn about the latest technologies in the industry. `
-          },
-          {
-            tag: 'button',
-            attr: {
-              href: 'https://www.eventbrite.ca/e/canadian-space-conference-2023-tickets-403123833237'
-            },
-            text: `REGISTRATION`
-          },
-          {
-            tag: 'button',
-            attr: {
-              href: 'https://csc.seds.ca/'
-            },
-            text: `WEBSITE`
-          }
-        ]
-      },
-      {
-        title: `SLAM Meeting`,
-        date: `Thursdays`,
-        location: `ME 4463`,
-        time: `6:00 - 9:00 PM`,
-        img: `slamAdvert.png`,
-        body: [
-          {
-              tag: 'p',
-              text: `You have an Idea and a Dream - We have: `
-          },
-          {
-              tag: 'p',
-              text: `Free arduinos`
-          },
-          {
-              tag: 'p',
-              text: `CAD workshops`
-          },
-          {
-              tag: 'p',
-              text: `1 on 1 Assistance`
-          },
-          {
-              tag: 'p',
-              text: `3D printing...? (future)`
-          }
-        ]
-      },
-      {
-        title: `AMATEUR RADIO - Training`,
-        date: `Every Sunday from Sept. 18 - Dec. 11`,
-        location: `Online`,
-        img: `v3.png`,
-        body: [
-          {
-              tag: 'p',
-              text: `Communicate with people around the world!`
-          },
-          {
-              tag: 'p',
-              text: `Experiment with Electronics`
-          },
-          {
-              tag: 'p',
-              text: `Help your Community!`
-          },
-          {
-              tag: 'p',
-              text: `Learn about the Amateur Radio Operator Certificate`
-          },
-          {
-              tag: 'p',
-              text: `First 10 IEEE members get 30$ off!`
-          },
-          {
-              tag: 'button',
-              attr: {
-                  href: 'https://bit.ly/3To7Za7'
-              },
-              text: `REGISTER`
+              text: `IEEE Carleton is planning on ordering another batch of sweaters in the near future! Come swing by the office or email services@ieeecarleton.ca for more information`
           }
         ]
       }
