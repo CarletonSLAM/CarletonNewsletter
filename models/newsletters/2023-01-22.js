32bea0dca42c56349a74be6d99b6e5c7952a9067
brian_yang4444 <briany4444@gmail.com>
2023-01-23 00:21:31 -0500

    asdad

---
+++ models/newsletters/2023-01-22.js
@@ -0,0 +1,201 @@
+module.exports = {
+    toast: [
+        {
+            tag: 'h2',
+            text: 'Keeping up with IEEE Carleton.'
+        },
+        {
+            tag: 'p',
+            text: 'Feel free to forward this newsletter to anyone you think might be interested.'
+        },
+        {
+            tag: 'htmlString',
+            text: `If you are having trouble viewing this as an email, click <a href='http://staylateandmake.ca/CarletonNewsletter/output/2022-11-14.html'>here</a> to view in browser.`
+        }
+    ],
+    articles: [
+      {
+        title: `LaTeX Software Workshops`,
+        img: `w1.png`,
+        time: `7:00 - 9:00 pm`,
+        date: `Wednesday, Jan. 25'th 2023`,
+        location: `Minto CASE 6030`,
+        body: [
+          {
+              tag: 'p',
+              text: `Week 1: What is LaTeX?   @Jan. 25'th    7-9 pm`
+          },
+          {
+              tag: 'p',
+              text: `Week 2: More Advanced LaTeX packages   @Feb. 1    7-8 pm`
+          },
+          {
+              tag: 'p',
+              text: `Week 3: LaTeX in different environments   @Feb. 8   7-8 pm`
+          },
+          {
+              tag: 'p',
+              text: `Week 4: Fully Reproducible Documents, Literate Programming, and Experiment LaTeX   @Feb. 16   7-8pm`
+          },
+          {
+              tag: 'p',
+              text: `Note that the workshops will run a hybrid model. It will be in person while being streamed online via zoom`
+          },
+          {
+            tag: 'button',
+            attr: {
+              href: 'https://www.eventbrite.ca/e/latex-workshop-series-week-1-tickets-506483013327?utm-campaign=social&utm-content=attendeeshare&utm-medium=discovery&utm-term=listing&utm-source=cp&aff=escb&fbclid=IwAR3_j07l21a-svY8CVy7Mr5z2lE_HfhQ1xRK_87SQFTzvc_YnfbCZvbIu98'
+            },
+            text: `WEEK 1 REGISTRATION`
+          }
+        ]
+      },
+      {
+        title: `BITsoc Nokia Speaker Event`,
+        img: `krebs.png`,
+        location: `Azrieli Pavilion room 132 (AP132)`,
+        date: `Tuesday, Jan. 24'th 2023`,
+        time: `6:00 - 7:00 pm`,
+        body: [
+          {
+              tag: 'p',
+              text: `Hey BITizens!`
+          },
+          {
+            tag: 'p',
+            text: `On Tuesday, January 24th 2023 at 6PM, BITsoc is welcoming Linda Krebs and Jason Rusmisel from Nokia Canada as guest speakers!`
+          },
+          {
+            tag: 'p',
+            text: `If you are looking for summer employment opportunities, please feel free to prepare a soft copy (PDF format only) of your resume and instructions will be offered during the event about how it can be sent out!
+
+            This event is accessible to all. Make sure to register via link provided.`
+          },
+          {
+            tag: 'button',
+            attr: {
+              href: 'https://forms.gle/oKiyTeLHFqEwDRii6'
+            },
+            text: `REGISTER`
+          }
+        ]
+      },
+      {
+        title: `LEED Training Special`,
+        body: [
+          {
+              tag: 'p',
+              text: `Start off the year by making your resume stand out with the premier sustainability designation called the LEED Green Associate! LEED is simply a sustainability scorecard for green buildings. As buildings can become LEED Certified, people can become LEED accredited! The best way to break into the sustainability space is to attain the LEED Green Associate. It also shows employers and clients you have certified knowledge in the field. Since the LEED GA exam doesn’t have a stellar pass rate, the value of the extra letters behind your name will carry even more weight.`
+          },
+          {
+            tag: 'p',
+            text: `Our engaging webinars have helped over 10K students and professionals learn the material cold AND clear the exam with a very high passing rate. The LEED Professional Designation has helped past participants attain internships and jobs and now is a great time to add this credential.`
+          },
+          {
+            tag: 'p',
+            text: `This course is instructed by a USGBC Faculty member and is the most effective way to pass. The USGBC charges a $100 (reduced for students) fee for the actual exam which can now be taken online from home. Save money by reserving your spot today and make a positive difference in your career!`
+          },
+          {
+            tag: 'p',
+            text: `Cost: $200 - Students can use the coupon code ‘green’ for $50 off (Discounted course price $150)`
+          },
+          {
+            tag: 'p',
+            text: `Please contact the instructor Lorne directly with any questions at lorne@leadinggreen.com and for those new to LEED`
+          },
+          {
+            tag: 'p',
+            text: `live webinars that can be streamed on any of the following dates:`
+          },
+          {
+            tag: 'p',
+            text: `1. January 21 2023 – 1:00PM – 5:30PM EST`
+          },
+          {
+            tag: 'p',
+            text: `2. February 11 2023 – 1:00PM – 5:30PM EST`
+          },
+          {
+            tag: 'p',
+            text: `3. February 28 2023 – 5:00PM – 9:30PM EST`
+          },
+          {
+            tag: 'p',
+            text: `4. March 25 2023 – 1:00PM – 5:30PM EDT`
+          },
+          {
+            tag: 'p',
+            text: `5. April 15 2023 – 1:00PM – 5:30PM EDT`
+          },
+          {
+            tag: 'p',
+            text: `6. May 6 2023 – 1:00PM – 5:30PM EDT     - OR -`
+          },
+          {
+            tag: 'p',
+            text: `7. On-demand recordings completed at your own pace`
+          },
+          {
+            tag: 'p',
+            text: `A. LEED AP BD+C (Level 2) – On-Demand`
+          },
+          {
+            tag: 'p',
+            text: `The above options (1-7) are all identical.`
+          },
+          {
+            tag: 'button',
+            attr: {
+              href:  `https://leadinggreen.com/online`
+            },
+            text: `REGISTER`
+          },
+          {
+            tag: 'button',
+            attr: {
+              href:  `https://youtu.be/X9FUJpIW9bI`
+            },
+            text: `VIDEO INTRODUCTION`
+          }
+        ]
+      },
+      {
+        title: `SLAM Meetings!`,
+        img: `slamAdvert.png`,
+        location: `ME4463`,
+        time: `Thursdays 6:00 - 9:00 pm`,
+        body: [
+          {
+              tag: 'p',
+              text: `You have an Idea and a Dream - We have:`
+          },
+          {
+              tag: 'p',
+              text: `Free arduinos`
+          },
+          {
+            tag: 'p',
+            text: `CAD Workshops`
+          },
+          {
+            tag: 'p',
+            text: `1 on 1 Assistance`
+          },
+          {
+            tag: 'p',
+            text: `3D Printing`
+          }
+        ]
+      },
+      {
+        title: `IEEE Sweater Sale`,
+        img: `sweater.png`,
+        body: [
+          {
+              tag: 'p',
+              text: `IEEE Carleton is planning on ordering another batch of sweaters in the near future! Come swing by the office or email services@ieeecarleton.ca for more information`
+          }
+        ]
+      }
+    ]
+}
