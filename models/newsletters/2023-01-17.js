8e5714b94649427c2cf98eb7fbc6440581fb55cd
brian_yang4444 <briany4444@gmail.com>
2023-01-23 00:02:11 -0500

    asd

--- models/newsletters/2023-01-17.js
+++ models/newsletters/2023-01-17.js
@@ -16,24 +16,26 @@ module.exports = {
     articles: [
       {
         title: `LaTeX Software Workshops`,
-        img: `w1 - Ghassan Arnouk.png`,
+        img: `w1.png`,
+        time: `7:00 - 9:00 pm`,
+        date: `Wednesday, Jan. 25'th 2023`,
         location: `Minto CASE 6030`,
         body: [
           {
               tag: 'p',
-              text: `Week 1: What is LaTeX?  @Jan. 25th 7 - 9pm`
+              text: `Week 1: What is LaTeX?   @Jan. 25'th    7-9 pm`
           },
           {
               tag: 'p',
-              text: `Week 2: More Advanced LaTeX packages @Feb. 1 7-8 pm`
+              text: `Week 2: More Advanced LaTeX packages   @Feb. 1    7-8 pm`
           },
           {
               tag: 'p',
-              text: `Week 3: LaTeX in different environments   @Feb. 7   7-8 pm`
+              text: `Week 3: LaTeX in different environments   @Feb. 8   7-8 pm`
           },
           {
               tag: 'p',
-              text: `Week 4: Fully Reproducible Documents, Literate Programming, and Experiment LaTeX   @Feb. 15   7-8pm`
+              text: `Week 4: Fully Reproducible Documents, Literate Programming, and Experiment LaTeX   @Feb. 16   7-8pm`
           },
           {
               tag: 'p',
@@ -45,6 +47,36 @@ module.exports = {
               href: 'https://www.eventbrite.ca/e/latex-workshop-series-week-1-tickets-506483013327?utm-campaign=social&utm-content=attendeeshare&utm-medium=discovery&utm-term=listing&utm-source=cp&aff=escb&fbclid=IwAR3_j07l21a-svY8CVy7Mr5z2lE_HfhQ1xRK_87SQFTzvc_YnfbCZvbIu98'
             },
             text: `WEEK 1 REGISTRATION`
+          }
+        ]
+      },
+      {
+        title: `IEEE Sweater Sale`,
+        img: `krebs.png`,
+        location: `Azrieli Pavilion room 132 (AP132)`,
+        date: `Tuesday, Jan. 24'th 2023`,
+        time: `6:00 - 7:00 pm`,
+        body: [
+          {
+              tag: 'p',
+              text: `Hey BITizens!`
+          },
+          {
+            tag: 'p',
+            text: `On Tuesday, January 24th 2023 at 6PM, BITsoc is welcoming Linda Krebs and Jason Rusmisel from Nokia Canada as guest speakers!`
+          },
+          {
+            tag: 'p',
+            text: `If you are looking for summer employment opportunities, please feel free to prepare a soft copy (PDF format only) of your resume and instructions will be offered during the event about how it can be sent out!
+
+            This event is accessible to all. Make sure to register via link provided.`
+          },
+          {
+            tag: 'p',
+            attr: {
+              href: 'https://forms.gle/oKiyTeLHFqEwDRii6'
+            },
+            text: `REGISTER`
           }
         ]
       },
