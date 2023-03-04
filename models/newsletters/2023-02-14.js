e93fc79a8b0bf884bc5fd5404978d3c1fda677c7
brian_yang4444 <briany4444@gmail.com>
2023-03-04 17:22:59 -0500

    aef

--- models/newsletters/2023-02-14.js
+++ models/newsletters/2023-02-14.js
@@ -15,159 +15,8 @@ module.exports = {
     ],
     articles: [
       {
-        title: `WIE X SLAM - Heart Soldering & 3D Printing Night`,
-        img: `solder.png`,
-        location: `ME4464 (Maker Space)`,
-        date: `Wednesday February 15`,
-        time: `6:00 pm`,
-        body: [
-          {
-              tag: 'p',
-              text: `Join SLAM & WIE in our 3D printing and heart soldering workshop this Wednesday!`
-          },
-          {
-              tag: 'p',
-              text: `Be sure to sign up quickly as space is limited!`
-          },
-          {
-            tag: 'button',
-            attr : {
-              href: `https://docs.google.com/forms/d/e/1FAIpQLScn3R9hf2Ltz3UuC0DfDmIgHcZkYj7tyBfD-z7gb5_ToxVA0Q/closedform`
-            },
-            text: `REGISTER`
-          }
-        ]
-      },
-      {
-        title: `WIE Panel - Career Transition`,
-        img: `panel_wie.png`,
-        location: `Virtual`,
-        date: `Thursday February 16`,
-        time: `7:00 pm`,
-        body: [
-          {
-              tag: 'p',
-              text: `With a lot of about to graduate and start your first job, or maybe getting ready to do your
-              first co-op, its natural to have a lot of questions. Don't worry! WIE got you covered!`
-          },
-          {
-              tag: 'p',
-              text: `Join women from all sorts of career paths to talk about their experience transitioning from undergrad to career!`
-          },
-          {
-            tag: 'button',
-            attr : {
-              href: `https://forms.gle/SxrpopwPDjWzVP4m9`
-            },
-            text: `REGISTER`
-          }
-        ]
-      },
-      {
-        title: `LaTeX Software Workshops`,
-        img: `week4.png`,
-        time: `7:00 - 8:00 pm`,
-        date: `Wednesday, Feb. 15'th 2023`,
-        location: `Minto CASE 6030`,
-        body: [
-          {
-              tag: 'p',
-              text: `Week 4: Fully Reproducible Documents, Literate Programming, and Experiment LaTeX   @Feb. 16   7-8pm`
-          },
-          {
-              tag: 'p',
-              text: `Note that the workshops will run a hybrid model. It will be in person while being streamed online via zoom`
-          },
-          {
-            tag: 'button',
-            attr: {
-              href: 'https://www.eventbrite.ca/e/latex-workshop-series-week-4-tickets-515695197217?aff=ebdsoporgprofile&fbclid=IwAR22jA2x4zX4oU8qVjv-GawCce4jupq3UUrExGJZglL81S4x_MRE9vOXnWo'
-            },
-            text: `WEEK 4 REGISTRATION`
-          }
-        ]
-      },
-      {
-        title: `LEED Training Special`,
-        body: [
-          {
-              tag: 'p',
-              text: `Start off the year by making your resume stand out with the premier sustainability designation called the LEED Green Associate! LEED is simply a sustainability scorecard for green buildings. As buildings can become LEED Certified, people can become LEED accredited! The best way to break into the sustainability space is to attain the LEED Green Associate. It also shows employers and clients you have certified knowledge in the field. Since the LEED GA exam doesn’t have a stellar pass rate, the value of the extra letters behind your name will carry even more weight.`
-          },
-          {
-            tag: 'p',
-            text: `Our engaging webinars have helped over 10K students and professionals learn the material cold AND clear the exam with a very high passing rate. The LEED Professional Designation has helped past participants attain internships and jobs and now is a great time to add this credential.`
-          },
-          {
-            tag: 'p',
-            text: `This course is instructed by a USGBC Faculty member and is the most effective way to pass. The USGBC charges a $100 (reduced for students) fee for the actual exam which can now be taken online from home. Save money by reserving your spot today and make a positive difference in your career!`
-          },
-          {
-            tag: 'p',
-            text: `Cost: $200 - Students can use the coupon code ‘green’ for $50 off (Discounted course price $150)`
-          },
-          {
-            tag: 'p',
-            text: `Please contact the instructor Lorne directly with any questions at lorne@leadinggreen.com and for those new to LEED`
-          },
-          {
-            tag: 'p',
-            text: `live webinars that can be streamed on any of the following dates:`
-          },
-          {
-            tag: 'p',
-            text: `1. January 21 2023 – 1:00PM – 5:30PM EST`
-          },
-          {
-            tag: 'p',
-            text: `2. February 11 2023 – 1:00PM – 5:30PM EST`
-          },
-          {
-            tag: 'p',
-            text: `3. February 28 2023 – 5:00PM – 9:30PM EST`
-          },
-          {
-            tag: 'p',
-            text: `4. March 25 2023 – 1:00PM – 5:30PM EDT`
-          },
-          {
-            tag: 'p',
-            text: `5. April 15 2023 – 1:00PM – 5:30PM EDT`
-          },
-          {
-            tag: 'p',
-            text: `6. May 6 2023 – 1:00PM – 5:30PM EDT     - OR -`
-          },
-          {
-            tag: 'p',
-            text: `7. On-demand recordings completed at your own pace`
-          },
-          {
-            tag: 'p',
-            text: `A. LEED AP BD+C (Level 2) – On-Demand`
-          },
-          {
-            tag: 'p',
-            text: `The above options (1-7) are all identical.`
-          },
-          {
-            tag: 'button',
-            attr: {
-              href:  `https://leadinggreen.com/online`
-            },
-            text: `REGISTER`
-          },
-          {
-            tag: 'button',
-            attr: {
-              href:  `https://youtu.be/X9FUJpIW9bI`
-            },
-            text: `VIDEO INTRODUCTION`
-          }
-        ]
-      },
-      {
-        title: `IEEE Merch!`,
+        title: `NEW IEEE Merch!`,
+        img: `hat.png`,
         body: [
           {
               tag: 'p',
@@ -186,6 +35,13 @@ module.exports = {
               href: `https://docs.google.com/forms/u/1/d/e/1FAIpQLSe_Int_yaZ2wejj7FvM8_c27BRlaQNwQycCOGB2oMkBdtP_kQ/viewform?usp=send_form`
             },
             text : `BUCKET HATS`
+          },
+          {
+            tag : 'button',
+            attr : {
+              href : `https://docs.google.com/forms/u/1/d/e/1FAIpQLSeohM36rJYwB5---lmKebISd2vguERyxtJH7kfEcS05xgvl4g/viewform?usp=send_form`
+            },
+            text : `MUGS`
           }
         ]
       },
@@ -195,7 +51,7 @@ module.exports = {
         body: [
           {
               tag: 'p',
-              text: `IEEE Carleton is planning on ordering another batch of sweaters in the near future! Come swing by the office or email services@ieeecarleton.ca for more information`
+              text: `IEEE Carleton is still planning on ordering another batch of sweaters in the near future! Come swing by the office or email services@ieeecarleton.ca for more information`
           }
         ]
       }
