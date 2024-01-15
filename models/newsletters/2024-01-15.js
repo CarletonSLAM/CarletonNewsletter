1003fb9ff370f63f21084fbc1e27d05618c9e1d8
ethan_alvares <ethan.alvares.formal@gmail.com>
2024-01-15 17:45:39 -0500

    bababooey

---
+++ models/newsletters/2024-01-15.js
@@ -0,0 +1,107 @@
+module.exports = {
+    toast: [
+        {
+            tag: 'h2',
+            text: 'Keeping up with IEEE Carleton.'
+        },
+        {
+            tag: 'p',
+            text: 'Feel free to forward this newsletter to anyone you think might be interested.'
+        },
+        {
+            tag: 'htmlString',
+            text: `If you are having trouble viewing this as an email, click <a href='http://staylateandmake.ca/CarletonNewsletter/output/2023-03-13.html'>here</a> to view in browser.`
+        }
+    ],
+    articles: [
+        {
+            title: `IEEE Associates!`,
+            img: `branch_logo_blue.png`,
+            body: [
+              {
+                  tag: 'p',
+                  text: `Introducing the new IEEE membership!`
+              },
+              {
+                  tag: 'p',
+                  text: `Becoming an associate allows you to buy food, drinks and the oh so important redbull for our IEEE battlepass!*`
+              },
+              {
+                tag: 'htmlString',
+                text: `To join, click <a href='https://docs.google.com/forms/d/1U2Fpsw_n7rQlFM_mLG2UcVxldmcdUqWLC-GodKMKtaA/edit'>here</a>`
+              },
+              {
+                  tag: 'p',
+                  text: `*see below`
+              }
+            ]
+        },
+        { 
+        title: `IEEE Redbull Battlepass!`,
+        body: [
+          {
+              tag: 'p',
+              text: `Also introducing the IEEE redbull battlepass! `
+          },
+          {
+              tag: 'p',
+              text: `If you buy one redbull a week for 10 weeks you will recieve a IEEE Redbull patch!*`
+          },
+          {
+              tag: 'p',
+              text: `*Points will only be rewarded for ONE redbull bought per week, additional redbulls purchased have no weight`
+          }
+        ]
+        },
+        { 
+        title: `Fall environment projects`,
+        img: `climatechange.png`,
+        body: [
+          {
+              tag: 'p',
+              text: `Fall Environment Projects - Funding of up to 10k`
+          },
+          {
+              tag: 'p',
+              text: `Service-Learning Projects to Support the IEEE Initiative to Address Climate Change`
+          },
+          {
+            tag: 'htmlString',
+            text: `For more information, click <a href='https://epics.ieee.org/fall-environmental-call-for-projects/?fbclid=IwAR1wW80tiGrb0dhdzGUnk1PCq3H_IMG4AL0_KzIWmLmyJgtfpVpAkNgkWxc'>here</a>`
+          }
+        ]
+        },
+        { 
+        title: `IEEE is looking for an exec!`,
+        img: `ieeeelections.png`,
+        body: [
+          {
+              tag: 'p',
+              text: `IEEE is looking to hire a VP external!`
+          },
+          {
+              tag: 'p',
+              text: ``
+          }
+        ]
+        },
+        { 
+        title: `IEEE WIE Elections!`,
+        img: `wieelections.png`,
+        body: [
+          {
+              tag: 'p',
+              text: `WIE elections are happening on Tuesday October 3rd!`
+          },
+          {
+              tag: 'p',
+              text: `Positions available are VP Academic, VP Finance and VP Social.`
+          },
+          {
+            tag: 'htmlString',
+            text: `To view more information or fill out an election form click <a href='https://linktr.ee/ieee.carleton'>here</a>`
+          }
+        ]
+        }
+    ]
+}
