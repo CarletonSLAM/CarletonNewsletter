226286aafcfe3e49f19f5eebdc3c804f96f03d8e
ethan_alvares <ethan.alvares.formal@gmail.com>
2024-01-23 17:49:41 -0500

    bingus

--- models/newsletters/2024-01-15.js
+++ models/newsletters/2024-01-15.js
@@ -15,91 +15,44 @@ module.exports = {
     ],
     articles: [
         {
-            title: `IEEE Associates!`,
-            img: `branch_logo_blue.png`,
+            title: `WIE Wine and Cheese`,
+            img: `winencheese2024.jpeg`,
             body: [
               {
                   tag: 'p',
-                  text: `Introducing the new IEEE membership!`
-              },
-              {
-                  tag: 'p',
-                  text: `Becoming an associate allows you to buy food, drinks and the oh so important redbull for our IEEE battlepass!*`
+                  text: `Wine and Cheese tickets are still being sold!`
               },
               {
                 tag: 'htmlString',
-                text: `To join, click <a href='https://docs.google.com/forms/d/1U2Fpsw_n7rQlFM_mLG2UcVxldmcdUqWLC-GodKMKtaA/edit'>here</a>`
-              },
-              {
-                  tag: 'p',
-                  text: `*see below`
+                text: `Get your tickets <a href='https://www.eventbrite.ca/e/wie-wine-cheese-2024-tickets-759089064827?aff=ebdssbdestsearch'>here</a>`
               }
             ]
         },
         { 
-        title: `IEEE Redbull Battlepass!`,
+        title: `Redbull Patches`,
+        img: `redbull.jpeg`,
         body: [
           {
               tag: 'p',
-              text: `Also introducing the IEEE redbull battlepass! `
+              text: `Redbull patches are here!`
           },
           {
               tag: 'p',
-              text: `If you buy one redbull a week for 10 weeks you will recieve a IEEE Redbull patch!*`
+              text: `If you've been participating in our battlepass and have completed 10 weeks straight of buying redbull, come to our office for your hard earned patch!`
           },
-          {
-              tag: 'p',
-              text: `*Points will only be rewarded for ONE redbull bought per week, additional redbulls purchased have no weight`
-          }
         ]
         },
         { 
-        title: `Fall environment projects`,
-        img: `climatechange.png`,
+        title: `SLAM Proposal Submissions`,
+        img: `slamlogo.png`,
         body: [
           {
               tag: 'p',
-              text: `Fall Environment Projects - Funding of up to 10k`
-          },
-          {
-              tag: 'p',
-              text: `Service-Learning Projects to Support the IEEE Initiative to Address Climate Change`
+              text: `Send in your proposal submissions for SLAM!`
           },
           {
             tag: 'htmlString',
-            text: `For more information, click <a href='https://epics.ieee.org/fall-environmental-call-for-projects/?fbclid=IwAR1wW80tiGrb0dhdzGUnk1PCq3H_IMG4AL0_KzIWmLmyJgtfpVpAkNgkWxc'>here</a>`
-          }
-        ]
-        },
-        { 
-        title: `IEEE is looking for an exec!`,
-        img: `ieeeelections.png`,
-        body: [
-          {
-              tag: 'p',
-              text: `IEEE is looking to hire a VP external!`
-          },
-          {
-              tag: 'p',
-              text: ``
-          }
-        ]
-        },
-        { 
-        title: `IEEE WIE Elections!`,
-        img: `wieelections.png`,
-        body: [
-          {
-              tag: 'p',
-              text: `WIE elections are happening on Tuesday October 3rd!`
-          },
-          {
-              tag: 'p',
-              text: `Positions available are VP Academic, VP Finance and VP Social.`
-          },
-          {
-            tag: 'htmlString',
-            text: `To view more information or fill out an election form click <a href='https://linktr.ee/ieee.carleton'>here</a>`
+            text: `Click <a href='https://epics.ieee.org/fall-environmental-call-for-projects/?fbclid=IwAR1wW80tiGrb0dhdzGUnk1PCq3H_IMG4AL0_KzIWmLmyJgtfpVpAkNgkWxc'>here</a>`
           }
         ]
         }
