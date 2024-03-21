f2b031b84aa7cbfa4607e828390ca2360bfc822c
ethan_alvares <ethan.alvares.formal@gmail.com>
2024-03-21 17:18:38 -0400

    boof

---
+++ models/newsletters/2024-03-21.js
@@ -0,0 +1,84 @@
+module.exports = {
+    toast: [
+        {
+            tag: 'h2',
+            text: 'Keeping up with IEEE Carleton.'
+        },
+        {
+            tag: 'p',
+            text: 'Feel free to forward this newsletter to anyone you think might be interested.'
+        },
+        {
+            tag: 'htmlString',
+            text: `If you are having trouble viewing this as an email, click <a href='http://staylateandmake.ca/CarletonNewsletter/output/2023-03-13.html'>here</a> to view in browser.`
+        }
+    ],
+    articles: [
+        {
+            title: `IEEE Elections`,
+            img: `elections.png`,
+            body: [
+              {
+                  tag: 'p',
+                  text: `Good evening folks. Please visit the following link to view the updated executive position roles and descriptions, as well as the IEEE 2024 - 2025 platform template. To submit a platform, please email chair@ieeecarleton.ca
+
+                  Note* All positions are open to be elected, however please be advised that Officer roles requires being in the branch for at least one year.
+                  
+                  Each candidate will breifly spend 3-5 minutes describing their platform, which will be followed by a 3-5 minute question period by the audience. 
+                  
+                  Voting period beings immeditaely following the election by current IEEE executive team and decisions will be made public the following Wednesday April 3rd at our AGM.
+                  
+                  For additional questions or concerns involving the process or platforms, please direct your questions to chair@ieeecarleton.ca`
+              },
+              {
+                tag: 'p',
+                text: `Elections are Wednesday March 27th at 5:30 PM in ME4332`
+            },
+              {
+                tag: 'htmlString',
+                text: `Positions and Template! <a href='https://drive.google.com/drive/folders/12TauIgYbYd09FoX2dYmVE0DmNXDfF4W6?usp=drive_link'>here</a>`
+              }
+            ]
+        },
+        {
+            title: `Battle Royale XV`,
+            img: `BRXV.png`,
+            body: [
+              {
+                  tag: 'p',
+                  text: `From intense gaming to unbeatable bragging rights, Battle Royale has it all. If you think you have what it takes to rise to the top or simply want to have a good time. Battle Royale is for you!
+
+                  From Friday March 22nd - Sunday March 24th IEEE is proud to offer CS:2, League of Legends, and Rocket League with the addition of a Game Jam for those who love making games with up to $1000 in prizes! 
+                  
+                  The event is fully online on their discord channel and all proceeds will go towards the Ottawa Food Bank to support the local community.
+                  
+                  Click the link below to register and for a more detailed breakdown of the scheduling and prizing.
+                  
+                  See you on the Battlegrounds`
+              },
+              {
+                tag: 'htmlString',
+                text: `Click <a href='https://l.facebook.com/l.php?u=https%3A%2F%2Flinktr.ee%2Fieeebattleroyale%3Ffbclid%3DIwAR2lW11Ve3czdoePmGKYRK8ILbKz8Uc14inpxnqu7bjh5txEbVA3Nbxx9Dc&h=AT0xm6tPc9cN5HN7HblPZ9AqpPXovTefhJrPqyOg2nJGQea6PoY5qrmZ-tko8D75smStBjPacyaKpZhLG5PAUNpca-3wRZ9JN5bopSnggEKTDlxf0i7JyYVjbHBCRudEtd-x6Q'>here</a>`
+              }
+            ]
+        },
+        { 
+        title: `SLAM Proposal Submissions`,
+        img: `slamnew.png`,
+        body: [
+          {
+              tag: 'p',
+              text: `Send in your proposal submissions for SLAM!`
+          },
+          {
+            tag: 'htmlString',
+            text: `We are now also accepting commissions for our 3D-Printer!`
+          },
+          {
+            tag: 'htmlString',
+            text: `Click <a href='https://linktr.ee/ieeeslam?utm_source=qr_code&fbclid=IwAR2H9v2dVEhxLo7U72IhRU5YKEpiSSYqRTBVCU6mwKeTO2j_wL4fkyauFg4'>here</a>`
+          }
+        ]
+        }
+    ]
+}
