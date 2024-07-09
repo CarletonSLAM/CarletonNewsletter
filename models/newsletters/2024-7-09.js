32bee4bb3eceacea468772f771aefb80e00aa725
Mia Cornell <117601492+mia107@users.noreply.github.com>
2024-07-09 16:51:28 -0400

    Create 2024-7-09.js

---
+++ models/newsletters/2024-7-09.js
@@ -0,0 +1,76 @@
+module.exports = {
+    toast: [
+        {
+            tag: 'h2',
+            text: 'Keeping up with IEEE Carleton.'
+        },
+        {
+            tag: 'p',
+            text: 'Feel free to forward this newsletter to anyone you think might be interested.'
+        },
+        {
+            tag: 'htmlString',
+            text: `If you are having trouble viewing this as an email, click <a href='http://staylateandmake.ca/CarletonNewsletter/output/2023-03-13.html'>here</a> to view in browser.`
+        }
+    ],
+    articles: [
+        {
+            title: `The TITLE of This Document`,
+            img: '',
+            body: [
+              {
+                  tag: 'p',
+                  text: '"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."'
+              },
+              {
+                tag: 'p',
+                text: `Elections are Wednesday March 27th at 5:30 PM in ME4332`
+            },
+              {
+                tag: 'htmlString',
+                text: `Positions and Template! <a href='https://drive.google.com/drive/folders/12TauIgYbYd09FoX2dYmVE0DmNXDfF4W6?usp=drive_link'>here</a>`
+              }
+            ]
+        },
+        {
+            title: `Battle Royale XV`,
+            img: `BRXV.png`,
+            body: [
+              {
+                  tag: 'p',
+                  text: `From intense gaming to unbeatable bragging rights, Battle Royale has it all. If you think you have what it takes to rise to the top or simply want to have a good time. Battle Royale is for you!
+
+                  From Friday March 22nd - Sunday March 24th IEEE is proud to offer CS:2, League of Legends, and Rocket League with the addition of a Game Jam for those who love making games with up to $1000 in prizes! 
+                  
+                  The event is fully online on their discord channel and all proceeds will go towards the Ottawa Food Bank to support the local community.
+                  
+                  Click the link below to register and for a more detailed breakdown of the scheduling and prizing.
+                  
+                  See you on the Battlegrounds`
+              },
+              {
+                tag: 'htmlString',
+                text: `Click <a href='https://l.facebook.com/l.php?u=https%3A%2F%2Flinktr.ee%2Fieeebattleroyale%3Ffbclid%3DIwAR2lW11Ve3czdoePmGKYRK8ILbKz8Uc14inpxnqu7bjh5txEbVA3Nbxx9Dc&h=AT0xm6tPc9cN5HN7HblPZ9AqpPXovTefhJrPqyOg2nJGQea6PoY5qrmZ-tko8D75smStBjPacyaKpZhLG5PAUNpca-3wRZ9JN5bopSnggEKTDlxf0i7JyYVjbHBCRudEtd-x6Q'>here</a>`
+              }
+            ]
+        },
+        { 
+        title: `SLAM Proposal Submissions`,
+        img: `slamnew.png`,
+        body: [
+          {
+              tag: 'p',
+              text: `Send in your proposal submissions for SLAM!`
+          },
+          {
+            tag: 'htmlString',
+            text: `We are now also accepting commissions for our 3D-Printer!`
+          },
+          {
+            tag: 'htmlString',
+            text: `Click <a href='https://linktr.ee/ieeeslam?utm_source=qr_code&fbclid=IwAR2H9v2dVEhxLo7U72IhRU5YKEpiSSYqRTBVCU6mwKeTO2j_wL4fkyauFg4'>here</a>`
+          }
+        ]
+        }
+    ]
+}
