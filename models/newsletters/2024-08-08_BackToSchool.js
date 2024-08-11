d5c377afc3e4d0f50713b7f786eb34935bbe648b
Mia Cornell <117601492+mia107@users.noreply.github.com>
2024-08-11 13:22:28 -0400

    Back to School newsletter almost done

--- models/newsletters/2024-08-08_BackToSchool.js
+++ models/newsletters/2024-08-08_BackToSchool.js
@@ -58,25 +58,34 @@ module.exports = {
         },
         {
             title: `Stay Late and Make: SLAM`,
-            img: `ieee-carleton.png`,
+            img: `GroupShot1.jpg`,
             body: [
               {
                   tag: 'p',
-                  text: ``
+                  text: 'Photo: CO2 Car Competition Hosted by SLAM',
+              },
+              {
+                  tag: 'p',
+                  text: 'Bring your ideas to live with SLAM! SLAM is a project that allows students use IEEE Carleton materials and facilities to create their dream projects. This includes 3D Printing, Wires, PCB Design and many other tools that can bring your idea to life!'
               },
               {
                 tag: 'htmlString',
-                text: `Click <a href=''>here</a>`
+                text: `Submit your project <a href=>Now!</a>`
               }
             ]
         },
         { 
         title: `WIE Wine and Cheese`,
-        img: `ieee-carleton.png`,
+        img: `IMG_3883.JPEG`,
         body: [
           {
               tag: 'p',
-              text: `One of our biggest networking events.`
+              text: `Photo: Wine and Cheese 2024`
+          },
+          {
+            tag: 'p',
+            text: `Wine and Cheese is an event hosted by the Women In Engineering (WIE) IEEE Student Branch. It is one of our biggest networking events. 
+            In general the WIE IEEE Student Branch hopes to encourage inclusion within engineering and all other technical fields.`
           },
           {
             tag: 'htmlString',
@@ -84,43 +93,42 @@ module.exports = {
           },
           {
             tag: 'htmlString',
-            text: `Visit the WIE Carleton Student Branch <a href=''>website! </a>`
+            text: `Visit the WIE Carleton Student Branch <a href='https://www.celebratewie.ca/'>website! </a>`
           }
         ]
         },
         { 
           title: `cuHacking`,
-          img: `ieee-carleton.png`,
+          img: `cuhacking.png`,
           body: [
             {
                 tag: 'p',
-                text: ``
+                text: `cuHacking is the official hacking event hosted by Carleton University. It is for hobbyists, aspiring developers, 
+                or anyone who is interested in programming. It is a chance to learn, develop your skills and win prizes!`
             },
             {
               tag: 'htmlString',
-              text: ``
-            },
-            {
-              tag: 'htmlString',
-              text: `Click <a href=''>here</a>`
+              text: `Learn more <a href='https://www.cuhacking.ca/'>HERE!</a>`
             }
           ]
         },
         { 
-          title: `Networking and Other Events`,
-          img: `Networking Photo.jpg`,
+          title: `SPAC`,
+          img: `SPAC 2020_22.jpg`,
           body: [
             {
                 tag: 'p',
-                text: ``
+                text: `Photo: SPAC 2020`
             },
             {
-              tag: 'htmlString',
-              text: ``
-            },
+              tag: 'p',
+              text: `The IEEE Student Professional Awareness Conference (SPAC) is a formal dinner and networking event that 
+              connects our student branch with partners throughout the entrity of the engineering community. With around 100 companies to connect with 
+              there are loads of oppurtunities for students to enter the industry`
+          },
             {
               tag: 'htmlString',
-              text: `Click <a href=''>here</a>`
+              text: `Learn more about <a href='https://www.ieeespac.ca/'>SPAC</a>`
             }
           ]
         }
