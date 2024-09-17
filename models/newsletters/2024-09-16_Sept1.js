a80d763e42e2bcfbd9faeff51ff75ae7cc449d82
Mia Cornell <117601492+mia107@users.noreply.github.com>
2024-09-17 00:08:08 -0400

    Draft 2 Done?

--- models/newsletters/2024-09-16_Sept1.js
+++ models/newsletters/2024-09-16_Sept1.js
@@ -1,243 +1,264 @@
 module.exports = {
-    toast: [
+  toast: [
+    {
+      tag: "h2",
+      text: "SEPETEMBER 17, 2024",
+    },
+    {
+      tag: "p",
+      text: "Welcome Back To School!",
+    },
+    {
+      tag: "p",
+      text: `If you are having trouble viewing this as an email, please contact publications@ieeecarleton.ca.`,
+    },
+  ],
+  articles: [
+    {
+      title: `What is IEEE?`,
+      img: `ieee-carleton.png`,
+      body: [
         {
-            tag: 'h2',
-            text: 'SEPETEMBER 17, 2024'
-        },
-        {
-            tag: 'p',
-            text: 'Welcome Back To School!'
-        },
-        {
-            tag: 'p',
-            text: `If you are having trouble viewing this as an email, please contact publications@ieeecarleton.ca.`
-        }
-    ],
-    articles: [
-        {
-            title: `What is IEEE?`,
-            img: `ieee-carleton.png`,
-            body: [
-              {
-                  tag: 'p',
-                  text: `If you are viewing this newletter you are a current newletter 
+          tag: "p",
+          text: `If you are viewing this newletter you are a current newletter 
                   recipient of the IEEE Carleton Student Branch. 
                   You probably already know who we are, 
                   but if you signed up for this for no reason- 
-                  let us give you an intro on who were are!`
-              },
-              {
-                tag: 'p',
-                text: `The Institute of Electrical and Electronics Engineers (IEEE) is 
+                  let us give you an intro on who were are!`,
+        },
+        {
+          tag: "p",
+          text: `The Institute of Electrical and Electronics Engineers (IEEE) is 
                 the largest technical professional organization in the world. Since 1975, the IEEE
                 has been a diverse group of students at Carleton interested in engineering, computer science and other
                 technical fields! We are a smaller community within the IEEE Ottawa Branch and host numerous social events
                 throughout the year at Carleton and across the Greater Ottawa Region. No- you do not have to be in electrical engineering or anything engineering to join!
                 So, if you know people who are interested in being a part of a tight-knit group we are always ready to welcome in new members.
-                Feel free to share this newsletter with your friends as well!`
-              },
-              {
-                tag: 'p',
-                text: `If you ever want to visit us, we are located in the Mackenzie Building Room 3359.`
-              },
-              {
-                tag: 'htmlString',
-                text: `Our newly updated website! <a href='https://www.ieeecarleton.ca/'>HERE</a>`
-              }
-            ]
+                Feel free to share this newsletter with your friends as well!`,
         },
         {
-          title: 'UPCOMING EVENTS!',
-          body: [
-            {
-              tag: 'h2',
-              text: 'Here are some events that are happening in the next few weeks!',
-            }
-          ]
-      },
-      { 
-        title: `Election???`,
-        img: `Hiring.png`,
-        body: [
-          {
-              tag: 'h2',
-              text: `DUE DATE: SEPTEMBER 22, 2024`
-          },
-          {
-            tag: 'p',
-            text: `We are hiring!!! IEEE is currently hiring two positions that will run all year round. 
-            Do you want to add an awesome postion to your resume? Here are the three positions we are hiring: `
+          tag: "p",
+          text: `If you ever want to visit us, we are located in the Mackenzie Building Room 3359.`,
         },
         {
-          tag: 'h2',
-          text: `Academic Director: Be in charge of organizing Technical Workshops and Seminars. Communicate with other clubs about our events.`
-      },
-      {
-        tag: 'h2',
-        text: `Technical Director: Help update and maintain our website. Ensures server is working. Maintain the domains and hostings`
+          tag: "htmlString",
+          text: `Our newly updated website! <a href='https://www.ieeecarleton.ca/'>HERE</a>`,
+        },
+      ],
     },
     {
-      tag: 'h2',
-      text: `WIE Graphics Designer: Represent IEEE events on campus! Design posts for the WIE social media pages`
-  },
-  {
-    tag: 'p',
-    text: `To apply, please visit our linktree and make a copy of our Elections Platform Template. 
-    Then send your platform to secretary@ieeecarleton.ca. If you have any questions feel free to reach out!`
-},
-{
-  tag: 'htmlString',
-  text: `Apply<a href= https://linktr.ee/ieee.carleton> Now!</a>`
-}
-        ]
-      },
-      { 
-        title: `Intern Panel`,
-        img:`Intern TechTalk Promo Social Media (2)-images-0.jpg`,
-        body: [
-          {
-              tag: 'h2',
-              text: `DATE: SEPTEMBER 24, 2024` 
-          },
-          {
-            tag: 'p',
-            text: `Interested in learning all the ins and outs of finding your first co-op? 
+      title: "UPCOMING EVENTS!",
+      body: [
+        {
+          tag: "h2",
+          text: "Here are some events that are happening in the next few weeks!",
+        },
+      ],
+    },
+    {
+      title: `Election`,
+      img: `Hiring.png`,
+      body: [
+        {
+          tag: "h2",
+          text: `DUE DATE: SEPTEMBER 22, 2024`,
+        },
+        {
+          tag: "p",
+          text: `We are hiring!!! IEEE is currently hiring three positions that will run all year round. 
+            Do you want to add an awesome postion to your resume? Here are the three positions we are hiring: `,
+        },
+        {
+          tag: "h2",
+          text: `Academic Director`,
+        },
+        {
+          tag: "p",
+          text: `Be in charge of organizing Technical Workshops and Seminars. Communicate with other clubs about our events.`,
+        },
+        {
+          tag: "h2",
+          text: `Technical Director`,
+        },
+        {
+          tag: "p",
+          text: `Help update and maintain our website. Ensures server is working. Maintain the domains and hostings`,
+        },
+        {
+          tag: "h2",
+          text: `WIE Graphics Designer`,
+        },
+        {
+          tag: "p",
+          text: `Represent IEEE events on campus! Design posts for the WIE social media pages`,
+        },
+        {
+          tag: "p",
+          text: `To apply, please visit our linktree and make a copy of our Elections Platform Template. 
+    Then send your platform to secretary@ieeecarleton.ca. If you have any questions feel free to reach out!`,
+        },
+        {
+          tag: "htmlString",
+          text: `Apply<a href= https://linktr.ee/ieee.carleton> Now!</a>`,
+        },
+      ],
+    },
+    {
+      title: `Intern Panel`,
+      img: `Intern TechTalk Promo Social Media (2)-images-0.jpg`,
+      body: [
+        {
+          tag: "h2",
+          text: `DATE: SEPTEMBER 24, 2024`,
+        },
+        {
+          tag: "p",
+          text: `Interested in learning all the ins and outs of finding your first co-op? 
             Want to hear from successful students in various industries? 
             Want to come out and ask one question at IEEE’s STAMPED EVENT? Well look no further, 
             the IEEE SCESOC intern panel boasts a variety of panelists that want to share their 
-            co-op experiences and answer all your burning questions!`
+            co-op experiences and answer all your burning questions!`,
         },
         {
-          tag: 'htmlString',
-          text: `Sign up <a href='https://scesoc.ck.page/9d2d945267?fbclid=IwY2xjawEZ2QpleHRuA2FlbQIxMAABHbX_eYsfCYnkBoX4AfP_WY6tVjKzHquVI7tkDGz7o4rBalx1ROLF5Elw_w_aem_8WFW3YDqFOi2cyorikbgJA'> HERE</a>`
-        }
-        ]
-      },
-      { 
-        title: `Sweaters!`,
-        img: `sweatshirt ordering.png`,
-        body: [
-          {
-              tag: 'h2',
-              text: `Buy now!`
-          },
-          {
-            tag: 'p',
-            text: `Fill out our form to be able to buy a swaggy IEEE sweater! A photo of the sweater is featured within the form!`
+          tag: "htmlString",
+          text: `Sign up <a href='https://scesoc.ck.page/9d2d945267?fbclid=IwY2xjawEZ2QpleHRuA2FlbQIxMAABHbX_eYsfCYnkBoX4AfP_WY6tVjKzHquVI7tkDGz7o4rBalx1ROLF5Elw_w_aem_8WFW3YDqFOi2cyorikbgJA'> HERE</a>`,
+        },
+      ],
+    },
+    {
+      title: `Sweaters!`,
+      img: 'IMG_6341.jpeg',
+      body: [
+        {
+          tag: "h2",
+          text: `Buy now!`,
         },
         {
-          tag: 'htmlString',
-          text: `Put in your order<a href= https://forms.gle/4BrVwNcWb3UvAQey8> Now!</a>`
-        },
-        ]
-      },
-        {
-            title: 'Future Events!',
-            body: [
-              {
-                tag: 'h2',
-                text: 'There are loads of ways to get involved within the IEEE Student Branch Community during the year! Here are some events that are happening throughout the year.',
-
-              }
-            ]
+          tag: "p",
+          text: `Fill out our form to be able to buy a swaggy IEEE sweater! We will contact you when you can pick them up!`,
         },
         {
-            title: `Stay Late and Make: SLAM`,
-            img: `GroupShot1.jpg`,
-            body: [
-              {
-                  tag: 'p',
-                  text: 'Photo: CO2 Car Competition Hosted by SLAM',
-              },
-              {
-                  tag: 'p',
-                  text: 'Do you have a dream? A vision? SLAM is a project that allows students use IEEE Carleton materials and facilities to create their dream projects. This includes 3D Printing, Wires, PCB Design and many other tools that can bring your idea to life! This activity runs throughout the year, so feel free to submit a project whenever!'
-              },
-              {
-                tag: 'htmlString',
-                text: `Submit your project <a href= https://forms.gle/ovyUZ26kMz9ZSc818>Now!</a>`
-              }
-            ]
+          tag: "htmlString",
+          text: `Put in your order<a href= https://forms.gle/4BrVwNcWb3UvAQey8> Now!</a>`,
         },
-        { 
-        title: `WIE Wine and Cheese`,
-        img: `IMG_3883.JPEG`,
-        body: [
-          {
-              tag: 'p',
-              text: `Photo: Wine and Cheese 2024`
-          },
-          {
-            tag: 'p',
-            text: `Wine and Cheese is an event hosted by the IEEE Women In Engineering (WIE) Student Branch. It is one of our biggest networking events. 
-            In general the IEEE WIE Student Branch hopes to encourage the inclusion of women+ within engineering and all other technical fields.`
-          },
-          {
-            tag: 'p',
-            text: `Some other IEEE WIE Carleton specific events include Cocoa and Cram, Self Care Night, Professor Panel, and more!`
-          },
-          {
-            tag: 'htmlString',
-            text: `Visit the IEEE WIE Carleton Student Branch <a href='https://www.celebratewie.ca/'>website! </a>`
-          }
-        ]
+      ],
+    },
+    {
+      title: `Sweaters (QR CODE)`,
+      img: 'sweatshirt ordering.png',
+      body: [
+        {
+          tag: "h2",
+          text: `Buy now!`,
         },
-        { 
-          title: `cuHacking`,
-          img: `cuhacking.png`,
-          body: [
-            {
-                tag: 'p',
-                text: `cuHacking is the official hacking event hosted by Carleton University. It is for hobbyists, aspiring developers, 
-                or anyone who is interested in programming. It is a chance to learn, develop your skills and win prizes!`
-            },
-            {
-              tag: 'htmlString',
-              text: `Learn more <a href='https://www.cuhacking.ca/'>HERE!</a>`
-            }
-          ]
+      ],
+    },
+    {
+      title: "Future Events!",
+      body: [
+        {
+          tag: "h2",
+          text: "There are loads of ways to get involved within the IEEE Student Branch Community during the year! Here are some events that are happening throughout the year.",
         },
-        { 
-          title: `SPAC`,
-          img: `SPAC 2020_22.jpg`,
-          body: [
-            {
-                tag: 'p',
-                text: `Photo: SPAC 2020`
-            },
-            {
-              tag: 'p',
-              text: `The IEEE Student Professional Awareness Conference (SPAC) is a formal dinner and networking event that 
+      ],
+    },
+    {
+      title: `Stay Late and Make: SLAM`,
+      img: `GroupShot1.jpg`,
+      body: [
+        {
+          tag: "p",
+          text: "Photo: CO2 Car Competition Hosted by SLAM",
+        },
+        {
+          tag: "p",
+          text: "Do you have a dream? A vision? SLAM is a project that allows students use IEEE Carleton materials and facilities to create their dream projects. This includes 3D Printing, Wires, PCB Design and many other tools that can bring your idea to life! This activity runs throughout the year, so feel free to submit a project whenever!",
+        },
+        {
+          tag: "htmlString",
+          text: `Submit your project <a href= https://forms.gle/fBBodBSCVtbnL3g78>Now!</a>`,
+        },
+      ],
+    },
+    {
+      title: `WIE Wine and Cheese`,
+      img: `IMG_3883.JPEG`,
+      body: [
+        {
+          tag: "p",
+          text: `Photo: Wine and Cheese 2024`,
+        },
+        {
+          tag: "p",
+          text: `Wine and Cheese is an event hosted by the IEEE Women In Engineering (WIE) Student Branch. It is one of our biggest networking events. 
+            In general the IEEE WIE Student Branch hopes to encourage the inclusion of women+ within engineering and all other technical fields.`,
+        },
+        {
+          tag: "p",
+          text: `Some other IEEE WIE Carleton specific events include Cocoa and Cram, Self Care Night, Professor Panel, and more!`,
+        },
+        {
+          tag: "htmlString",
+          text: `Visit the IEEE WIE Carleton Student Branch <a href='https://www.celebratewie.ca/'>website! </a>`,
+        },
+      ],
+    },
+    {
+      title: `cuHacking`,
+      img: `cuhacking.png`,
+      body: [
+        {
+          tag: "p",
+          text: `cuHacking is the official hacking event hosted by Carleton University. It is for hobbyists, aspiring developers, 
+                or anyone who is interested in programming. It is a chance to learn, develop your skills and win prizes!`,
+        },
+        {
+          tag: "htmlString",
+          text: `Learn more <a href='https://www.cuhacking.ca/'>HERE!</a>`,
+        },
+      ],
+    },
+    {
+      title: `SPAC`,
+      img: `SPAC 2020_22.jpg`,
+      body: [
+        {
+          tag: "p",
+          text: `Photo: SPAC 2020`,
+        },
+        {
+          tag: "p",
+          text: `The IEEE Student Professional Awareness Conference (SPAC) is a formal dinner and networking event that 
               connects our student branch with partners throughout the entrity of the engineering community. With around 100 companies to connect with 
-              there are loads of oppurtunities for students to enter the industry`
-          },
-            {
-              tag: 'htmlString',
-              text: `Learn more about <a href='https://www.ieeespac.ca/'>SPAC</a>`
-            }
-          ]
-        },
-        { 
-          title: `Other Events`,
-          img: `branch_logo_blue.png`,
-          body: [
-            {
-                tag: 'p',
-                text: `Photo: IEEE Logo`
-            },
-            {
-              tag: 'p',
-              text: `IEEE Carleton hosts so many events, we simply can't put them all here. Here are a few social events that you should look out for.`
-          },
-          {
-            tag: 'p',
-            text: 'Social Events: Polar plunge, Battle Royal, Societal Wars, Movie Night'
+              there are loads of oppurtunities for students to enter the industry`,
         },
         {
-          tag: 'p',
-          text: 'Networking/Academic: Resume roast, LinkedIn Headshots, LaTex workshops, Conferences'
-      }
-          ]
-        }
-    ]
-}
+          tag: "htmlString",
+          text: `Learn more about <a href='https://www.ieeespac.ca/'>SPAC</a>`,
+        },
+      ],
+    },
+    {
+      title: `Other Events`,
+      img: `branch_logo_blue.png`,
+      body: [
+        {
+          tag: "p",
+          text: `Photo: IEEE Logo`,
+        },
+        {
+          tag: "p",
+          text: `IEEE Carleton hosts so many events, we simply can't put them all here. Here are a few social events that you should look out for.`,
+        },
+        {
+          tag: "p",
+          text: "Social Events: Polar plunge, Battle Royal, Societal Wars, Movie Night",
+        },
+        {
+          tag: "p",
+          text: "Networking/Academic: Resume roast, LinkedIn Headshots, LaTex workshops, Conferences",
+        },
+      ],
+    },
+  ],
+};
