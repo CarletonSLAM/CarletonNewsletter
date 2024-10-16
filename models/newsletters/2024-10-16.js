5d2a252e8ee58b0781f6cce4dbbeea30b52356c6
Mia Cornell <117601492+mia107@users.noreply.github.com>
2024-10-15 23:38:38 -0400

    News Letter

---
+++ models/newsletters/2024-10-16.js
@@ -0,0 +1,100 @@
+module.exports = {
+  toast: [
+    {
+      tag: "h2",
+      text: "October 16, 2024",
+    },
+    {
+      tag: "p",
+      text: "Happy Thanksgiving From IEEE",
+    },
+    {
+      tag: "p",
+      text: `It's that time of year already? We hope everyone had some R&R this past long weekend! Good luck to everyone during this midterm season!`,
+    },
+    {
+      tag: "p",
+      text: `If you are having trouble viewing this as an email, please contact publications@ieeecarleton.ca.`,
+    },
+  ],
+  articles: [
+    {
+      title: "UPCOMING EVENTS!",
+      body: [
+        {
+          tag: "h2",
+          text: "Here are some events that are happening in the next few weeks!",
+        },
+      ],
+    },
+    {
+      title: `Student Professinal Awareness Conference`,
+      img: ``,
+      body: [
+        {
+          tag: "h2",
+          text: `Early Bird Tickets On-sale Now!`,
+        },
+        {
+          tag: "h2",
+          text: `November 1st, 2024 | Brookstreet Hotel`,
+        },
+        {
+          tag: "h3",
+          text: `Why Should I Go?`,
+        },
+        {
+          tag: "p",
+          text: `The Student Professional Awareness Conference (SPAC) is an 
+          amazing oppurtunity for students to network and meet industry professionals in a semi-formal setting. 
+          It is a great way to get your foot in the door and learn more about the employment outlook in your desired field.`,
+        },
+        {
+          tag: "h3",
+          text: `Early Bird Ticket Offer and Raffle!`,
+        },
+        {
+          tag: "p",
+          text: `If you buy your ticket before October 18th, you get a $10 discount on your ticket! Additionally, you are entered into
+          a raffle to earn prizes such as Airpods, a portable charger and other cool tech!`,
+        },
+        {
+          tag: "h3",
+          text: `"But I don't know how to get there!"`,
+        },
+        {
+          tag: "p",
+          text: `You are in luck! For an additional $15, you can pay for a round-trip to the Brookstreet Hotel from Carleton University!`,
+        },
+        {
+          tag: "htmlString",
+          text: `Register<a href= https://www.ieeespac.ca/> Now</a>`,
+        },
+      ],
+    },
+    {
+      title: "What Did I Miss?",
+      body: [
+        {
+          tag: "h2",
+          text: "Here are some important updates!",
+        },
+      ],
+    },
+    {
+      title: `Fall Semester General Meeting`,
+      img: ``,
+      body: [
+        {
+          tag: "h2",
+          text: `October 7th, 2024`,
+        },
+        {
+          tag: "p",
+          text: `Thank you to all our lovely executive members, and supporters for coming to join us at our FSGM! 
+          We had a blast hearing from our current team, and are so happy to those who came.`,
+        },
+      ],
+    },
+  ],
+};
