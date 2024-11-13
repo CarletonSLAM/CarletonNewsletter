1bf94e7b455c2791a8f210d1ea11ae4dbea6c3cf
Mia Cornell <117601492+mia107@users.noreply.github.com>
2024-11-12 22:40:39 -0500

    Hoodie photo upload

---
+++ models/newsletters/2024-11-12.js
@@ -0,0 +1,94 @@
+module.exports = {
+  toast: [
+    {
+      tag: "h2",
+      text: "NOVEMBER 12, 2024",
+    },
+    {
+      tag: "p",
+      text: "Happy Fall!",
+    },
+    {
+      tag: "p",
+      text: `If you are having trouble viewing this as an email, please contact publications@ieeecarleton.ca.`,
+    },
+  ],
+  articles: [
+    {
+      title: "UPCOMING EVENTS!",
+      body: [
+        {
+          tag: "h2",
+          text: "Here are some events that are happening in the next few weeks!",
+        },
+      ],
+    },
+    {
+      title: `Women In Engineering Hoodies!`,
+      img: ``,
+      body: [
+        {
+          tag: "h2",
+          text: `Due Date: November 16, 2024`,
+        },
+        {
+          tag: "p",
+          text: `Support IEEE WIE branch by ordering a quater-zip hoodie! Only $45!`,
+        },
+        {
+          tag: "htmlString",
+          text: `Order<a href= https://forms.gle/uXUmuqr2JPRs3LTL8> Now!</a>`,
+        },
+      ],
+    },
+    {
+      title: `WIE: Self Care Night!`,
+      img: ``,
+      body: [
+        {
+          tag: "h2",
+          text: `Date: November 22, 2024`,
+        },
+        {
+          tag: "h2",
+          text: `Time: 7pm - 10pm`,
+        },
+        {
+          tag: "h2",
+          text: `Location: Roosters Coffeehouse`,
+        },
+        {
+          tag: "p",
+          text: `Join us for a night for oneself, to forgo the stresses of academics, a cheerful night of de-stressing.`,
+        },
+      ],
+    },
+    {
+      title: "What Did I Miss?",
+      body: [
+        {
+          tag: "h2",
+          text: "Here are some important updates!",
+        },
+      ],
+    },
+    {
+      title: `EVENT TEMPLATE`,
+      img: ``,
+      body: [
+        {
+          tag: "h2",
+          text: `DATE`,
+        },
+        {
+          tag: "p",
+          text: `Description`,
+        },
+        {
+          tag: "htmlString",
+          text: `Link<a href= https://linktr.ee/ieee.carleton> Link</a>`,
+        },
+      ],
+    },
+  ],
+};
