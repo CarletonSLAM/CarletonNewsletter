f9459e83b5bbc047b6992afd59e41bef82d82142
Mia Cornell <117601492+mia107@users.noreply.github.com>
2024-11-26 23:18:54 -0500

    Added

---
+++ models/newsletters/2024-11-26.js
@@ -0,0 +1,145 @@
+module.exports = {
+  toast: [
+    {
+      tag: "h2",
+      text: "NOVEMBER 27, 2024",
+    },
+    {
+      tag: "p",
+      text: "Hoping everyone is having a great end to their semester!",
+    },
+    {
+      tag: "p",
+      text: `If you are having trouble viewing this as an email, please contact publications@ieeecarleton.ca.`,
+    },
+  ],
+  articles: [
+    {
+      title: "UPCOMING EVENTS!",
+      body: [
+        {
+          tag: "h2",
+          text: "Here are some events that are happening in the next few weeks!",
+        },
+      ],
+    },
+    {
+      title: `WIE X CUE Workshop`,
+      img: `cue_wie_gender_workshop.jpg`,
+      body: [
+        {
+          tag: "h2",
+          text: `November 27, 2024`,
+        },
+        {
+          tag: "p",
+          text: `Join us this Wednesday, 6:30pm at CB3101 for a workshop to learn more about gender 
+          minority perspectives and issues as well as how certain behaviours may affect minorities. 
+          We welcome everyone who wants to understand gender 
+          minority perspectives and learn how to better support them! 
+          I hear there will be pizza and snacks!`,
+        },
+        {
+          tag: "htmlString",
+          text: `Link<a href= https://linktr.ee/ieee.carleton> Link</a>`,
+        },
+      ],
+    },
+    {
+      title: `Wine and Cheese`,
+      img: `Wine&cheese1999.jpg`,
+      body: [
+        {
+          tag: "h2",
+          text: `DATE: JAN 2025`,
+        },
+        {
+          tag: "h2",
+          text: `TICKET DROPS BEGIN: DECEMBER 2 2024`,
+        },
+        {
+          tag: "p",
+          text: `Join us for one of the biggest networking events of the year! 
+          W&C is a celebration of Women in Engineering. It provides an opportunity for students in 
+          engineering to expand their network and spark valuable discussions in a 
+          low-pressure environment with experts, professionals, faculty, and fellow students in their 
+          interested career fields. Be on the lookout for when ticket sales drop!`,
+        },
+        {
+          tag: "htmlString",
+          text: `Learn<a href= https://www.celebratewie.ca/> more!</a>`,
+        },
+      ],
+    },
+    {
+      title: `Other Winter Semester Events...`,
+      body: [
+        {
+          tag: "p",
+          text: `Polar Plunge - Jan 2025`,
+        },
+        {
+          tag: "p",
+          text: `Battle Royal Competition - TBD`,
+        },
+        {
+          tag: "p",
+          text: `Academic Workshops - TBD`,
+        },
+      ],
+    },
+    {
+      title: "What Did I Miss?",
+      body: [
+        {
+          tag: "h2",
+          text: "Here are some important updates!",
+        },
+      ],
+    },
+    {
+      title: `IEEE Ornament Decorating!`,
+      body: [
+        {
+          tag: "h2",
+          text: `November 13, 2024`,
+        },
+        {
+          tag: "p",
+          text: `We had fun decorating ornaments to hang around the 
+          IEEE office and the 3300 block! Can you spot any?`,
+        },
+      ],
+    },
+    {
+      title: `WIE Self-Care Night!`,
+      img: `wie_selfcare_2024.jpg`,
+      body: [
+        {
+          tag: "h2",
+          text: `November 22, 2024`,
+        },
+        {
+          tag: "p",
+          text: `The WIE Self-Care night was a blast! 
+          Our participants were able to relax and destress at this crazy time of year!
+          Stay tuned for our other exam destressors!`,
+        },
+      ],
+    },
+    {
+      title: `IEEE Movie Night!`,
+      body: [
+        {
+          tag: "h2",
+          text: `November 26, 2024`,
+        },
+        {
+          tag: "p",
+          text: `Yesterday, IEEE watched the Polar Express to get into the holiday spirit! 
+          We will be hosting loads more movie nights next semester!`,
+        },
+      ],
+    },
+  ],
+};
