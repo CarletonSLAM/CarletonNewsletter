e43e669589d22632f4f5a129a9503dbec029d34b
pyrobeck <rbraun12321@gmail.com>
2025-09-25 22:31:57 -0400

    afsjklf;sl

---
+++ models/newsletters/2025-09-26.js
@@ -0,0 +1,118 @@
+module.exports = {
+  toast: [
+    {
+      tag: "h2",
+      text: "SEPTEMBER 26, 2025",
+    },
+    {
+      tag: "p",
+      text: "Hope your term started off well!",
+    },
+    {
+      tag: "p",
+      text: `If you are having trouble viewing this as an email, please contact publications@ieeecarleton.ca.`,
+    },
+  ],
+  articles: [
+    {
+      title: "UPCOMING EVENTS!",
+      body: [
+        {
+          tag: "h2",
+          text: "Here are some events that are happening in the next few weeks!",
+        },
+      ],
+    },
+    {
+      title: `SPAC 2025`,
+      img: `SPAC 2020_23.jpg`,
+      body: [
+        {
+          tag: "h2",
+          text: `Early Bird Tickets Available Now!`,
+        },
+        {
+          tag: "h2",
+          text: `DATE: October 31, 2025`,
+        },
+        {
+          tag: "p",
+          text: `The IEEE Student Professional Awareness Conference (SPAC) is a formal networking event & dinner that 
+          serves esteemed professionals and allows them to engage with engineering and computer science students within 
+          their fields.
+          SPAC provides students, academics, and industry professionals an opportunity to network and bridge the gap 
+          between classrooms and boardrooms, thereby empowering attendees to build professional connections and form a bond.`,
+        },
+        {
+          tag: "p",
+          text: `More info at the link below:`,
+        },
+        {
+          tag: "htmlString",
+          text: `Link  <a href= "https://www.ieeespac.ca/"> IEEE SPAC Website</a>`,
+        },
+        {
+          tag: "p",
+          text: `Get tickets here:`,
+        },
+        {
+          tag: "htmlString",
+          text: `Link  <a href="https://www.eventbrite.com/e/student-professional-awareness-conference-2025-tickets-1661438470189?utm_experiment=test_share_listing&aff=ebdsshios&sg=bf636a3326bfc7828ddb9ea51559095e5f653bb9904b80128a3a064f5b791bb315ad13acc808e3435ae3f32dea34a9d5d7104072e1b5e860a075d9873a8603a388e3eca087c247b96e87800342"> Eventbrite</a>`,
+        },
+      ],
+    },
+    {
+      title: `LinkedIn Workshop`,
+      body: [
+        {
+          tag: "h2",
+          text: `DATE: October 9, 2025`,
+        },
+        {
+          tag: "h2",
+          text: `WHERE: ME 4342`,
+        },
+        {
+          tag: "p",
+          text: `Hear our speaker, Jose Lizarzabura, give advice on how to spruce up your LinkedIn presence!`,
+        },
+        {
+          tag: "htmlString",
+          text: `Learn<a href= "https://www.instagram.com/ieeecarleton/"> more!</a>`,
+        },
+      ],
+    },
+    {
+      title: "Ongoing",
+      body: [],
+    },
+    {
+      title: `SLAM`,
+      body: [
+        {
+          tag: "p",
+          text: `Need resources, advice, and/or tools for your project?
+          Fill out this form and the SLAM team can help you with 3D printing and our various other tools/services! 
+          Your project will then be rejected or approved at the discretion of the SLAM Lead.`,
+        },
+        {
+          tag: "p",
+          text: `You may reach the SLAM Lead at: slamlead@ieeecarleton.ca`,
+        },
+        {
+          tag: "htmlString",
+          text: `<a href= "https://docs.google.com/forms/d/e/1FAIpQLSfxxIQEnLlSaJsXSakaBcax5SLRToCbogUNEasgU6T8TLmydg/viewform">Project Proposal Form</a>`,
+        },
+      ],
+    },
+    {
+      title: `Important Dates`,
+      body: [
+        {
+          tag: "p",
+          text: `IEEE Day - October 7th`,
+        },
+      ],
+    },
+  ],
+};
