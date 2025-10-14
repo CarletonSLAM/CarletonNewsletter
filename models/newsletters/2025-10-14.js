605ee6d8f47fd49af2f94e2bdbf5a981fff5e593
pyrobeck <rbraun12321@gmail.com>
2025-10-14 11:36:48 -0400

    october newsletter 2025

---
+++ models/newsletters/2025-10-14.js
@@ -0,0 +1,132 @@
+module.exports = {
+  toast: [
+    {
+      tag: "h2",
+      text: "OCTOBER 14, 2025",
+    },
+    {
+      tag: "p",
+      text: "Almost reading week!!!",
+    },
+    {
+      tag: "p",
+      text: `If you are having trouble viewing this as an email, please contact publications@ieeecarleton.ca.`,
+    },
+  ],
+  articles: [
+    {
+      title: "UPCOMING EVENTS!",
+      body: [
+        {
+          tag: "h2",
+          text: "Here are some events that are happening in the next few weeks!",
+        },
+      ],
+    },
+    {
+      title: `SPAC 2025`,
+      img: `SPAC2020_23.jpg`,
+      body: [
+        {
+          tag: "h2",
+          text: `Sign up now!`,
+        },
+        {
+          tag: "h2",
+          text: `DATE: October 31, 2025`,
+        },
+        {
+          tag: "p",
+          text: `The IEEE Student Professional Awareness Conference (SPAC) is a formal networking event & dinner that 
+          serves esteemed professionals and allows them to engage with engineering and computer science students within 
+          their fields.
+          SPAC provides students, academics, and industry professionals an opportunity to network and bridge the gap 
+          between classrooms and boardrooms, thereby empowering attendees to build professional connections and form a bond.`,
+        },
+        {
+          tag: "p",
+          text: `More info at the link below:`,
+        },
+        {
+          tag: "htmlString",
+          text: `Link  <a href= "https://www.ieeespac.ca/"> IEEE SPAC Website</a>`,
+        },
+        {
+          tag: "p",
+          text: `Get tickets here:`,
+        },
+        {
+          tag: "htmlString",
+          text: `Link  <a href="https://www.eventbrite.com/e/student-professional-awareness-conference-2025-tickets-1661438470189?utm_experiment=test_share_listing&aff=ebdsshios&sg=bf636a3326bfc7828ddb9ea51559095e5f653bb9904b80128a3a064f5b791bb315ad13acc808e3435ae3f32dea34a9d5d7104072e1b5e860a075d9873a8603a388e3eca087c247b96e87800342"> Eventbrite</a>`,
+        },
+      ],
+    },
+    {
+      title: `IEEE FSGM`,
+      img: `IEEE_FSGM.png`,
+      body: [
+        {
+          tag: "h2",
+          text: `DATE: October 29th, 2025`,
+        },
+        {
+          tag: "h2",
+          text: `WHERE: TBD`,
+        },
+        {
+          tag: "h1",
+          text: `WE WILL HAVE PIZZA!!!`,
+        },
+        {
+          tag: "p",
+          text: `Come hear about what IEEE Carleton has been up to and bring up any concerns with the team!`,
+        },
+      ],
+    },
+    {
+      title: "Ongoing",
+      body: [],
+    },
+    {
+      title: `SLAM`,
+      img: 'SLAM_poster.png',
+      body: [
+        {
+          tag: "p",
+          text: `Need resources, advice, and/or tools for your project?
+          Fill out this form and the SLAM team can help you with 3D printing and our various other tools/services! 
+          Your project will then be rejected or approved at the discretion of the SLAM Lead.`,
+        },
+        {
+          tag: "p",
+          text: `You may reach the SLAM Lead at: slamlead@ieeecarleton.ca`,
+        },
+        {
+          tag: "htmlString",
+          text: `<a href= "https://docs.google.com/forms/d/e/1FAIpQLSfxxIQEnLlSaJsXSakaBcax5SLRToCbogUNEasgU6T8TLmydg/viewform">Project Proposal Form</a>`,
+        },
+      ],
+    },
+    {
+      title: "Hiring",
+      body: [],
+    },
+    {
+      title: `IEEE Social Media Director`,
+      body: [
+        {
+          tag: "p",
+          text: `We are hiring a Social Media Director!`,
+        },
+        {
+          tag: "p",
+          text: `To apply, please fill out this form:`,
+        },
+        {
+          tag: "htmlString",
+          text: `<a href= "https://forms.gle/GXNmtUaHcZ7zMtGg9">Social Media Director Application Form</a>`,
+        },
+      ],
+    },
+  ],
+};
