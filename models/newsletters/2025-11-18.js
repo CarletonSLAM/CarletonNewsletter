8a7bed263bdf00aff5019febb025bd44083d917a
pyrobeck <rbraun12321@gmail.com>
2025-11-17 22:20:09 -0500

    november newsletter

---
+++ models/newsletters/2025-11-18.js
@@ -0,0 +1,60 @@
+module.exports = {
+  toast: [
+    {
+      tag: "h2",
+      text: "NOVEMBER 18, 2025",
+    },
+    {
+      tag: "p",
+      text: "There's snow outside now!",
+    },
+    {
+      tag: "p",
+      text: `If you are having trouble viewing this as an email, please contact publications@ieeecarleton.ca.`,
+    },
+  ],
+  articles: [
+    {
+      title: "NEWS!",
+      body: [],
+    },
+    {
+      title: `Sweaters are here!`,
+      img: `beigesweaterpromo.jpg`,
+      body: [
+        {
+          tag: "h2",
+          text: `Get your new IEEE sweater now!`,
+        },
+        {
+          tag: "p",
+          text: `Just come to the IEEE room 3359 during office hours to buy your own cool sweater!`,
+        },
+      ],
+    },
+    {
+      title: "Ongoing",
+      body: [],
+    },
+    {
+      title: `SLAM`,
+      img: 'SLAM_poster.png',
+      body: [
+        {
+          tag: "p",
+          text: `Need resources, advice, and/or tools for your project?
+          Fill out this form and the SLAM team can help you with 3D printing and our various other tools/services! 
+          Your project will then be rejected or approved at the discretion of the SLAM Lead.`,
+        },
+        {
+          tag: "p",
+          text: `You may reach the SLAM Lead at: slamlead@ieeecarleton.ca`,
+        },
+        {
+          tag: "htmlString",
+          text: `<a href= "https://docs.google.com/forms/d/e/1FAIpQLSfxxIQEnLlSaJsXSakaBcax5SLRToCbogUNEasgU6T8TLmydg/viewform">Project Proposal Form</a>`,
+        },
+      ],
+    },
+  ],
+};
