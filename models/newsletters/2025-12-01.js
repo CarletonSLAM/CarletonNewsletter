4e7744bbd57afe0e50131795c560042a643d6166
pyrobeck <87209578+pyrobeck@users.noreply.github.com>
2025-11-30 18:43:03 -0500

    december newsletter

---
+++ models/newsletters/2025-12-01.js
@@ -0,0 +1,92 @@
+module.exports = {
+  toast: [
+    {
+      tag: "h2",
+      text: "DECEMBER 01, 2025",
+    },
+    {
+      tag: "p",
+      text: "Almost the break, I can see the end!",
+    },
+    {
+      tag: "p",
+      text: `If you are having trouble viewing this as an email, please contact publications@ieeecarleton.ca.`,
+    },
+  ],
+  articles: [
+    {
+      title: "NEWS!",
+      body: [],
+    },
+    {
+      title: `Cocoa and Cram!`,
+      img: `Cocoa_and_Cram_2025.png`,
+      body: [
+        {
+          tag: "h2",
+          text: `Nicol 3040, December 6th 2-6 pm`,
+        },
+        {
+          tag: "p",
+          text: `Come get some hot cocoa and help studying for your exams!  There will be upper years who maybe able to help you with specific courses!`,
+        },
+      ],
+    },
+    {
+      title: `Wine and Cheese 2026`,
+      img: 'Wine&cheese1999.jpg',
+      body: [
+        {
+          tag: "p",
+          text: `IEEE WIE is back for year 17 connecting pros and students!
+          
+The 17th Annual Celebration of Women in Engineering Wine & Cheese provides an opportunity for the industry to connect to students in engineering and science who are seeking to expand their network and spark valuable discussions in a low-pressure environment. Academia, industry, and students are invited to meet others over refreshments and create lasting connections for years to come.
+
+Our team is focused on connecting passionate and motivated individuals with the opportunities that will help them make an impact. We believe that getting creative and intellectual people to the places that they can excel is essential in building a sustainable and innovative future.`,
+        },
+        {
+          tag: "htmlString",
+          text: `<a href= "https://www.eventbrite.ca/e/wie-wine-cheese-2026-tickets-1875170919699?aff=oddtdtcreator">Get tickets here!</a>`,
+        },
+      ],
+    },
+    {
+      title: "Ongoing",
+      body: [],
+    },
+    {
+      title: `SLAM`,
+      img: 'SLAM_poster.png',
+      body: [
+        {
+          tag: "p",
+          text: `Need resources, advice, and/or tools for your project?
+          Fill out this form and the SLAM team can help you with 3D printing and our various other tools/services! 
+          Your project will then be rejected or approved at the discretion of the SLAM Lead.`,
+        },
+        {
+          tag: "p",
+          text: `You may reach the SLAM Lead at: slamlead@ieeecarleton.ca`,
+        },
+        {
+          tag: "htmlString",
+          text: `<a href= "https://docs.google.com/forms/d/e/1FAIpQLSfxxIQEnLlSaJsXSakaBcax5SLRToCbogUNEasgU6T8TLmydg/viewform">Project Proposal Form</a>`,
+        },
+      ],
+    },
+    {
+      title: `Sweaters are here!`,
+      img: `beigesweaterpromo.jpg`,
+      body: [
+        {
+          tag: "h2",
+          text: `Get your new IEEE sweater now!`,
+        },
+        {
+          tag: "p",
+          text: `Just come to the IEEE room 3359 during office hours to buy your own cool sweater!`,
+        },
+      ],
+    },
+  ],
+};
