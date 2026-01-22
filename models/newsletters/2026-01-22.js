acbbed6199cc4ef813f64620333f879dd0f6105a
pyrobeck <87209578+pyrobeck@users.noreply.github.com>
2026-01-22 16:17:22 -0500

    uploading photos for jan issue

---
+++ models/newsletters/2026-01-22.js
@@ -0,0 +1,110 @@
+module.exports = {
+  toast: [
+    {
+      tag: "h2",
+      text: "JANUARY 22, 2026",
+    },
+    {
+      tag: "p",
+      text: "Happy (Late) New Year!",
+    },
+    {
+      tag: "p",
+      text: `If you are having trouble viewing this as an email, please contact publications@ieeecarleton.ca.`,
+    },
+  ],
+  articles: [
+    {
+      title: "NEWS!",
+      body: [],
+    },
+    {
+      title: `I Triple Billiards Night`,
+      img: `I Triple billiards night_updateddate.png`,
+      body: [
+        {
+          tag: "h2",
+          text: `MacLaren's on Elgin (301 Elgins str) - Tuesday, February 11th 7 - 9 pm`,
+        },
+        {
+          tag: "p",
+          text: `Come play pool with IEEE! Tickets are available in the IEEE office (ME 3359)! `,
+        },
+      ],
+    },
+    {
+      title: `Wine and Cheese 2026`,
+      img: 'wineandcheese2026.jpeg',
+      body: [
+        {
+          tag: "p",
+          text: `IEEE WIE is back for year 17 connecting pros and students!
+          
+The 17th Annual Celebration of Women in Engineering Wine & Cheese provides an opportunity for the industry to connect to students in engineering and science who are seeking to expand their network and spark valuable discussions in a low-pressure environment. Academia, industry, and students are invited to meet others over refreshments and create lasting connections for years to come.
+
+Our team is focused on connecting passionate and motivated individuals with the opportunities that will help them make an impact. We believe that getting creative and intellectual people to the places that they can excel is essential in building a sustainable and innovative future.`,
+        },
+        {
+          tag: "htmlString",
+          text: `<a href= "https://www.eventbrite.ca/e/wie-wine-cheese-2026-tickets-1875170919699?aff=oddtdtcreator">Get tickets here!</a>`,
+        },
+      ],
+    },
+    {
+      title: `INTERNSHIP PANEL`,
+      img: 'internshipicon.png',
+      body: [
+        {
+          tag: "h2",
+          text: `Location TBD (check instagram) - Tuesday, February 3rd, 6 - 8 pm `,
+        },
+        {
+          tag: "p",
+          text: `Want to learn more about how to land an internship? Keep going to networking events but don't know how to turn connections into jobs? Attend the internship panel to get actionable tips from people who have.`,
+        },
+        {
+          tag: "htmlString",
+          text: `<a href= "https://www.eventbrite.ca/e/wie-wine-cheese-2026-tickets-1875170919699?aff=oddtdtcreator">Get tickets here!</a>`,
+        },
+      ],
+    },
+    {
+      title: "Ongoing",
+      body: [],
+    },
+    {
+      title: `SLAM`,
+      img: 'SLAM_poster.png',
+      body: [
+        {
+          tag: "p",
+          text: `Need resources, advice, and/or tools for your project?
+          Fill out this form and the SLAM team can help you with 3D printing and our various other tools/services! 
+          Your project will then be rejected or approved at the discretion of the SLAM Lead.`,
+        },
+        {
+          tag: "p",
+          text: `You may reach the SLAM Lead at: slamlead@ieeecarleton.ca`,
+        },
+        {
+          tag: "htmlString",
+          text: `<a href= "https://docs.google.com/forms/d/e/1FAIpQLSfxxIQEnLlSaJsXSakaBcax5SLRToCbogUNEasgU6T8TLmydg/viewform">Project Proposal Form</a>`,
+        },
+      ],
+    },
+    {
+      title: `Sweaters are here!`,
+      img: `beigesweaterpromo.jpg`,
+      body: [
+        {
+          tag: "h2",
+          text: `Get your new IEEE sweater now!`,
+        },
+        {
+          tag: "p",
+          text: `Just come to the IEEE room 3359 during office hours to buy your own cool sweater!`,
+        },
+      ],
+    },
+  ],
+};
