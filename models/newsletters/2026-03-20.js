f24f45f1c5b635996dc7437eb8ab3d110bb08311
Beck Braun <87209578+pyrobeck@users.noreply.github.com>
2026-03-20 15:44:18 -0400

    March Newsletter

---
+++ models/newsletters/2026-03-20.js
@@ -0,0 +1,107 @@
+module.exports = {
+  toast: [
+    {
+      tag: "h2",
+      text: "MARCH 20, 2026",
+    },
+    {
+      tag: "p",
+      text: "Hope your term is going well!",
+    },
+    {
+      tag: "p",
+      text: `If you are having trouble viewing this as an email, please contact publications@ieeecarleton.ca.`,
+    },
+  ],
+  articles: [
+    {
+      title: "NEWS!",
+      body: [],
+    },
+    {
+      title: `IEEE Carleton AGM`,
+      img: `IEEE AGM 2026.png`,
+      body: [
+        {
+          tag: "h2",
+          text: `ME4332 - Wednesday, April 1st 6 - 8 pm`,
+        },
+        {
+          tag: "p",
+          text: `Come see what the 2025-2026 executives have been doing this year and see the candidates running for our 2026-2027 year! `,
+        },
+      ],
+    },
+    {
+      title: `Run for the IEEE & WIE 2026-2027 executive team! `,
+      img: `IEEE Carleton Executive 2026.png`,
+      img: `IEEE Carleton Executive 2026 - WIE.png`,
+      body: [
+        {
+          tag: "h2",
+          text: `Present your election platform at our AGM: Wednesday, April 1st 6 - 8 pm, in ME4332`,
+        },
+        {
+          tag: "htmlString",
+          text: `Find the nomination form and election platform to run here: <a href= "https://linktr.ee/ieee.carleton">IEEE Carleton Linktree</a>`,
+        },
+      ],
+    },
+    {
+      title: "EVENTS!",
+      body: [],
+    },
+    {
+      title: `WIE Prof Talk`,
+      img: 'avani_jo_spac_2024.jpg',
+      body: [
+        {
+          tag: "h2",
+          text: `Wednesday, March 25th at 6 pm`,
+        },
+        {
+          tag: "p",
+          text: `Make sure to check the carletonwie instagram closer to the date for more details!`,
+        },
+      ],
+    },
+    {
+      title: `WIE Cocoa & Cram`,
+      img: 'hotchocolateicon.png',
+      body: [
+        {
+          tag: "h2",
+          text: `April 10th 12-4`,
+        },
+        {
+          tag: "p",
+          text: `Make sure to check the carletonwie instagram closer to the date for more details!`,
+        },
+      ],
+    },
+    {
+      title: "Ongoing",
+      body: [],
+    },
+    {
+      title: `SLAM`,
+      img: 'SLAM_poster.png',
+      body: [
+        {
+          tag: "p",
+          text: `Need resources, advice, and/or tools for your project?
+          Fill out this form and the SLAM team can help you with 3D printing and our various other tools/services! 
+          Your project will then be rejected or approved at the discretion of the SLAM Lead.`,
+        },
+        {
+          tag: "p",
+          text: `You may reach the SLAM Lead at: slamlead@ieeecarleton.ca`,
+        },
+        {
+          tag: "htmlString",
+          text: `<a href= "https://docs.google.com/forms/d/e/1FAIpQLSfxxIQEnLlSaJsXSakaBcax5SLRToCbogUNEasgU6T8TLmydg/viewform">Project Proposal Form</a>`,
+        },
+      ],
+    },
+  ],
+};
