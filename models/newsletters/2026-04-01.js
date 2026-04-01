b7fd71d720f0c8319c7596151d39091c3e994ad9
Beck Braun <87209578+pyrobeck@users.noreply.github.com>
2026-03-31 23:31:24 -0400

    april newsletter

---
+++ models/newsletters/2026-04-01.js
@@ -0,0 +1,93 @@
+module.exports = {
+  toast: [
+    {
+      tag: "h2",
+      text: "APRIL 1, 2026",
+    },
+    {
+      tag: "p",
+      text: "Happy End of Term & April Fools!",
+    },
+    {
+      tag: "p",
+      text: `If you are having trouble viewing this as an email, please contact publications@ieeecarleton.ca.`,
+    },
+  ],
+  articles: [
+    {
+      title: "TONIGHT!",
+      body: [],
+    },
+    {
+      title: `IEEE Carleton AGM`,
+      img: `IEEE AGM 2026.png`,
+      body: [
+        {
+          tag: "h2",
+          text: `ME4332 - Wednesday, April 1st 6 - 8 pm`,
+        },
+        {
+          tag: "p",
+          text: `Come see what the 2025-2026 executives have been doing this year and see the candidates running for our 2026-2027 year! `,
+        },
+      ],
+    },
+    {
+      title: `Run for the IEEE & WIE 2026-2027 executive team! `,
+      img: `IEEE Carleton Executive 2026.png`,
+      //img: `IEEE Carleton Executive 2026 - WIE.png`,
+      body: [
+        {
+          tag: "h2",
+          text: `Present your election platform at our AGM tonight: Wednesday, April 1st 6 - 8 pm, in ME4332`,
+        },
+        {
+          tag: "htmlString",
+          text: `Find the nomination form and election platform to run here: <a href= "https://linktr.ee/ieee.carleton">IEEE Carleton Linktree</a>`,
+        },
+      ],
+    },
+    {
+      title: "EVENTS!",
+      body: [],
+    },
+    {
+      title: `WIE Cocoa & Cram`,
+      img: 'Cocoa and cram icon winter 2026.png',
+      body: [
+        {
+          tag: "h2",
+          text: `April 10th 12-4`,
+        },
+        {
+          tag: "p",
+          text: `Make sure to check the carletonwie instagram closer to the date for more details!`,
+        },
+      ],
+    },
+    {
+      title: "Ongoing",
+      body: [],
+    },
+    {
+      title: `SLAM`,
+      img: 'SLAM_poster.png',
+      body: [
+        {
+          tag: "p",
+          text: `Need resources, advice, and/or tools for your project?
+          Fill out this form and the SLAM team can help you with 3D printing and our various other tools/services! 
+          Your project will then be rejected or approved at the discretion of the SLAM Lead.`,
+        },
+        {
+          tag: "p",
+          text: `You may reach the SLAM Lead at: slamlead@ieeecarleton.ca`,
+        },
+        {
+          tag: "htmlString",
+          text: `<a href= "https://docs.google.com/forms/d/e/1FAIpQLSfxxIQEnLlSaJsXSakaBcax5SLRToCbogUNEasgU6T8TLmydg/viewform">Project Proposal Form</a>`,
+        },
+      ],
+    },
+  ],
+};
